8d5f6031ff22b8eee13612272d053ab67e80d674
Andrecox1 <andresfelipear3357963@gmail.com>
2026-03-31 23:03:02 -0500

    add 412020/412020.lua

---
+++ 412020/412020.lua
@@ -0,0 +1,45 @@
+-- 412020's Lua and Manifest Created by Morrenus
+-- Metro Exodus
+-- Created: September 30, 2025 at 00:33:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 10
+-- Total DLCs: 5
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(412020) -- Metro Exodus
+-- MAIN APP DEPOTS
+addappid(412021, 1, "f06003bf9aba2630c97f3f84b80f14067b0df94fb9bfabadcbaea93e1ae4c6f7") -- Project Malta Content
+--setManifestid(412021, "8743909117511884672", 75168350582)
+addappid(412022, 1, "fdaa0cc6fe8207b1742e3d1fe9b0c62fc848da589cba441eef409d816d6aa775") -- Project Malta Binaries
+--setManifestid(412022, "4169096906348758341", 76944296)
+addappid(412023, 1, "2d1188fda4fe6c142425d0a9c34f0e6353e5b09eaf0f9967995ecc2948af10f8") -- Metro Exodus Linux
+--setManifestid(412023, "4901698992981806692", 76590606429)
+addappid(412024, 1, "45161252e073acc402c8cb3c9227484c10aa4f205f1e709294d2c220f6241ded") -- Metro Exodus MacOS
+--setManifestid(412024, "2770016577399373364", 84180704400)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+--setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Metro Exodus - The Two Colonels (AppID: 889920)
+addappid(889920)
+addappid(889920, 1, "0c8376c99ff46438fef56701e400b882822dd57b5d292398e222228f628849a6") -- Metro Exodus - The Two Colonels - Metro Exodus DLC 01 (889920) Depot
+--setManifestid(889920, "361436782434919800", 1294304024)
+-- Metro Exodus - Sams Story (AppID: 889921)
+addappid(889921)
+addappid(889921, 1, "ba6a1b0ca33f2370d28d58adc6b53ed79d5fb88d1931130c96ecf95df0530ae2") -- Metro Exodus - Sams Story - "Metro Exodus DLC 02 (889921)"-Depot
+--setManifestid(889921, "181613428803252911", 6929930285)
+-- Metro Exodus (D1) OST (AppID: 889922)
+addappid(889922)
+addtoken(889922, "18063194108132517671")
+addappid(889922, 1, "8bed7c94e8395e6a090d72ce60b7048ef9995126be4d7c04896c25ed74382608") -- Metro Exodus (D1) OST - Metro Exodus (D1) OST (889922) Depot
+--setManifestid(889922, "2273948263506713647", 172227586)
+-- Metro Exodus Artbook (AppID: 889923)
+addappid(889923)
+addtoken(889923, "10381871630932097954")
+addappid(889923, 1, "2853b589418e3e2cdbe78e9e851ac23e54a9f14bef73641a0309f59f4d4f37d1") -- Metro Exodus Artbook - Metro Exodus Artbook (889923) Depot
+--setManifestid(889923, "1970836693290980708", 54189804)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(924220) -- Metro Exodus Season Pass
