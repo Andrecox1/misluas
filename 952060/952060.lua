cdc575ba8b1421073a4cc5135aa153d9e5caebff
Andrecox1 <andresfelipear3357963@gmail.com>
2026-03-31 22:54:12 -0500

    add 952060/952060.lua

---
+++ 952060/952060.lua
@@ -0,0 +1,29 @@
+-- 952060's Lua and Manifest Created by Morrenus
+-- Resident Evil 3
+-- Created: October 05, 2025 at 08:13:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 2
+-- Shared Depots: 2
+-- Blacklisted Depots: 1
+--   Depot 952061: Censored Assets
+
+-- MAIN APPLICATION
+addappid(952060) -- Resident Evil 3
+-- MAIN APP DEPOTS
+addappid(952062, 1, "fdfbc9447d8302b8a7b325ec2bccaa81e77cef9d8289ead20348a702cce53e40") -- E1 Content WW
+--setManifestid(952062, "8863028453226553691", 24854070365)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+--setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- RESIDENT EVIL 3 - Classic Costume Pack (AppID: 1158730)
+addappid(1158730)
+addappid(952063, 1, "e5e87afc561b9663966c6f3fb6a9de1dac7466dfb32db183fbcfbeb5f011257b") -- RESIDENT EVIL 3 - Classic Costume Pack - E1 DLC
+--setManifestid(952063, "7504296696893507629", 680936443)
+-- Resident Evil 3 - All In-game Rewards Unlock (AppID: 1348140)
+addappid(1348140)
+addappid(952064, 1, "438a8c22458963cec5059bec6e195c24b9988cf2c2d2a33298db12146597812c") -- Resident Evil 3 - All In-game Rewards Unlock - E1 Unlock DLC
+--setManifestid(952064, "4916965303803227400", 11404)
