c620148062588f46decb5352847ccac7eb7fe5f4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-03-31 22:30:12 -0500

    add 1229490/1229490.lua

---
+++ 1229490/1229490.lua
@@ -0,0 +1,12 @@
+-- 1229490's Lua and Manifest Created by Morrenus
+-- ULTRAKILL
+-- Created: March 04, 2026 at 22:35:17 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1229490, 1, "3e69a773d4912c50a402095b7a0595abb1f73d80770be3aebda90d35e2881a21") -- ULTRAKILL
+-- MAIN APP DEPOTS
+addappid(1229491, 1, "347634f48592b176da96c10b8afe6898527aa5c218d89930f9d6e5b6d5e7c8ba") -- ULTRAKILL Content
+--setManifestid(1229491, "5628746843149106870", 3847538373)
