b05ae1e8cba5c893f477299f00197c3a01398abf
Andrecox1 <andresfelipear3357963@gmail.com>
2026-03-31 22:22:33 -0500

    add 242700/242700.lua

---
+++ 242700/242700.lua
@@ -0,0 +1,22 @@
+-- 242700's Lua and Manifest Created by Morrenus
+-- Injustice: Gods Among Us Ultimate Edition
+-- Created: September 29, 2025 at 19:14:54 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(242700) -- Injustice: Gods Among Us Ultimate Edition
+-- MAIN APP DEPOTS
+addappid(242701, 1, "a1bf375c183697499d648e6a4b10fbaf6f0913d6c54a0c99c44d9a4b45479bc1") -- Injustice Content
+--setManifestid(242701, "4325036726652909396", 21454831690)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+--setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+--setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") -- .NET 4.0 Client Profile Redist (Shared from App 228980)
+--setManifestid(229003, "8740933542064151477", 43001447)
