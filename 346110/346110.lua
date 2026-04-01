501c3c5fd84812a86d4008597d673ff58e44c96a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-01 16:07:34 -0500

    add 346110/346110.lua

---
+++ 346110/346110.lua
@@ -0,0 +1,110 @@
+-- 346110's Lua and Manifest Created by Morrenus
+-- ARK: Survival Evolved
+-- Created: February 01, 2026 at 11:39:07 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 29
+-- Total DLCs: 13
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(346110, 1, "e16ff7f08225d9ae3635a98833a1c63acc58b2a104b0b37a9605d494683787be") -- ARK: Survival Evolved
+-- MAIN APP DEPOTS
+addappid(346111, 1, "5edb307972cd5bf1e3081aedc986ef721dfd27cae16d0a976c6b7ea6e8ff2089") -- ARK: Survival Evolved Content
+--setManifestid(346111, "6154025194991279746", 177886893644)
+addappid(346113, 1, "3cde616ad9b9f9def5512691fe0a3ba1dfe8a6d773e68ea3b01f73b5f4f5a6bd") -- ARK: Survival Evolved Content Mac OS X
+--setManifestid(346113, "8339919800688829995", 97861863178)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+--setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+--setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+--setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- The Center - ARK Expansion Map (AppID: 473850)
+addappid(473850)
+addappid(346114, 1, "d8d49ab98f0f7530c0c2926213c7ac64625712a2efbbc96e6b3f06943741f806") -- The Center - ARK Expansion Map - TheCenter ARK Expansion Windows
+--setManifestid(346114, "3763053575682314024", 11975224342)
+addappid(346115, 1, "32f03ae1bf21e71fbd0361230a6442e2a221bbfced8fdb4fd37b7ab0225cb99c") -- The Center - ARK Expansion Map - The Center ARK Expansion Mac
+--setManifestid(346115, "7211460979435225614", 2811927869)
+-- Primitive - ARK Total Conversion (AppID: 508150)
+addappid(508150)
+addappid(346117, 1, "5a2bbc7508560b4d6f663c0db1923379ce85a1d08ac9750e64bdb821968e5245") -- Primitive - ARK Total Conversion - Primitive+ Windows
+--setManifestid(346117, "2507825098602328137", 1904405809)
+addappid(346118, 1, "8cffc3c5d721b966af7b470e8ed392cb0440ca975c4f28c6b20bbb0ae437d903") -- Primitive - ARK Total Conversion - Primitive+ Mac
+--setManifestid(346118, "1999006499798962652", 1711801410)
+-- ARK Scorched Earth - Expansion Pack (AppID: 512540)
+addappid(512540)
+addappid(375351, 1, "80573113a7bf294555b4c8e4e041c69e7a5a69527a4b65d3ce6f47c2382488a7") -- ARK Scorched Earth - Expansion Pack - Scorched Earth Expansion Pack - Windows
+--setManifestid(375351, "4395836124878524913", 9712594720)
+addappid(375352, 1, "4165ab1d0803718ec4e69bdea9e03cdb00d9be97d60761600f69aa02635a6818") -- ARK Scorched Earth - Expansion Pack - Scorched Earth Expansion Pack - Mac
+--setManifestid(375352, "2239387445239950885", 1177087271)
+-- Ragnarok - ARK Expansion Map (AppID: 642250)
+addappid(642250)
+addappid(375354, 1, "e2fa71e01810bbd554e94713e84efb084e0a99ff78f21fba3c201410f74e9a19") -- Ragnarok - ARK Expansion Map - Content Depot Windows
+--setManifestid(375354, "3831237975753864887", 26823559652)
+addappid(375355, 1, "538aa57c154732fc995904719ff0ae3ebde300e4f4d6be40ec1241b6abef9c54") -- Ragnarok - ARK Expansion Map - Content Depot Mac
+--setManifestid(375355, "5324252023372007884", 2953248412)
+-- ARK Aberration - Expansion Pack (AppID: 708770)
+addappid(708770)
+addappid(375357, 1, "9bc45b9b59f3f32154ee76b47ff8b6015e92d5a6342c444bed3eb210bcd24b60") -- ARK Aberration - Expansion Pack - New Content Windows
+--setManifestid(375357, "6791072184851719971", 17595802903)
+addappid(375358, 1, "380b1dd0c8d2a98996c20021513e4af8768d5c1c19b0299d428c149ea6419be0") -- ARK Aberration - Expansion Pack - New Content Mac
+--setManifestid(375358, "426338121519536361", 1124001660)
+-- ARK Extinction - Expansion Pack (AppID: 887380)
+addappid(887380)
+addappid(473851, 1, "99fb597107882942ca94e2038215f03f8707d57b059460ec6bde8265bf020733") -- ARK Extinction - Expansion Pack - Newest Content Windows
+--setManifestid(473851, "4803031605764221686", 20381009517)
+addappid(473852, 1, "cf96b8769d1734315042b0239dd110a83d831f4da2cfcc6066d52dd26e294069") -- ARK Extinction - Expansion Pack - Newest Content Mac
+--setManifestid(473852, "2616742401864906431", 1453254176)
+-- Valguero - ARK Expansion Map (AppID: 1100810)
+addappid(1100810)
+addappid(473854, 1, "dac71f84f6dd940b7bfe12e5df2bb98eff8ba622d76c30091c52ecfd60bbf334") -- Valguero - ARK Expansion Map - Digiorno Windows
+--setManifestid(473854, "3892036305094432143", 21763833829)
+addappid(473855, 1, "f4ab0847fc2c5a996d0dd7e62142319b91e057364458f627065a679042de5cf2") -- Valguero - ARK Expansion Map - Digiorno Mac
+--setManifestid(473855, "9041462482401519649", 2576558140)
+-- ARK Genesis Season Pass (AppID: 1113410)
+addappid(1113410)
+addappid(473858, 1, "41f8a645e812903465d0edae936402edbec301d6e0704cddc3192e6a661dc133") -- ARK Genesis Season Pass - 9035768 Mac
+--setManifestid(473858, "1929691095877787242", 6348254490)
+addappid(473857, 1, "c9bb21644270fd6883bc3f007aa66f625ea3281ad2196900dfbbdf12ceb58ce2") -- ARK Genesis Season Pass - 9035768 Windows
+--setManifestid(473857, "3988040328692901254", 62317741197)
+-- Crystal Isles - ARK Expansion Map (AppID: 1270830)
+addappid(1270830)
+addappid(1318685, 1, "323f243a7ced4f90628ba9da901ca96302350c23c625153d21425d93df0a62cf") -- Crystal Isles - ARK Expansion Map - HotPocketz Windows
+--setManifestid(1318685, "1956361019326166695", 49566129019)
+addappid(1318686, 1, "dd79aabbad1a2c16599b9c57ea58081e50c7ebd49a68a445823000c509361a43") -- Crystal Isles - ARK Expansion Map - HotPocketz Mac
+--setManifestid(1318686, "2905223544564987419", 5220066333)
+-- Lost Island - ARK Expansion Map (AppID: 1691800)
+addappid(1691800)
+addappid(1691801, 1, "db89fe99710be8d80b85377d37210487dbce8b9bfb8e90767f97e138d3e0206e") -- Lost Island - ARK Expansion Map - PizzaRollz Windows
+--setManifestid(1691801, "7582104043070509624", 65842061002)
+addappid(1691802, 1, "c8ad6983cc292e1bacdb1b413aef29dd704430d075258337a58edf62a8d04fd6") -- Lost Island - ARK Expansion Map - PizzaRollz Mac
+--setManifestid(1691802, "8855925838913262422", 9068912997)
+-- Fjordur - ARK Expansion Map (AppID: 1887560)
+addappid(1887560)
+addappid(1887561, 1, "a3f5aa53c16af324c6b57326578ce7a4341c7aef7a14ff52ccfa35cba9fccbc7") -- Fjordur - ARK Expansion Map - Lemonbar Windows
+--setManifestid(1887561, "1479180173861153414", 34616741759)
+addappid(1887562, 1, "9412a47f669490322d818e4b5d4d184fae7b3ac2ee0db526751f9da74ecd8901") -- Fjordur - ARK Expansion Map - Lemonbar Mac
+--setManifestid(1887562, "2849859682584703966", 6654144035)
+-- Aquatica - ARK Expansion Map (AppID: 3537070)
+addappid(3537070)
+addappid(3537070, 1, "9a07e220390d63d22b6fca95fca32f71bcad2d28494d72ee2c79b5afcb84c473") -- Aquatica - ARK Expansion Map - Depot 3537070
+--setManifestid(3537070, "4062438905818018280", 32780601789)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(696680) -- ARK Survival Evolved Season Pass
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(473850) -- Depot 473850 (empty depot)
+-- addappid(508150) -- Depot 508150 (empty depot)
+-- addappid(508152) -- Depot 508152 (empty depot)
+-- addappid(512540) -- Depot 512540 (empty depot)
+-- addappid(642250) -- Depot 642250 (empty depot)
+-- addappid(708770) -- Depot 708770 (empty depot)
+-- addappid(887380) -- Depot 887380 (empty depot)
+-- addappid(1100810) -- Depot 1100810 (empty depot)
+-- addappid(1113410) -- Depot 1113410 (empty depot)
+-- addappid(1270830) -- Depot 1270830 (empty depot)
+-- addappid(1691800) -- Depot 1691800 (empty depot)
+-- addappid(1887560) -- Depot 1887560 (empty depot)
