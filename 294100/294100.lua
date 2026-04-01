09fed7c19cca1c3c1c301051f4df4a493d66d3d5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-03-31 22:36:09 -0500

    add 294100/294100.lua

---
+++ 294100/294100.lua
@@ -0,0 +1,73 @@
+-- 294100's Lua and Manifest Created by Morrenus
+-- RimWorld
+-- Created: December 15, 2025 at 05:53:51 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 24
+-- Total DLCs: 8
+
+-- MAIN APPLICATION
+addappid(294100, 1, "44498055cce67eea5d84c5b6bc1bd5ca9e8c988963aea857574277fc217cbad0") -- RimWorld
+-- MAIN APP DEPOTS
+addappid(294101, 1, "a5f3f4466343e7297b20a43dde9d6b6ed9d0d4230b74689caf5746d11de0367d") -- RimWorld (Windows)
+--setManifestid(294101, "7953786152513421342", 565935234)
+addappid(294104, 1, "db7d2330e8748efe01ff1674580919b72e40879517e378b568533acc2863ec26") -- RimWorld (64-bit)
+--setManifestid(294104, "2772688105937893244", 576161411)
+addappid(294102, 1, "dfa49a9452d912eb031ecd2111c1a9b1331bfe58000b9d68b8d5b646acbd1054") -- RimWorld (Mac)
+--setManifestid(294102, "2497899177261850067", 599769619)
+addappid(294103, 1, "a8218a15388519f5ded274def07d1dd1c535e1bfe646eb17463775bf47f63e39") -- RimWorld (Linux)
+--setManifestid(294103, "3134934206210576837", 576202126)
+-- DLCS WITH DEDICATED DEPOTS
+-- RimWorld - Royalty (AppID: 1149640)
+addappid(1149640)
+addappid(1149641, 1, "298db7a9d64ecbc820422e53907ab224611829745e8e4b20a4387efba317bee1") -- RimWorld - Royalty - RimWorld - Merrymaker (Windows)
+--setManifestid(1149641, "2347721629532249182", 60997635)
+addappid(1149644, 1, "fafd64b303cda2e9b938ce60e7af4653a574b035d59cccd31be1ff5c2dc8c0ee") -- RimWorld - Royalty - RimWorld - Merrymaker (64-bit)
+--setManifestid(1149644, "1883707662054891831", 60997635)
+addappid(1149642, 1, "14ba0c9d972b57cf45870968dfd396baa35f94d87f117e91e64ba03d25d5d5ad") -- RimWorld - Royalty - RimWorld - Merrymaker (Mac)
+--setManifestid(1149642, "1765958875109776452", 60997631)
+addappid(1149643, 1, "a2065b38cf6723c9cc336f68388389a7b437e4404a9856982d2594a928b49b23") -- RimWorld - Royalty - RimWorld - Merrymaker (Linux)
+--setManifestid(1149643, "1527472528506197989", 60997635)
+-- RimWorld - Ideology (AppID: 1392840)
+addappid(1392840)
+addappid(294105, 1, "be81976289e837bb2cb5c9858b07b9f72612a69414697f44c5b138fb3041a250") -- RimWorld - Ideology - RimWorld - Kepler (Windows)
+--setManifestid(294105, "3885486926018788663", 57132312)
+addappid(294106, 1, "d1a9518d7b68f46dc53981fe05514f6daa66a3aa125637c13c1535e8cdb8e4b6") -- RimWorld - Ideology - RimWorld - Kepler (64-bit)
+--setManifestid(294106, "1227145614562470487", 57132313)
+addappid(294107, 1, "132106f7d7fd90e2cef97d5a01632ca943c2147afcd166c202d51865cf0786e6") -- RimWorld - Ideology - RimWorld - Kepler (Mac)
+--setManifestid(294107, "5359145108392473956", 57132311)
+addappid(294108, 1, "80efa15b6a1330572a30a52fc0147fd7fecf36ea2e3acfa8d7db1d78f28c2144") -- RimWorld - Ideology - RimWorld - Kepler (Linux)
+--setManifestid(294108, "569687740856239076", 57132312)
+-- RimWorld - Biotech (AppID: 1826140)
+addappid(1826140)
+addappid(367683, 1, "eae351654f6795aae5320150c2801aa853cc2e46412b72bccca2fbf3f50af32a") -- RimWorld - Biotech - RimWorld - Euclid (Windows)
+--setManifestid(367683, "883708408590570304", 69278535)
+addappid(367684, 1, "699e092d84e16c90a62ebfcd68d1c568c4947ae94d538e999fe56e7cc6043942") -- RimWorld - Biotech - RimWorld - Euclid (64-bit)
+--setManifestid(367684, "5027583327039997575", 69278535)
+addappid(367685, 1, "8a4f6b777fb54e2730c7ace0e4f85ddf4fc58ea3607ad94eb9cf01acb2571446") -- RimWorld - Biotech - RimWorld - Euclid (Mac)
+--setManifestid(367685, "1751266350468796361", 69278533)
+addappid(367686, 1, "ddec61094ce592f6db40f3867b99e458ea4ce2af56cb8f62474631b33a96c734") -- RimWorld - Biotech - RimWorld - Euclid (Linux)
+--setManifestid(367686, "1776297058088144151", 69278536)
+-- RimWorld - Anomaly (AppID: 2380740)
+addappid(2380740)
+addappid(294109, 1, "d670f6ba851c27bd62423853e8dd18d5e32b75393b65bf18914103a0b0db63e9") -- RimWorld - Anomaly - Depot 294109
+--setManifestid(294109, "7818336393444714786", 104972130)
+addappid(294110, 1, "96eea289fb25789e7d7791bcf77e2af4d62eacaa1c05866991b72e75b805069c") -- RimWorld - Anomaly - Depot 294110
+--setManifestid(294110, "868105332660730820", 104972130)
+addappid(294111, 1, "62487ea6d7f63684007a6713749fd8fb14c7955e885aa14cda8c5daa5d930f78") -- RimWorld - Anomaly - Depot 294111
+--setManifestid(294111, "8082717694556047429", 104972128)
+addappid(294112, 1, "69dc90bdbb7c2c47b168606c5b8bde5ab1c6ec88404fa31bfb1faaf2d5c452c3") -- RimWorld - Anomaly - Depot 294112
+--setManifestid(294112, "724110462483695149", 104972129)
+-- RimWorld - Odyssey (AppID: 3022790)
+addappid(3022790)
+addappid(294113, 1, "3a667846a7e65282aae8275c6848ede3e0e48b3959c6045ebc0ceff98db49e0a") -- RimWorld - Odyssey - Depot 294113
+--setManifestid(294113, "6306290553655225872", 94193221)
+addappid(294114, 1, "76f5a14f8283fd55fe1b8e0eca01a5408e01b23611e70c2cb92a7e303e835646") -- RimWorld - Odyssey - Depot 294114
+--setManifestid(294114, "6581907861000077546", 94193221)
+addappid(294115, 1, "40c853711dd9329158bf8acf348ff5b209664fb273949bee5276501836c32943") -- RimWorld - Odyssey - Depot 294115
+--setManifestid(294115, "5979071730349817278", 94193233)
+addappid(294116, 1, "17b44d4ac86a4bd0f697320c0b34672092058aba22133b9b9028e11cff1c498d") -- RimWorld - Odyssey - Depot 294116
+--setManifestid(294116, "6423111671391618215", 94193219)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(367680) -- RimWorld Name in Game Access
+addappid(367681) -- RimWorld Backstory in Game Access
+addappid(367682) -- RimWorld Pirate King Access
