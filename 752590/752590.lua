ceb5c0c013a3e140cacf4f079944a28389d3d2da
Andrecox1 <andresfelipear3357963@gmail.com>
2026-03-31 22:29:27 -0500

    add 752590/752590.lua

---
+++ 752590/752590.lua
@@ -0,0 +1,18 @@
+-- 752590's Lua and Manifest Created by Morrenus
+-- A Plague Tale: Innocence
+-- Created: September 30, 2025 at 17:34:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(752590) -- A Plague Tale: Innocence
+-- MAIN APP DEPOTS
+addappid(752591, 1, "31c0eb1a38b3926e0e589c25300225886ac653a8415d3612864cd26664681460") -- A Plague Tale: Innocence Content
+--setManifestid(752591, "476939438164423828", 44611560254)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+--setManifestid(228987, "4302102680580581867", 29664201)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(967380) -- A Plague Tale Innocence - Coats of Arms DLC
