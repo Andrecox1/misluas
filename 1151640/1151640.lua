0b6d85261a85b1df8e710cecf26d02e2f1f0bd0e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-03-31 22:36:30 -0500

    add 1151640/1151640.lua

---
+++ 1151640/1151640.lua
@@ -0,0 +1,26 @@
+-- 1151640's Lua and Manifest Created by Morrenus
+-- Horizon Zero Dawn™ Complete Edition
+-- Created: September 29, 2025 at 18:11:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1151640) -- Horizon Zero Dawn™ Complete Edition
+-- MAIN APP DEPOTS
+addappid(1151641, 1, "df518a946513a91a56704336384864918ec740df8537ca1812a8f54264355361") -- Azura Content
+--setManifestid(1151641, "3712644444013817231", 61956213062)
+addappid(1151642, 1, "59255c4691e86b0813ddced2314be6ac433c80eacb80b7d716c1cc76ca2032f2") -- GlobalData
+--setManifestid(1151642, "6403385661621685335", 15582635700)
+addappid(1151644, 1, "8a4848c62e8fcd4b79f78195190e4595e6c74b9a81ebe42b60e82bdd9b4b5c17") -- JapanData
+--setManifestid(1151644, "7685822332590815529", 17144068395)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+--setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITH DEDICATED DEPOTS
+-- HZD Comic (AppID: 2107510)
+addappid(2107510)
+addtoken(2107510, "17624319664149755126")
+addappid(2107510, 1, "3b0af1076eca77b7becd91fffc4698281fef99bc06ba98c1dd31f11aa63b5f8c") -- HZD Comic - Depot 2107510
+--setManifestid(2107510, "1773665774764488197", 393727455)
