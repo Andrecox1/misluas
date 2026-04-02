771f302c605f8b1d902dc3380c7b20f251f28ea6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-02 16:56:39 -0500

    add 2130460/2130460.lua

---
+++ 2130460/2130460.lua
@@ -0,0 +1,37 @@
+-- 2130460's Lua and Manifest Created by Morrenus
+-- FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse
+-- Created: September 29, 2025 at 11:53:37 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 7
+-- Total DLCs: 5
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2130460) -- FATAL FRAME / PROJECT ZERO: Mask of the Lunar Eclipse
+-- MAIN APP DEPOTS
+addappid(2130461, 1, "ebca96a0f4137d2bb98654148efb453f475b6f091226288bdf31d232a41b7c01") -- Depot 2130461
+--setManifestid(2130461, "3008367655729204016", 13867802106)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+--setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse Digital Artbook (AppID: 2257211)
+addappid(2257211)
+addappid(2257211, 1, "2b8fa2b81702ac842fbabc262af0a425b1cbd3d4cc2517b33217d654b3a2e19f") -- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse Digital Artbook - Depot 2257211
+--setManifestid(2257211, "10251861383826610", 1144268511)
+-- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse Rogetsu Isle Dinner Party Costume Set (AppID: 2257212)
+addappid(2257212)
+addappid(2257212, 1, "6a3c1c642b13e73002c765f0411d7162b6684697950c82f12b0364706ba6deab") -- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse Rogetsu Isle Dinner Party Costume Set - Depot 2257212
+--setManifestid(2257212, "207626751715885023", 172122620)
+-- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse Ruka Exclusive Costume Marie Rose Outfit (AppID: 2257213)
+addappid(2257213)
+addappid(2257213, 1, "08baa8a0787b02119e8c8723cb9696d781d91c742f1f0bb8b425a41567aee835") -- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse Ruka Exclusive Costume Marie Rose Outfit - Depot 2257213
+--setManifestid(2257213, "421178101224505117", 25502868)
+-- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse RukaMisakiChoshiro-Exclusive Accessory (Spirit Stone Flashlight Hat  Fox Mask) (AppID: 2257214)
+addappid(2257214)
+addappid(2257214, 1, "1cf2f80cdfae6511f1e0b872b9c1b16349c20f4cb9ee6c846a70cefdc520b723") -- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse RukaMisakiChoshiro-Exclusive Accessory (Spirit Stone Flashlight Hat  Fox Mask) - Depot 2257214
+--setManifestid(2257214, "740608742303706834", 32043200)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2257210) -- FATAL FRAME  PROJECT ZERO Mask of the Lunar Eclipse Digital Deluxe Upgrade Pack
