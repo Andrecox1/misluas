cd17abf94e4c65602f4b29b330f0116d0e6a041c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-02 17:33:17 -0500

    add 312520/312520.lua

---
+++ 312520/312520.lua
@@ -0,0 +1,21 @@
+-- 312520's Lua and Manifest Created by Morrenus
+-- Rain World
+-- Created: April 01, 2026 at 17:32:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(312520, 1, "ef800a2479027ef2829d53f0c9cccd74da5c5f7fa82e5bb626c069cd07cd2b99") -- Rain World
+-- MAIN APP DEPOTS
+addappid(312521, 1, "f36b23dfaca5b387508e33fe17732bcd48a16ab0fef3b1eb819494ad76d5ce2a") -- Rain World Content
+--setManifestid(312521, "3528836243210047492", 4329509353)
+-- DLCS WITH DEDICATED DEPOTS
+-- Rain World Downpour (AppID: 1933390)
+addappid(1933390)
+addappid(1933390, 1, "cf34e0fb21789d7fd65c827a966d574e2fbd1e6b0cb47f81322256690ad70532") -- Rain World Downpour - Depot 1933390
+--setManifestid(1933390, "513589851985004389", 2346405187)
+-- Rain World The Watcher (AppID: 2857120)
+addappid(2857120)
+addappid(2857120, 1, "f998f9f9862dc3cc8026d11be949bc4d7c17882f1240b6982766721fceaae0ad") -- Rain World The Watcher - Depot 2857120
+--setManifestid(2857120, "7297478180947374009", 1713375345)
