b481561254d87f6ab0853c82a83a640c65239153
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-02 12:10:30 -0500

    add 454650/454650.lua

---
+++ 454650/454650.lua
@@ -0,0 +1,61 @@
+-- 454650's Lua and Manifest Created by Morrenus
+-- DRAGON BALL XENOVERSE 2
+-- Created: December 17, 2025 at 07:40:37 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 36
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(454650) -- DRAGON BALL XENOVERSE 2
+-- MAIN APP DEPOTS
+addappid(454651, 1, "f42c03be492cf6df28782bcf7630521712bb5425d268fcfc599731056ec1a21c") -- SVAC10 Content
+--setManifestid(454651, "7238901667150908628", 37649783213)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+--setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- DRAGON BALL XENOVERSE 2 - Steve Aoki Pack (AppID: 540070)
+addappid(540070)
+addappid(540070, 1, "3fe54a572bc1b0c0f9f13f3aa0d468cb56bb0faee8eb10df6a992c161943cc21") -- DRAGON BALL XENOVERSE 2 - Steve Aoki Pack - DRAGON BALL Xenoverse 2 - Steve Aoki Pack (540070) Depot
+--setManifestid(540070, "5501846961741041819", 11792330)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(507510) -- DRAGON BALL XENOVERSE 2 - Super Pass
+addappid(508400) -- DRAGON BALL XENOVERSE 2 - Super Pack 1
+addappid(508401) -- DRAGON BALL XENOVERSE 2 - Super Pack 2
+addappid(508402) -- DRAGON BALL XENOVERSE 2 - Super Pack 3
+addappid(508403) -- DRAGON BALL XENOVERSE 2 - Super Pack 4
+addappid(508404) -- DRAGON BALL Xenoverse 2 - Future Trunks
+addappid(508405) -- DRAGON BALL XENOVERSE 2 - Pre-Order Bonus
+addappid(540071) -- DRAGON BALL Xenoverse 2 - TP Medal
+addappid(556270) -- DRAGON BALL Xenoverse 2 - TV show TP Medal
+addappid(621100) -- DRAGON BALL XENOVERSE 2 - Future Trunks (Super)
+addappid(739280) -- DRAGON BALL XENOVERSE 2 - Extra Pack Set
+addappid(739281) -- DRAGON BALL XENOVERSE 2 - Extra DLC Pack 1
+addappid(739282) -- DRAGON BALL XENOVERSE 2 - Extra DLC Pack 2
+addappid(740140) -- DRAGON BALL XENOVERSE 2 - Anime Music Pack
+addappid(740141) -- DRAGON BALL XENOVERSE 2 - Extra Pass
+addappid(740142) -- DRAGON BALL XENOVERSE 2 - Extra DLC Pack 3
+addtoken(740142, "1583159477200805808")
+addappid(740143) -- DRAGON BALL XENOVERSE 2 - Extra DLC Pack 4 
+addappid(740160) -- DRAGON BALL XENOVERSE 2 - Ultra Pack 1
+addappid(953020) -- DRAGON BALL XENOVERSE 2 - Anime Music Pack 2
+addappid(1108460) -- DRAGON BALL XENOVERSE 2 - Ultra Pack Set
+addappid(1108461) -- DRAGON BALL XENOVERSE 2 - Ultra Pack 2
+addtoken(1108461, "12437570787333637007")
+addappid(1507380) -- DRAGON BALL XENOVERSE 2 - Legendary Pack 1
+addappid(1544340) -- DRAGON BALL XENOVERSE 2 - Legendary Pack Set
+addappid(1744080) -- DRAGON BALL XENOVERSE 2 - Legendary Pack 2
+addappid(1999190) -- DRAGON BALL XENOVERSE 2 - Conton City Vote Pack
+addappid(2023140) -- DRAGON BALL XENOVERSE 2 Legend Patrol Pack
+addappid(2161540) -- DRAGON BALL XENOVERSE 2 - HERO OF JUSTICE Pack 1
+addappid(2161560) -- DRAGON BALL XENOVERSE 2 - HERO OF JUSTICE Pack Set
+addappid(2377310) -- DRAGON BALL XENOVERSE 2 - HERO OF JUSTICE Pack 2
+addappid(2814240) -- DRAGON BALL XENOVERSE 2 - FUTURE SAGA Chapter 1
+addappid(3188680) -- DRAGON BALL XENOVERSE 2 - FUTURE SAGA Chapter 2
+addappid(3387210) -- DRAGON BALL XENOVERSE 2 - Dragon Ball DAIMA Pack
+addappid(3581800) -- DRAGON BALL XENOVERSE 2 - Time Patrol Support Pack
+addappid(3839310) -- DRAGON BALL XENOVERSE 2 - FUTURE SAGA Chapter 3
+addappid(3905290) -- DRAGON BALL XENOVERSE 2 - FUTURE SAGA Pack Set
