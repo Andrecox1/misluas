12c374e3bca38339ebaa668cc8e0592d5056d665
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-03 12:02:44 -0500

    add 201810/201810.lua

---
+++ 201810/201810.lua
@@ -0,0 +1,48 @@
+-- 201810's Lua and Manifest Created by Morrenus
+-- Wolfenstein: The New Order
+-- Created: October 01, 2025 at 07:54:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 18
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(201810, 1, "6dfaef0cf35b15403d1bd15f88909b330ae83c471bb2ee22d937bddde19b1043") -- Wolfenstein: The New Order
+-- MAIN APP DEPOTS
+addappid(201811, 1, "d254eedc6a277b20e3dd87bdc8877882fa2aa9443cfd4d5169597ca24923bff2") -- Executable
+--setManifestid(201811, "4483959248306290042", 27769440)
+addappid(201812, 1, "5cceb657f3da6258c032aa8ada8d86274fa8c5d9cc37a06b9c41e10e9c881727") -- Resources (WW)
+--setManifestid(201812, "6190769423243292568", 4141015255)
+addappid(201813, 1, "84284dbd7c8c9dcad82c0f65dce2828ce06117045f5df674727dbc47e59439b1") -- Megatextures
+--setManifestid(201813, "4198771529530767213", 30499943154)
+addappid(201814, 1, "850746833329899a464019418dc2129117c56cd37d22886f37ba155b02ec0bc4") -- VMTR (WW)
+--setManifestid(201814, "7419404030065740466", 5127406724)
+addappid(201815, 1, "a67b5b0f70f9930f5bf5ef43b4c47a98edc934e8ee71cfbc1ef1e5cfe095326e") -- Videos C01-C07 (WW)
+--setManifestid(201815, "6372228485140000019", 2547612748)
+addappid(201816, 1, "cfb9994d4359e8d2644cd703a8635d68516e901cadd3ee49d109b872b5f77de9") -- Videos C08-C16 (WW)
+--setManifestid(201816, "2564310353164314558", 3882120248)
+addappid(201819, 1, "a391348b69b811d538e4541c335fd41e5f9603a07ca256a20c43a00a878a66b0") -- English Language
+--setManifestid(201819, "393115506002332855", 415137488)
+addappid(201824, 1, "f1caa0e4a7f205fe9f94cdbc268df210ba3e846986024e1d9d0384b0f8bcc969") -- Resources (JP)
+--setManifestid(201824, "4057511848496321450", 4125162006)
+addappid(201817, 1, "9aa6b86c00a585567a3b780191f32ebe6910ffb3b4729eb7baa76ed8214c82b4") -- VMTR (JP)
+--setManifestid(201817, "8962022361405031159", 5370085750)
+addappid(201818, 1, "9f693dc90add88bb4ddfa91b9be3bf594f52ba537040ea1be398a6e03f38bda5") -- Videos (JP)
+--setManifestid(201818, "8452286535441547273", 6130873844)
+addappid(201820, 1, "f20cee9d75e2f455cfc02f26304335e7c2e067f1ad8b517751cad7239bdcda72") -- French Language
+--setManifestid(201820, "4853410428376415487", 381303626)
+addappid(201821, 1, "b8d781a49b3cba8e81ee2d1bc1853b0202e9da232e71b6e7770c8acc826c0a8a") -- Spanish Language
+--setManifestid(201821, "2842838024493110567", 372439789)
+addappid(201822, 1, "ea3018204bab30951d6996ec98a6912fa73679ad57b312bbe65eb342a4a73735") -- Italian Language
+--setManifestid(201822, "7359381863497489497", 379845077)
+addappid(201823, 1, "33a582df83b02a0a791bc0c1009b71a19748bcdcd51b79f204246027984381a9") -- Japanese Language
+--setManifestid(201823, "8971042541874667620", 372850067)
+addappid(426820, 1, "b84790788f504d95fae914aff7d0ff39ac4e2ec66bb92f31f8f11675599a2c47") -- Wolfenstein: The New Order - Soundtrack (426820) Depot
+--setManifestid(426820, "8948924963522164096", 124636305)
+-- SHARED DEPOTS (from other apps)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (Shared from App 228980)
+--setManifestid(228982, "6413394087650432851", 9688647)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+--setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
