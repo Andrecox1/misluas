40849e4758f423a23f821a51289c746763473991
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-03 22:23:50 -0500

    add 612880/612880.lua

---
+++ 612880/612880.lua
@@ -0,0 +1,90 @@
+-- 612880's Lua and Manifest Created by Morrenus
+-- Wolfenstein II: The New Colossus
+-- Created: October 01, 2025 at 07:54:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 31
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(612880) -- Wolfenstein II: The New Colossus
+-- MAIN APP DEPOTS
+addappid(612882, 1, "d9aa24cc209df8e416ea797c131840676f26b57b5dbe2d0d99866474d304755d") -- Executable
+--setManifestid(612882, "332162649214284220", 59735024)
+addappid(624749, 1, "ad6398fc978e5a0b2ad763d2c398b7a1468fa7c9c4cd8980de79d0b5deca2af4") -- Resources (ROW, DEMO)
+--setManifestid(624749, "166817135839534724", 1088577225)
+addappid(612887, 1, "ca7800eddd8ba2e276b284cfb9420b1033e9e0c30baf88e373e6b9aa6603b6e3") -- Resources (ROW)
+--setManifestid(612887, "8273162783244227988", 2566340139)
+addappid(650412, 1, "de127e37b1dab8f431e9d0f20ce4bd82c4aa001b57c9907636ed5aacb0ebc8d9") -- Resources (CERO, DEMO)
+--setManifestid(650412, "9022113729599668303", 1044062904)
+addappid(624624, 1, "9591bbda4d937a04aa952ac5e123f4a8feaa909dad9a17b68b659734c72bcb87") -- Resources (CERO)
+--setManifestid(624624, "8613853100061840797", 2564982866)
+addappid(650414, 1, "0bcf1f48c344ed5088f436998ae02f7c33e7e666b0cc04ff43ef2c738bb55f0e") -- Videos (ROW, DEMO)
+--setManifestid(650414, "1746396293235515966", 673844500)
+addappid(612884, 1, "b6d8f534d5ec8bf04db31660757c5636454e29ade7141183cdf77e5edfb49aff") -- Videos (ROW)
+--setManifestid(612884, "2122901492143408844", 7215804116)
+addappid(650415, 1, "66c8ba97a64559e272aa9c60e0f90bf51bed0bde2ba8647b1c4d9c4b6f46dd66") -- Videos (CERO, DEMO)
+--setManifestid(650415, "1945011048598975996", 673843852)
+addappid(624623, 1, "a5b6a8cdb2664e3cbf66f98e10db2c679394849d876df685b71d5011af41a550") -- Videos (CERO)
+--setManifestid(624623, "618551768872795601", 7464644188)
+addappid(650416, 1, "99de8d9f6977fd3a54b88b91fd35d2f4394c4ec4659d36638d3375b007d4ad41") -- Videos (Shared, DEMO)
+--setManifestid(650416, "1613380009697405280", 36577660)
+addappid(624627, 1, "5eb4652adf06b6efe89cfd398ec58c662171ce7cee105331ecb39fe014a070cd") -- Videos (Shared)
+--setManifestid(624627, "3547469873055231551", 370612124)
+addappid(624748, 1, "83a558c1199e3a2ea2083bc55fd5ed67d298bbc57f0e2dcd042316d252241f9c") -- Textures (ROW, DEMO)
+--setManifestid(624748, "3592233195597680769", 10288694996)
+addappid(612886, 1, "3d21a2226482d6e8326922884a089f2f61fc8aabaa8da7dcf49507b5269142a4") -- Textures (ROW)
+--setManifestid(612886, "6858725330795673637", 13202919723)
+addappid(650411, 1, "ca94d0179b21d0ee64b662f619849ee6f5a1d03c93745a23535679adea3569a3") -- Textures (CERO, DEMO)
+--setManifestid(650411, "317468368842259630", 10301944973)
+addappid(624625, 1, "dad0ea7aad27ea8abbd11a493d912b4ee7bc5c5354626ed540a86e630b4e93e6") -- Textures (CERO)
+--setManifestid(624625, "5214085521133308917", 13184821040)
+addappid(650413, 1, "58cca4e469e31f759e00b5d1ea252a71e4c538d3051aa3579ec3d10806b86703") -- Lightmaps (DEMO)
+--setManifestid(650413, "4826278029060622680", 676215552)
+addappid(612883, 1, "c9b7a3baed5d35a9c24c81b571ce72da8e38e75ff57f1b7809046c0b482806d8") -- Lightmaps
+--setManifestid(612883, "5036286170467120241", 13011453120)
+addappid(612885, 1, "f0002d21bbb12a1cbcaf10fceede520be7d8b87e071c0f9f306ddd05caea957d") -- SoundBanks (ROW)
+--setManifestid(612885, "5038932946082422041", 2073573242)
+addappid(624626, 1, "d75ad5fdc6e1b5a0736979e584703e04ddc4753fef7ccb27f986baac549fecc9") -- SoundBanks (English)
+--setManifestid(624626, "6767799911521017463", 806519484)
+addappid(624628, 1, "c826f4a417336153e99ce1506b7d4da5e70ca05ab81848220447bcd084b8d9d2") -- SoundBanks (French)
+--setManifestid(624628, "5783598834907555405", 755641730)
+addappid(624629, 1, "ba7dd3d58ca23c51e9f179f1b18965fd1c3ec600d298d26afe88b5ecec434625") -- SoundBanks (Italian)
+--setManifestid(624629, "8742320481398942762", 775832022)
+addappid(624744, 1, "1beac930c286cb764dfb6fd379fbf50ae325132b064e2b0716a581b3478cacf5") -- SoundBanks (Spanish)
+--setManifestid(624744, "7588974950788225158", 763743719)
+addappid(624741, 1, "733d51cea2f0e1807db6f58ac79ea5264c0db12fea18d22aceaa9db7803a1c4f") -- SoundBanks (Russian)
+--setManifestid(624741, "3086930858230260400", 1149785023)
+addappid(624743, 1, "d63c0df8e700262db523d7a8d42abd2fa4c5367e07129af057b0d069a2ad976e") -- SoundBanks (Brazil)
+--setManifestid(624743, "5675117221408475835", 742869409)
+addappid(612889, 1, "9539f35bcc2bab1a9b6517a02cb2f44d295c651ffee2366d9899c12ee837b561") -- SoundBanks (Japanese)
+--setManifestid(612889, "7563900797837990703", 809868589)
+addappid(612888, 1, "be69fe4e99abe579beaca2b788c40e005b227efecaedb9757c45bdba209c5100") -- Simplified Chinese
+--setManifestid(612888, "4757386849333630100", 3492618433)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+--setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Wolfenstein II The Freedom Chronicles - Episode 1 (AppID: 624620)
+addappid(624620)
+addappid(762750, 1, "2ed466db2835036b650efcff72a34931a88fca7ff32c2990317e9a2e435a5936") -- Wolfenstein II The Freedom Chronicles - Episode 1 - DLC1 (ROW)
+--setManifestid(762750, "1153163775510174875", 3828717707)
+-- addappid(762751, 1, "MISSING_KEY") -- Wolfenstein II The Freedom Chronicles - Episode 1 - DLC1 (CERO) (no key available)
+-- Wolfenstein II The Freedom Chronicles - Episode 2 (AppID: 624621)
+addappid(624621)
+addappid(762740, 1, "518461383a927fa6f8b01c3ce717561a8061ec27057f00af03be400e4c4ac77e") -- Wolfenstein II The Freedom Chronicles - Episode 2 - DLC2 (ROW)
+--setManifestid(762740, "1386194745156710921", 3801703982)
+-- addappid(762741, 1, "MISSING_KEY") -- Wolfenstein II The Freedom Chronicles - Episode 2 - DLC2 (CERO) (no key available)
+-- Wolfenstein II The Freedom Chronicles - Episode 3 (AppID: 624622)
+addappid(624622)
+addappid(762730, 1, "d9969fc225d52c22003bab9092a13b5b2b40e8b35aadd653952e68da0bc637b2") -- Wolfenstein II The Freedom Chronicles - Episode 3 - DLC3 (ROW)
+--setManifestid(762730, "447099527328778696", 3714807871)
+-- addappid(762731, 1, "MISSING_KEY") -- Wolfenstein II The Freedom Chronicles - Episode 3 - DLC3 (CERO) (no key available)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(624740) -- Wolfenstein II The Freedom Chronicles - Episode 0
+addtoken(624740, "3911098109811334779")
+addappid(650410) -- Wolfenstein II The Freedom Chronicles - Season Pass
+addappid(719980) -- Wolfenstein II Terror Billy Cosmetic Quake Champions
+addtoken(719980, "2693347029106600910")
