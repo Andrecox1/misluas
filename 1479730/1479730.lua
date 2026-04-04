f7a4db68d393a70267d0ebb92117974ffc8a0b7a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-03 22:31:26 -0500

    add 1479730/1479730.lua

--- 1479730/1479730.lua
+++ 1479730/1479730.lua
@@ -10,17 +10,17 @@
 addappid(1479730) -- Commandos: Origins
 -- MAIN APP DEPOTS
 addappid(1479731, 1, "64205c91ebfac38afeb2011798b9270dc5d1eec20ef0d4b176a42fbf5570de84") -- Depot 1479731
-setManifestid(1479731, "8653943449851790426", 34568840188)
+--setManifestid(1479731, "8653943449851790426", 34568840188)
 -- SHARED DEPOTS (from other apps)
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
-setManifestid(228989, "3514306556860204959", 39590283)
+--setManifestid(228989, "3514306556860204959", 39590283)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
-setManifestid(228990, "1829726630299308803", 102931551)
+--setManifestid(228990, "1829726630299308803", 102931551)
 -- DLCS WITH DEDICATED DEPOTS
 -- Commandos Origins - Classified Archives (AppID: 3565060)
 addappid(3565060)
 addappid(3565060, 1, "25969d0cfdf09a97590616156414c9996ebf94afe209ed4cb0eb957fa79a2c77") -- Commandos Origins - Classified Archives - Depot 3565060
-setManifestid(3565060, "3073898408723519129", 703903538)
+--setManifestid(3565060, "3073898408723519129", 703903538)
 -- DLCS WITHOUT DEDICATED DEPOTS
 addappid(3194180) -- Commandos Origins - Deluxe Edition Upgrade
 addappid(3933550) -- Commandos Origins - Shadows over Crete
