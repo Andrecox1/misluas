e9094137bedc959cf579bad9970c01c1f3e1a38f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-03 22:23:15 -0500

    add 1659420/1659420.lua

---
+++ 1659420/1659420.lua
@@ -0,0 +1,20 @@
+-- 1659420's Lua and Manifest Created by Morrenus
+-- UNCHARTED™: Legacy of Thieves Collection
+-- Created: December 06, 2025 at 01:42:02 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+-- Blacklisted Depots: 1
+--   Depot 1659425: Blacklisted
+
+-- MAIN APPLICATION
+addappid(1659420) -- UNCHARTED™: Legacy of Thieves Collection
+-- MAIN APP DEPOTS
+addappid(1659421, 1, "54f1e37db693643220de1240ebc50a0c00d4bc3a53ffdd91cf3f52cc71c91382") -- Depot 1659421
+--setManifestid(1659421, "7819112604911333886", 100173520609)
+addappid(1659422, 1, "513eb93ddc7de41caabd6dfc7f8b2f5da716cc75ca9df1df674e4e339fceada2") -- Depot 1659422
+--setManifestid(1659422, "9007195248264946670", 33102814122)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+--setManifestid(228988, "6645201662696499616", 29212173)
