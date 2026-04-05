173b7862157fa14f3fd7637c7001a8f3ab655625
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-05 15:23:40 -0500

    add 1325200/1325200.lua

---
+++ 1325200/1325200.lua
@@ -0,0 +1,22 @@
+-- 1325200's Lua and Manifest Created by Morrenus
+-- Nioh 2 – The Complete Edition
+-- Created: January 19, 2026 at 14:50:47 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1325200, 1, "8eb55cac45a68934d7f485b2757c1c49d98713faf8c1e48839ad1bf8afb6f40d") -- Nioh 2 – The Complete Edition
+-- MAIN APP DEPOTS
+addappid(1325201, 1, "20da81589e0a73461a18d3f42426068e48228b81ea86664c8b93ffdf7285762a") -- Nioh2 Common Data
+--setManifestid(1325201, "114676460224695371", 81975937290)
+addappid(1325203, 1, "9c164a5820759db569bf273ebbd44f4d5d48df4cc884899e99a9586fa7269330") -- Nioh2 Application
+--setManifestid(1325203, "7274020265141192108", 33014240)
+addappid(1325202, 1, "e644223a7b80d05032b8d93def9d0473b80c7430da7e55e3540eefbf0961d0e1") -- Nioh2 Application
+--setManifestid(1325202, "3674821973469504854", 33014240)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+--setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
