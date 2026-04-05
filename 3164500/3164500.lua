444097816cd0dae0615fd15649703e431092d559
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-05 16:11:54 -0500

    add 3164500/3164500.lua

---
+++ 3164500/3164500.lua
@@ -0,0 +1,12 @@
+-- 3164500's Lua and Manifest Created by Morrenus
+-- Schedule I
+-- Created: March 29, 2026 at 21:28:43 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3164500, 1, "dc594c390586e093182147f795ed1ea481969d347bd6a261a3bc83a1066e7fc3") -- Schedule I
+-- MAIN APP DEPOTS
+addappid(3164501, 1, "81d0de07788de885787b0519b9dabf51618be6cba9124497decf572ac96624f4") -- Depot 3164501
+--setManifestid(3164501, "3828069228120160165", 7885915850)
