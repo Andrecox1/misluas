878e63c41bad4fcb7543311d2e15df0b4b707a2e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-05 13:51:06 -0500

    add 1222140/1222140.lua

---
+++ 1222140/1222140.lua
@@ -0,0 +1,12 @@
+-- 1222140's Lua and Manifest Created by Morrenus
+-- Detroit: Become Human
+-- Created: September 29, 2025 at 07:33:07 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1222140) -- Detroit: Become Human
+-- MAIN APP DEPOTS
+addappid(1222141, 1, "d87af32872df297ec519d0e79b6e0afc327b09048c3414622606b54efc46f519") -- Detroit: Become Human Content
+--setManifestid(1222141, "7324084008489949045", 63077578235)
