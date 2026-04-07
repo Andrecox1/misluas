4ce3b33fbcd4a8dfe1ba9338477a82c1815e667d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-07 15:20:55 -0500

    add 264710/264710.lua

---
+++ 264710/264710.lua
@@ -0,0 +1,18 @@
+-- 264710's Lua and Manifest Created by Morrenus
+-- Subnautica
+-- Created: October 07, 2025 at 13:54:27 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(264710) -- Subnautica
+-- MAIN APP DEPOTS
+addappid(264712, 1, "d764b2a41e9bdbf338f04948309614ea6f8d462073a610940aaf10cbfc35aa85") -- Subnautica (Win32 Specific)
+--setManifestid(264712, "4495119477327785570", 7038266148)
+addappid(264713, 1, "6da396b527d2b570a039778566cef5c1e94ba486fefa835553a1831e40353dec") -- Subnautica (OSX Specific)
+--setManifestid(264713, "6303477024342771846", 7024278574)
+-- SHARED DEPOTS (from other apps)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+--setManifestid(228985, "3966345552745568756", 13699237)
