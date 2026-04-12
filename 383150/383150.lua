6323395eb986b11fa1529db5e39738de5764d97b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-12 10:48:44 -0500

    add 383150/383150.lua

---
+++ 383150/383150.lua
@@ -0,0 +1,32 @@
+-- 383150's Lua and Manifest Created by Morrenus
+-- Dead Island Definitive Edition
+-- Created: September 29, 2025 at 06:50:53 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 10
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(383150) -- Dead Island Definitive Edition
+-- MAIN APP DEPOTS
+addappid(383151, 1, "848765cbc4ad068b9676d33d38c720ad28c690b3aceceb467947e4564b95e9e4") -- Binaries pc
+--setManifestid(383151, "8159924524727733546", 41982465)
+addappid(383152, 1, "2bbe10937bfd3798ca64c41702a970237c6316d80544d2f8636f344bd9f0f1f5") -- Binaries linux
+--setManifestid(383152, "7318972889813881710", 78616714)
+addappid(383153, 1, "00b123ed897e65d3ed13036cbc16e867bfb0e31c14af87a2b3b35b425cef47d2") -- Data platform independent
+--setManifestid(383153, "3530186727903563836", 3091130057)
+addappid(383154, 1, "cfc88d94c4e825a0367280ca7395214ab6e6e73987ea66368b275b80a727b170") -- Data pc platform related
+--setManifestid(383154, "6391798167256944570", 5381241016)
+addappid(383155, 1, "545717ce30503c9738098d4e5450da026acc272a575fe815d87d0cbcc600f1d9") -- Data linux platform related
+--setManifestid(383155, "5936660404927479705", 5504805906)
+addappid(383156, 1, "1ef83354cda51c9e87f7901171d8ab5224fabf69e2605f550d47ed3ad1620bfd") -- Language data En
+--setManifestid(383156, "7736023019209677579", 984499115)
+addappid(383157, 1, "eed0d30f6698a7562af48e6b88cd246befc2e76aa6fba153b63b952af19f41fb") -- Language data Ru
+--setManifestid(383157, "6357865423433303878", 979679073)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+--setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+--setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
