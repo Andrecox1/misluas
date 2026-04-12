035f354676bef037e8da91a55bff24fdb575bc6b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-12 11:17:39 -0500

    add 323470/323470.lua

---
+++ 323470/323470.lua
@@ -0,0 +1,28 @@
+-- 323470's Lua and Manifest Created by Morrenus
+-- DRAGON BALL XENOVERSE
+-- Created: September 29, 2025 at 08:32:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(323470) -- DRAGON BALL XENOVERSE
+-- MAIN APP DEPOTS
+addappid(323471, 1, "a92b27f33a671ba1ff67c80c0508d6f8d276ac2936d558fba95d8dc83e5b0765") -- DRAGONBALL XENOVERSE Content
+--setManifestid(323471, "3461626906485904206", 10366975704)
+addappid(323473, 1, "a9fc74b57e30b5d14ca436ebad3a4136d68941ba504102597afbb6410ec4c983") -- DRAGON BALL XENOVERSE compatibility pack
+--setManifestid(323473, "2822964839275909432", 427087667)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+--setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(336800) -- DRAGON BALL XENOVERSE Pre-order DLC
+addappid(344460) -- DRAGON BALL XENOVERSE Season Pass
+addtoken(344460, "13138949577396548177")
+addappid(344461) -- DRAGON BALL XENOVERSE GT Pack 1
+addappid(344462) -- DRAGON BALL XENOVERSE GT Pack 2
+addappid(344463) -- DRAGON BALL XENOVERSE Resurrection F pack
+addappid(362600) -- DRAGON BALL XENOVERSE MOVIE COSTUMES PACK
