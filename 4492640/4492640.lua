c2157f324d17fc5f5479e895f7525dffa9efb746
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:58 -0500

    add 4492640/4492640.lua

---
+++ 4492640/4492640.lua
@@ -0,0 +1,14 @@
+-- 4492640's Lua and Manifest Created by Morrenus
+-- Dosa Divas Soundtrack
+-- Created: April 14, 2026 at 12:46:28 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4492640) -- Dosa Divas Soundtrack
+-- MAIN APP DEPOTS
+addappid(4492641, 1, "44824fe84b352455a0c31a356a457a181240d1ac5c55939daa0444187c3933c1") -- Depot 4492641
+setManifestid(4492641, "7079879969082636590", 79749089)
+addappid(4492642, 1, "c364431de4cddb3752e2a81461bc10fb006fe306b5873365f7720723bac0e947") -- Depot 4492642
+setManifestid(4492642, "4301878714143424742", 1146711952)
