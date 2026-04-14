dcc85e45aed0c513f81e573169de5fcbd77818fa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:36:05 -0500

    add 3298520/3298520.lua

---
+++ 3298520/3298520.lua
@@ -0,0 +1,17 @@
+-- 3298520's Lua and Manifest Created by Morrenus
+-- Idle Iktah
+-- Created: April 14, 2026 at 15:02:44 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3298520) -- Idle Iktah
+-- MAIN APP DEPOTS
+addappid(3298521, 1, "d3dea1960a8dc68afb5d37d0af0e70d03dfaac6047b1771bb35722b56237c8a4") -- Depot 3298521
+setManifestid(3298521, "5782649875326566996", 74084030)
+addappid(3298522, 1, "c43849ec61f6b2516cf1cd759978a02589caa3bcd8c6a7b49f8b8d2c1b84c532") -- Depot 3298522
+setManifestid(3298522, "3702907694926904331", 897471802)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4221180) -- Idle Iktah - Golden Sunstone Status (unreleased)
