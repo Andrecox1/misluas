e331f5f2666855ffbe80c6ea51d4fa6a4f8b17a8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:41 -0500

    add 4280900/4280900.lua

---
+++ 4280900/4280900.lua
@@ -0,0 +1,14 @@
+-- 4280900's Lua and Manifest Created by Morrenus
+-- Warhammer 40,000: Rites of War (Classic)
+-- Created: April 14, 2026 at 12:35:41 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4280900) -- Warhammer 40,000: Rites of War (Classic)
+-- MAIN APP DEPOTS
+addappid(4280901, 1, "c2c5092231c40eaad7d785af4b85be6616aef07dbe97cf725cd619f209da63e3") -- Depot 4280901
+setManifestid(4280901, "6892300880419078161", 580583128)
+addappid(4280902, 1, "80737886f53e3ad1ae0f721c4453ab6764a19fa6e3ac958058278fa8149c29b6") -- Depot 4280902
+setManifestid(4280902, "5649475151094317671", 585874530)
