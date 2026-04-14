11b32f0b2f43a1f10892ce386096351809599f46
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:35:47 -0500

    add 2950710/2950710.lua

---
+++ 2950710/2950710.lua
@@ -0,0 +1,12 @@
+-- 2950710's Lua and Manifest Created by Morrenus
+-- Dosa Divas
+-- Created: April 14, 2026 at 12:46:11 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2950710) -- Dosa Divas
+-- MAIN APP DEPOTS
+addappid(2950711, 1, "98a02c002b571bc9c38cb654c3342dd24679b0cf6b10c6de851b272892c8df0f") -- Depot 2950711
+setManifestid(2950711, "5570815379642453148", 1571799901)
