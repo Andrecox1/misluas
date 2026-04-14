3fa9e57a2200ea37425d7d42f9f6795e1d663250
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:26 -0500

    add 4280870/4280870.lua

---
+++ 4280870/4280870.lua
@@ -0,0 +1,18 @@
+-- 4280870's Lua and Manifest Created by Morrenus
+-- Warhammer: Shadow of the Horned Rat (Classic)
+-- Created: April 14, 2026 at 12:35:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4280870) -- Warhammer: Shadow of the Horned Rat (Classic)
+-- MAIN APP DEPOTS
+addappid(4280871, 1, "2cd76303ad3f1a44c63c4e43619f4913946b58bab952c62f4e31736c350502fa") -- Depot 4280871
+setManifestid(4280871, "3031278875912248294", 429050617)
+addappid(4280872, 1, "2f600d27c3d4c9fb5ab488e6ce1de5e8dcbf066d70d274c39a5ee73a3a180f6c") -- Depot 4280872
+setManifestid(4280872, "5938282524926222337", 465263237)
+addappid(4280873, 1, "3c29e8df767af9ed1aea611987fbde74a8e0bf9ca73bf40968e967ebcf5d0ee7") -- Depot 4280873
+setManifestid(4280873, "1090518298382564759", 516551467)
+addappid(4280874, 1, "44d307f3cf5b44a509656b1aa4bf90d4801035106b8ad63ffe21d115b746ce6d") -- Depot 4280874
+setManifestid(4280874, "2801481789649577425", 463619891)
