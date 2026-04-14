0eca2fc0cba75b43d2a1228d4f195eb71669b3f9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:20 -0500

    add 4280860/4280860.lua

---
+++ 4280860/4280860.lua
@@ -0,0 +1,14 @@
+﻿-- 4280860's Lua and Manifest Created by Morrenus
+-- Warhammer: Dark Omen (Classic)
+-- Created: April 14, 2026 at 12:35:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4280860, 1, "027d32a02fd9cf1c68a16a665cfa2d0d87c164f14bee6fd056c56bf17ad64973") -- Warhammer: Dark Omen (Classic)
+-- MAIN APP DEPOTS
+addappid(4280861, 1, "4d80b09da81050b886390238e0239a43cb291bc544e4de4ec5d485a750f976c8") -- Depot 4280861
+addappid(4280862, 1, "f536e281d505ae780c9d036604ed176fa079b58f12bd4b7420b7896b259ebb2a") -- Depot 4280862
+addappid(4280863, 1, "4d8a5d3ffca7d0201faae1cb3cd5fe3ef07519f8ac76732a0fe4d517c8c3e3d3") -- Depot 4280863
+addappid(4280864, 1, "c8deb13c7dd7569812fb3ee8f3995f6c337cbc42f7fb1173bd5214210c4d4458") -- Depot 4280864
