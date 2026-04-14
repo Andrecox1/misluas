d6a359ec3abef3aabb5a9cf1049bcb515e65ffb3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:35:55 -0500

    add 3003690/3003690.lua

---
+++ 3003690/3003690.lua
@@ -0,0 +1,12 @@
+-- 3003690's Lua and Manifest Created by Morrenus
+-- Lofi Haven
+-- Created: April 14, 2026 at 15:26:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3003690) -- Lofi Haven
+-- MAIN APP DEPOTS
+addappid(3003691, 1, "decdfae905e41bf4504c8e9ef75ad0073e60d9c1bb4ca8ecce69877a4637e254") -- Depot 3003691
+setManifestid(3003691, "977965329854462468", 4848884200)
