6bdfa919e4e2f81b5ff6df6152218935ebcafc59
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:49 -0500

    add 4280920/4280920.lua

---
+++ 4280920/4280920.lua
@@ -0,0 +1,12 @@
+-- 4280920's Lua and Manifest Created by Morrenus
+-- Final Liberation: Warhammer Epic 40,000 (Classic)
+-- Created: April 14, 2026 at 12:35:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4280920) -- Final Liberation: Warhammer Epic 40,000 (Classic)
+-- MAIN APP DEPOTS
+addappid(4280921, 1, "05d757eb7c43fb965c589b348592f03d36dcd2049eb5731531427d879cb3afed") -- Depot 4280921
+setManifestid(4280921, "1331552488536982571", 677983138)
