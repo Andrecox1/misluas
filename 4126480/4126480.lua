2a8803472179956d63e4f6b95422261d788a3f47
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:09 -0500

    add 4126480/4126480.lua

---
+++ 4126480/4126480.lua
@@ -0,0 +1,12 @@
+-- 4126480's Lua and Manifest Created by Morrenus
+-- Fishing Inc
+-- Created: April 14, 2026 at 15:10:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4126480) -- Fishing Inc
+-- MAIN APP DEPOTS
+addappid(4126481, 1, "053157d632c043702a7e643403d24b829619723e29fc09cd0431cb939ed5ff42") -- Depot 4126481
+setManifestid(4126481, "4387161982448400373", 255269047)
