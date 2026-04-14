9773c8cb208eeb090d0763596a966a46032afe1f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:35:23 -0500

    add 2620140/2620140.lua

---
+++ 2620140/2620140.lua
@@ -0,0 +1,12 @@
+-- 2620140's Lua and Manifest Created by Morrenus
+-- AVARIS3
+-- Created: April 14, 2026 at 12:45:56 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2620140, 1, "339f816cd0482fa0487a13bb76b64df8a84346953eb5d7aba2ad0b8b743f492a") -- AVARIS3
+-- MAIN APP DEPOTS
+addappid(2620141, 1, "9a77a43e567d23eaf6061a4c44a0d417656f3c34a68fa6e3d60676b841688665") -- Depot 2620141
+setManifestid(2620141, "8901247452763915934", 120303761)
