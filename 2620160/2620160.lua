6bf8c654004224a9fb45213787c11ff617a5e488
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:35:39 -0500

    add 2620160/2620160.lua

---
+++ 2620160/2620160.lua
@@ -0,0 +1,12 @@
+-- 2620160's Lua and Manifest Created by Morrenus
+-- AVARIS3 Soundtrack
+-- Created: April 14, 2026 at 12:46:02 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2620160) -- AVARIS3 Soundtrack
+-- MAIN APP DEPOTS
+addappid(2620161, 1, "951bcebda8f337c2dd88fe8b667760f8753c8ca87ddab936dab120d2d5f5af28") -- Depot 2620161
+setManifestid(2620161, "1759737086003171981", 130086438)
