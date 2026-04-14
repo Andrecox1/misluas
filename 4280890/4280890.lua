c21f97db58462eb497efcb113ca9ae5b02fc14a3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:34 -0500

    add 4280890/4280890.lua

---
+++ 4280890/4280890.lua
@@ -0,0 +1,12 @@
+-- 4280890's Lua and Manifest Created by Morrenus
+-- Warhammer 40,000: Chaos Gate (Classic)
+-- Created: April 14, 2026 at 12:35:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4280890) -- Warhammer 40,000: Chaos Gate (Classic)
+-- MAIN APP DEPOTS
+addappid(4280891, 1, "3ce6d561df5af94d8b3ac615efff33aaf86d4a772fbe6be1be30af05f9425242") -- Depot 4280891
+setManifestid(4280891, "2318123389514246880", 726353431)
