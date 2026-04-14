0b5504ce1b12b4685fd1c464fb5970c3ab782e4f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 14:37:03 -0500

    add 3868490/3868490.lua

---
+++ 3868490/3868490.lua
@@ -0,0 +1,12 @@
+-- 3868490's Lua and Manifest Created by Morrenus
+-- Marshals of Yore
+-- Created: April 14, 2026 at 12:46:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3868490) -- Marshals of Yore
+-- MAIN APP DEPOTS
+addappid(3868491, 1, "f2a17fa4c2e36e11c3d9a0ca287dc985abee0c315926539f9d97a90935ccebfb") -- Depot 3868491
+setManifestid(3868491, "4373001076748956281", 470609161)
