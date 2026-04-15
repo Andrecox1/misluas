7daabc470964064f4e4ca855c33649ebb311343a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:02:46 -0500

    add 2906070/2906070.lua

---
+++ 2906070/2906070.lua
@@ -0,0 +1,11 @@
+﻿-- 2906070's Lua and Manifest Created by Hubcap Manifest
+-- Blood Reaver
+-- Created: April 15, 2026 at 11:48:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2906070) -- Blood Reaver
+-- MAIN APP DEPOTS
+addappid(2906073, 1, "6f727f6c41798f7115aab41c4022e1f1f43f70482cdbbb6b266ea259b21d804e") -- Depot 2906073
