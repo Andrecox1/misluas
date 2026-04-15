7416e84878e3b7ba27bc9c2a8b153a18496c8fc8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:07:56 -0500

    add 4338370/4338370.lua

---
+++ 4338370/4338370.lua
@@ -0,0 +1,14 @@
+-- 4338370's Lua and Manifest Created by Morrenus
+-- Regions of Ruin: Runegate Original Soundtrack
+-- Created: April 14, 2026 at 22:29:47 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4338370) -- Regions of Ruin: Runegate Original Soundtrack
+-- MAIN APP DEPOTS
+addappid(4338371, 1, "d2a6d10bc9ed75e5e77fefee8950328b73f303ca98bec80e7fdfa1a39ac7042e") -- Depot 4338371
+setManifestid(4338371, "4643517060216401116", 180714078)
+addappid(4338372, 1, "f1df03b6ccf680843bbf8e86b02ba81d516ce433ab08c61993e415a4300b9b1d") -- Depot 4338372
+setManifestid(4338372, "5390269076734523245", 328013763)
