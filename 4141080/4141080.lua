443288ec69110e82a06d174fde529326849939f5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:07:55 -0500

    add 4141080/4141080.lua

---
+++ 4141080/4141080.lua
@@ -0,0 +1,11 @@
+﻿-- 4141080's Lua and Manifest Created by Hubcap Manifest
+-- A Game About Making Mosaics
+-- Created: April 15, 2026 at 10:34:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4141080) -- A Game About Making Mosaics
+-- MAIN APP DEPOTS
+addappid(4141081, 1, "6a88dc69bead521be2b572270712195d885e410aaba439c3de8f1892fe49323f") -- Depot 4141081
