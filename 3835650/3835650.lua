ef7ecadbce8c7e94f176d3929c5764738ccb50b4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:03:42 -0500

    add 3835650/3835650.lua

---
+++ 3835650/3835650.lua
@@ -0,0 +1,24 @@
+-- 3835650's Lua and Manifest Created by Hubcap Manifest
+-- Cleaning Up!
+-- Created: April 15, 2026 at 18:00:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 6
+-- Total DLCs: 0
+-- Shared Depots: 5
+
+-- MAIN APPLICATION
+addappid(3835650) -- Cleaning Up!
+-- MAIN APP DEPOTS
+addappid(3835651, 1, "56a6ab2ae5e531ece70dfd4de2cd12c8ffea02d161a4886be9796457606e28ea") -- Depot 3835651
+setManifestid(3835651, "4318460415365938546", 459610029)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
