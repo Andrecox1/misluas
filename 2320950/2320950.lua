09931ac7d80901f4719bae75d9ea555696b9f840
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:06:52 -0500

    add 2320950/2320950.lua

---
+++ 2320950/2320950.lua
@@ -0,0 +1,13 @@
+-- 2320950's Lua and Manifest Created by Morrenus
+-- Last Epoch Multiplayer Beta Playtest
+-- Created: April 14, 2026 at 22:31:08 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2320950) -- Last Epoch Multiplayer Beta Playtest
+addtoken(2320950, "12534876620171910583")
+-- MAIN APP DEPOTS
+addappid(2320951, 1, "ae27d5d6157723a7bff2950eff7d0ff8338ba9b275f87833bcc063264e670f19") -- Depot 2320951
+setManifestid(2320951, "6775359867154209927", 23129126084)
