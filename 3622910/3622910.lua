74cf832d65da0e7bfb506b9186a411bf9a40db45
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:07:41 -0500

    add 3622910/3622910.lua

---
+++ 3622910/3622910.lua
@@ -0,0 +1,12 @@
+-- 3622910's Lua and Manifest Created by Morrenus
+-- PAW ALLIANCE
+-- Created: April 14, 2026 at 22:29:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3622910) -- PAW ALLIANCE
+-- MAIN APP DEPOTS
+addappid(3622911, 1, "182127f877b7875e273fa609b022bbde1e5e8ce6b9650d8ae56fdff8ed82f189") -- Depot 3622911
+setManifestid(3622911, "3838363560433298264", 1757071226)
