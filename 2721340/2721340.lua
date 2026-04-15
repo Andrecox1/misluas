9b1556f17420b8a306de0478d30f04a0d5f67272
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:01:43 -0500

    add 2721340/2721340.lua

---
+++ 2721340/2721340.lua
@@ -0,0 +1,21 @@
+-- 2721340's Lua and Manifest Created by Hubcap Manifest
+-- Last Flag
+-- Created: April 15, 2026 at 16:50:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2721340) -- Last Flag
+addtoken(2721340, "4430615586680372850")
+-- MAIN APP DEPOTS
+addappid(2721341, 1, "80a8ff5f2a55eabfc974f7277468b02c74a0824ac626196709dc2077ca6b55e1") -- Depot 2721341
+setManifestid(2721341, "720731318608869799", 4689320542)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4323640) -- Last Flag - Upgrade to Supporter Edition
