25a21d1175b6cce9f6e5a883215b0ae8ec669c96
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:07:36 -0500

    add 3516780/3516780.lua

---
+++ 3516780/3516780.lua
@@ -0,0 +1,18 @@
+-- 3516780's Lua and Manifest Created by Morrenus
+-- 2nd EVE
+-- Created: April 14, 2026 at 22:29:42 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3516780) -- 2nd EVE
+-- MAIN APP DEPOTS
+addappid(3516781, 1, "7b353eabcb8e30f8234120b119011f524cb2bc9885da12e89543a20fe3c8d87c") -- Depot 3516781
+setManifestid(3516781, "648394138684726058", 2713130842)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
