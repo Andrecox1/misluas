ec7a737fa4f2ab96c74c4cdedb14a19169edfe37
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:09:22 -0500

    add 4553840/4553840.lua

---
+++ 4553840/4553840.lua
@@ -0,0 +1,12 @@
+-- 4553840's Lua and Manifest Created by Morrenus
+-- EraPuzzle: Grid & Glory
+-- Created: April 14, 2026 at 22:29:16 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4553840) -- EraPuzzle: Grid & Glory
+-- MAIN APP DEPOTS
+addappid(4553841, 1, "f8d4c8bafeedbab13677f7c183bd5f326a96cf557e692439adecfaf5b041e9f7") -- Depot 4553841
+setManifestid(4553841, "7303673032872527520", 198307607)
