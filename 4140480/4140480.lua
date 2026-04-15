ac0dcdff9eeca94eac238374173ae9832eca3818
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:07:22 -0500

    add 4140480/4140480.lua

---
+++ 4140480/4140480.lua
@@ -0,0 +1,14 @@
+-- 4140480's Lua and Manifest Created by Hubcap Manifest
+-- Neon Inferno Original Soundtrack
+-- Created: April 15, 2026 at 10:57:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4140480) -- Neon Inferno Original Soundtrack
+-- MAIN APP DEPOTS
+addappid(4140481, 1, "5c26be2a35772f99754397c823888c1ad03fd3937e88db80bff75fd724c4d5e0") -- Depot 4140481
+setManifestid(4140481, "1063697600300293639", 212896122)
+addappid(4140482, 1, "94d00d648fd9d05f742fa15ad53aaa069b929567419a37fff32414721cd9d659") -- Depot 4140482
+setManifestid(4140482, "2094673088692181805", 674379655)
