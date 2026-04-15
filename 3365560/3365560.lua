3e548b65896a480c821274a545cbc6e99093743c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:03:11 -0500

    add 3365560/3365560.lua

---
+++ 3365560/3365560.lua
@@ -0,0 +1,14 @@
+-- 3365560's Lua and Manifest Created by Hubcap Manifest
+-- Fireside Fables
+-- Created: April 15, 2026 at 14:42:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3365560) -- Fireside Fables
+-- DLCS WITH DEDICATED DEPOTS
+-- Fireside Fables Artbook (AppID: 4506610)
+addappid(4506610)
+addappid(4506610, 1, "27116cbd93eb8a2d2f8c047ee7c96fb40d3936c735e4c4f58e63908443f05bc5") -- Fireside Fables Artbook - Depot 4506610
+setManifestid(4506610, "3837018783665058789", 35940530)
