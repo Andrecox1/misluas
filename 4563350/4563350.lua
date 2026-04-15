6f456823479b84412456d8868f3f4c551991a56d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:09:38 -0500

    add 4563350/4563350.lua

---
+++ 4563350/4563350.lua
@@ -0,0 +1,13 @@
+-- 4563350's Lua and Manifest Created by Morrenus
+-- 暗影地牢 Demo
+-- Created: April 14, 2026 at 22:29:36 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4563350) -- 暗影地牢 Demo
+addtoken(4563350, "3032252196862899768")
+-- MAIN APP DEPOTS
+addappid(4563351, 1, "c1458c9f210dcf70e61df3a41205c28b92bb7504ab31ee19afe00b1d5facd31d") -- Depot 4563351
+setManifestid(4563351, "8401778910667282335", 5862888763)
