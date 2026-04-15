d756b7f399830666372796457010cca7289800a4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:04:18 -0500

    add 4527930/4527930.lua

---
+++ 4527930/4527930.lua
@@ -0,0 +1,12 @@
+-- 4527930's Lua and Manifest Created by Hubcap Manifest
+-- Idle Deepcore
+-- Created: April 15, 2026 at 11:54:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4527930) -- Idle Deepcore
+-- MAIN APP DEPOTS
+addappid(4527931, 1, "9486c39b95c7ed469f8a1e1c0a1f26c5393fe434eadf307b5e887bdffb141aaf") -- Depot 4527931
+setManifestid(4527931, "7895645941910239687", 133558639)
