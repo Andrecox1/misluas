37c2eb5c14b32b76486ca6ef43d7b150a1a807fa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:03:31 -0500

    add 3811840/3811840.lua

---
+++ 3811840/3811840.lua
@@ -0,0 +1,14 @@
+-- 3811840's Lua and Manifest Created by Hubcap Manifest
+-- Simultree
+-- Created: April 15, 2026 at 12:43:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3811840) -- Simultree
+-- MAIN APP DEPOTS
+addappid(3811841, 1, "4ce4d353cf1908e926e711e02096f3868254338e5a142b7d91823574062edfa0") -- Depot 3811841
+setManifestid(3811841, "3724049145882901939", 305168788)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4619680) -- Simultree - Supporter Pack
