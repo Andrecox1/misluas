8409d511c1be2b86566bd2013ad39076767fd246
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:07:12 -0500

    add 3107050/3107050.lua

---
+++ 3107050/3107050.lua
@@ -0,0 +1,11 @@
+﻿-- 3107050's Lua and Manifest Created by Morrenus
+-- Stoplights
+-- Created: April 14, 2026 at 22:29:12 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3107050) -- Stoplights
+-- MAIN APP DEPOTS
+addappid(3107051, 1, "6070d903bd2438aa271adbf20c2e219c366e1a98ef7a3a1e0253ebd92cb0b66e") -- Depot 3107051
