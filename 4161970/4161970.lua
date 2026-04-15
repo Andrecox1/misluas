52673a8ef61d826ea78415fc06ea8204fb6e0274
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:07:43 -0500

    add 4161970/4161970.lua

---
+++ 4161970/4161970.lua
@@ -0,0 +1,12 @@
+-- 4161970's Lua and Manifest Created by Hubcap Manifest
+-- Let It Consume
+-- Created: April 15, 2026 at 11:22:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4161970) -- Let It Consume
+-- MAIN APP DEPOTS
+addappid(4161971, 1, "5a7f047fe57569c87d762d4b95dabea73f04519221ac6ca3a576d2876591e074") -- Depot 4161971
+setManifestid(4161971, "6582972890387257077", 137082184)
