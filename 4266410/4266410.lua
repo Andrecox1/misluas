ccba92702542977f536c2eb6d7346fc1ba9efdd8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:03:49 -0500

    add 4266410/4266410.lua

---
+++ 4266410/4266410.lua
@@ -0,0 +1,12 @@
+-- 4266410's Lua and Manifest Created by Hubcap Manifest
+-- LucidVR Video Player
+-- Created: April 15, 2026 at 12:56:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4266410) -- LucidVR Video Player
+-- MAIN APP DEPOTS
+addappid(4266411, 1, "fb280c6bf14b16acd5e76a82b0074004f432e883d995616bdcd50b937db965e4") -- Depot 4266411
+setManifestid(4266411, "5590031505561999994", 220212112)
