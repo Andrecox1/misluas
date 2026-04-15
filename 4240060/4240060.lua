42d5e0b7245ae13e77c4348ace304d52c1fb02ee
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:07:48 -0500

    add 4240060/4240060.lua

---
+++ 4240060/4240060.lua
@@ -0,0 +1,14 @@
+-- 4240060's Lua and Manifest Created by Morrenus
+-- Nimillion - The last expedition Soundtrack
+-- Created: April 14, 2026 at 22:30:34 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4240060) -- Nimillion - The last expedition Soundtrack
+-- MAIN APP DEPOTS
+addappid(4240061, 1, "92ff8848375b75097a597391955ea8e5b11aec4e2f9d08ed3d0a53394f9c9b62") -- Depot 4240061
+setManifestid(4240061, "7526050273738966739", 87428148)
+addappid(4240062, 1, "04a7fbe05400c942a64506b9092615f472fbd425a6710a4c05bc7790e5dceae7") -- Depot 4240062
+setManifestid(4240062, "3887872742879222043", 153479778)
