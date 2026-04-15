c5b73e5eb42099fc28e17a797b7e39ffeb475d73
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:08:00 -0500

    add 4394620/4394620.lua

---
+++ 4394620/4394620.lua
@@ -0,0 +1,12 @@
+-- 4394620's Lua and Manifest Created by Hubcap Manifest
+-- Historias de un Golpe: Los Tres Hermanos
+-- Created: April 15, 2026 at 10:34:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4394620, 1, "11a522f2203f6e99e81a35e32f41321219ccfac4418fe6325a90dccaa9ba88d6") -- Historias de un Golpe: Los Tres Hermanos
+-- MAIN APP DEPOTS
+addappid(4394621, 1, "89f0dd2e95b36a3de38ddc5177b5de2ebcff79158232c9492db9e8c6aa5bc83f") -- Depot 4394621
+setManifestid(4394621, "2314178660739560009", 361204799)
