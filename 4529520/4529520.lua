bbf13fdba743f41735d4e894790998c48a6655ee
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:08:16 -0500

    add 4529520/4529520.lua

---
+++ 4529520/4529520.lua
@@ -0,0 +1,12 @@
+-- 4529520's Lua and Manifest Created by Hubcap Manifest
+-- 30 minutes to rescue
+-- Created: April 15, 2026 at 10:34:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4529520) -- 30 minutes to rescue
+-- MAIN APP DEPOTS
+addappid(4529521, 1, "c093a660b29a250571abd03bf19e2adceb8facb50d6659af63d73c09977966d0") -- Depot 4529521
+setManifestid(4529521, "9114422898206187934", 352247096)
