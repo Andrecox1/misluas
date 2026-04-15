e2d2bf122328141e0d4b1c905d7d7630f299f8b8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:07:01 -0500

    add 2889880/2889880.lua

---
+++ 2889880/2889880.lua
@@ -0,0 +1,18 @@
+-- 2889880's Lua and Manifest Created by Morrenus
+-- Nimillion - The last expedition
+-- Created: April 14, 2026 at 22:29:58 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2889880) -- Nimillion - The last expedition
+-- MAIN APP DEPOTS
+addappid(2889881, 1, "cb7e4b6a80478b32e9a8d9a0ac691674ed0b05b0ae11b3c413b1e87e00ac4e8c") -- Depot 2889881
+setManifestid(2889881, "6653539201781692983", 1277426415)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
