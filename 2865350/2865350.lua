fa25e4ed8f06ba0f3254ed698dfb9c73ded06455
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:01:59 -0500

    add 2865350/2865350.lua

---
+++ 2865350/2865350.lua
@@ -0,0 +1,12 @@
+-- 2865350's Lua and Manifest Created by Hubcap Manifest
+-- ZOMBIECAM
+-- Created: April 15, 2026 at 12:55:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2865350) -- ZOMBIECAM
+-- MAIN APP DEPOTS
+addappid(2865351, 1, "4b627b5285effb9bb122c6cb74eb178ebedaea114bedc49deb01a88fb9f265de") -- Depot 2865351
+setManifestid(2865351, "4037643934990513012", 21804243171)
