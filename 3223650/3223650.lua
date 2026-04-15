1d98224f958828144e774c1e321b33f4a38f932b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:07:19 -0500

    add 3223650/3223650.lua

---
+++ 3223650/3223650.lua
@@ -0,0 +1,12 @@
+-- 3223650's Lua and Manifest Created by Morrenus
+-- Trapped In The Omniverse
+-- Created: April 14, 2026 at 22:30:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3223650) -- Trapped In The Omniverse
+-- MAIN APP DEPOTS
+addappid(3223651, 1, "04048567eca5e2fe6a7083f6ff5fcb613232eaa87703c992c306fe032aac10b3") -- Depot 3223651
+setManifestid(3223651, "9051397094881112591", 21189346010)
