2b1a2e02773f9d03f90d2556532ebc7664c6ed0d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-15 17:03:25 -0500

    add 3443820/3443820.lua

---
+++ 3443820/3443820.lua
@@ -0,0 +1,12 @@
+-- 3443820's Lua and Manifest Created by Hubcap Manifest
+-- BUNNY GARDEN 2
+-- Created: April 15, 2026 at 11:49:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3443820) -- BUNNY GARDEN 2
+-- MAIN APP DEPOTS
+addappid(3443821, 1, "cd4522685c82ddfb0db51c80f7d06233990b514b45388972c143903d2e7c5544") -- Depot 3443821
+setManifestid(3443821, "5000899916532489807", 4773164858)
