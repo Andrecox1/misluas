10bdf045204e1bc9516dc5faeecd1d9fc3a6bee0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-14 23:09:29 -0500

    add 4544030/4544030.lua

---
+++ 4544030/4544030.lua
@@ -0,0 +1,12 @@
+-- 4544030's Lua and Manifest Created by Morrenus
+-- I'm blocked
+-- Created: April 14, 2026 at 22:30:23 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4544030) -- I'm blocked
+-- MAIN APP DEPOTS
+addappid(4544031, 1, "e188b0b2ea1accb7865c39f4df1f0f4838778e9d03c8a3675c3947e49f007d99") -- Depot 4544031
+setManifestid(4544031, "1999676342442284908", 530409178)
