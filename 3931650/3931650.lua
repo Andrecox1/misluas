2246d484fdc673cd53bc857f9a9551458ec9635e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-16 14:34:29 -0500

    add 3931650/3931650.lua

---
+++ 3931650/3931650.lua
@@ -0,0 +1,12 @@
+-- 3931650's Lua and Manifest Created by Hubcap Manifest
+-- Switchboard
+-- Created: April 16, 2026 at 11:49:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3931650, 1, "66db37e24228ff5b026bcaceb4a94ad86bb22092260d5033098569097c99802a") -- Switchboard
+-- MAIN APP DEPOTS
+addappid(3931651, 1, "099b3e0a8590191458fc27613d5a504d05a6da1cb9ada722c15c085eb8169741") -- Depot 3931651
+setManifestid(3931651, "6004451213970192203", 780938862)
