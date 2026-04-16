394ce96c56872261b1d166423837577216e22d18
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-16 14:34:05 -0500

    add 2627690/2627690.lua

---
+++ 2627690/2627690.lua
@@ -0,0 +1,12 @@
+-- 2627690's Lua and Manifest Created by Hubcap Manifest
+-- G-MODEアーカイブス+ DIGITAL DEVIL SAGA アバタール・チューナー A's TEST Server完全版
+-- Created: April 16, 2026 at 11:12:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2627690) -- G-MODEアーカイブス+ DIGITAL DEVIL SAGA アバタール・チューナー A's TEST Server完全版
+-- MAIN APP DEPOTS
+addappid(2627691, 1, "eb2e947012e0f2869d603e227222ca425f4802b7a1be7c275502d3913048c844") -- Depot 2627691
+setManifestid(2627691, "782397482585548783", 163731587)
