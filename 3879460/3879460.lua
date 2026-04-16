04f4877629e405f488a231ba62f9f64eb7da6cdb
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-16 14:34:14 -0500

    add 3879460/3879460.lua

---
+++ 3879460/3879460.lua
@@ -0,0 +1,14 @@
+-- 3879460's Lua and Manifest Created by Hubcap Manifest
+-- MOUSE: P.I. For Hire Original Game Soundtrack
+-- Created: April 16, 2026 at 10:55:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3879460) -- MOUSE: P.I. For Hire Original Game Soundtrack
+-- MAIN APP DEPOTS
+addappid(3879461, 1, "b3e7c788fbda957f417eb83889b9931b8e88982efbef7d580d060990b04214e4") -- Depot 3879461
+setManifestid(3879461, "6090253963428527198", 123594357)
+addappid(3879462, 1, "bc2416beedd708c794eb9ef31d7bba8563e05091a90552126236d653d12fd605") -- Depot 3879462
+setManifestid(3879462, "3114757053344046138", 522781880)
