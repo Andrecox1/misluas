fc24c66a8e13d12b17209824162a76b366853719
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-16 14:35:08 -0500

    add 4384110/4384110.lua

---
+++ 4384110/4384110.lua
@@ -0,0 +1,18 @@
+-- 4384110's Lua and Manifest Created by Hubcap Manifest
+-- False Exit
+-- Created: April 16, 2026 at 10:09:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4384110) -- False Exit
+-- MAIN APP DEPOTS
+addappid(4384111, 1, "31556ca5a3563cb4f4e0ea444ed209fda9de2ad5da861fd5f6c8241a23eefdce") -- Depot 4384111
+setManifestid(4384111, "2843693584268916346", 1503808576)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
