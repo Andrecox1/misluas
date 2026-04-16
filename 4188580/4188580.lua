f945193ade5eec6a41d2d30473d5aa5314cf24b7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-16 14:34:54 -0500

    add 4188580/4188580.lua

---
+++ 4188580/4188580.lua
@@ -0,0 +1,26 @@
+-- 4188580's Lua and Manifest Created by Hubcap Manifest
+-- ImpossiBall
+-- Created: April 16, 2026 at 11:46:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 0
+-- Shared Depots: 6
+
+-- MAIN APPLICATION
+addappid(4188580) -- ImpossiBall
+-- MAIN APP DEPOTS
+addappid(4188581, 1, "36e570e5c18913daa350a6627f1ae68add5f5bf9622f65c9826907337577fc5d") -- Depot 4188581
+setManifestid(4188581, "6365950770246621290", 653967178)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229030, 1, "92f0f88867c8d5f351090eb2d27aeb50e2b621ae8a34708265d2c20c36fbcf5e") -- PhysX System Software 8.09.04 (Shared from App 228980)
+setManifestid(229030, "1043465440436835055", 51790718)
+addappid(229031, 1, "98d01f1eaa90ae08f978994e043bcbbf1f23fc72a325528ca7d722c827c0eb54") -- PhysX System Software 9.12.1031 (Shared from App 228980)
+setManifestid(229031, "7746630274301172884", 26729083)
+addappid(229032, 1, "15b3308d4e95d1156182211bab1c823b6df0a06fd74a5c0833abed89bc307cfb") -- PhysX System Software 9.13.1220 (Shared from App 228980)
+setManifestid(229032, "3616495131483866412", 41178235)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
