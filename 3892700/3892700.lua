bd3342a0f84a9bd9cccc9d9ae038bd0adbd1d9be
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-16 14:34:22 -0500

    add 3892700/3892700.lua

---
+++ 3892700/3892700.lua
@@ -0,0 +1,11 @@
+﻿-- 3892700's Lua and Manifest Created by Hubcap Manifest
+-- Verse Bound
+-- Created: April 16, 2026 at 09:56:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3892700) -- Verse Bound
+-- MAIN APP DEPOTS
+addappid(3892701, 1, "d925d159259fd0cd8b5eb28eb4b0d4904a4196f900d9312f0deba899c41c7a74") -- Depot 3892701
