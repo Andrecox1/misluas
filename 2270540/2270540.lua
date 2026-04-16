3672edfce3160e05aa27f9110cd9ab742efcaaa1
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-16 14:33:43 -0500

    add 2270540/2270540.lua

---
+++ 2270540/2270540.lua
@@ -0,0 +1,12 @@
+-- 2270540's Lua and Manifest Created by Hubcap Manifest
+-- Fun with Ragdolls Plus
+-- Created: April 16, 2026 at 13:31:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2270540) -- Fun with Ragdolls Plus
+-- MAIN APP DEPOTS
+addappid(2270543, 1, "fa90840dc6ba3f5fec5dddd2d6ba0eb08917d873f4bab82f2b2b78fbe8038c0c") -- Depot 2270543
+setManifestid(2270543, "2714626489516061949", 3717720208)
