677721b0503a23ca75352878bcae9cab4c798687
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:19:52 -0500

    add 4024760/4024760.lua

---
+++ 4024760/4024760.lua
@@ -0,0 +1,14 @@
+-- 4024760's Lua and Manifest Created by Hubcap Manifest
+-- Mind Diver Soundtrack
+-- Created: April 17, 2026 at 06:41:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4024760) -- Mind Diver Soundtrack
+-- MAIN APP DEPOTS
+addappid(4024761, 1, "e36494dbdcb144ae5c909b237102a7ea39e91957367d87b7a3913c3e5581b84b") -- Depot 4024761
+setManifestid(4024761, "6972946073204174376", 43576336)
+addappid(4024762, 1, "2d48c9598922107460f3d35a6ceebc42fbb6f9884c79702409e181b8d3a28c92") -- Depot 4024762
+setManifestid(4024762, "7327454315081383494", 314000250)
