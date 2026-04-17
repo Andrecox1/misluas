a4f90f018cdf3e0509676d578a3ac380360b450c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:20:18 -0500

    add 4524090/4524090.lua

---
+++ 4524090/4524090.lua
@@ -0,0 +1,12 @@
+-- 4524090's Lua and Manifest Created by Hubcap Manifest
+-- John's Quest Soundtrack
+-- Created: April 17, 2026 at 10:56:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4524090) -- John's Quest Soundtrack
+-- MAIN APP DEPOTS
+addappid(4524091, 1, "870aaefa4c618791b9c80175eb804325eb1763ea7cbbf2cc4ae64142c2b22af6") -- Depot 4524091
+setManifestid(4524091, "5387298473354500061", 48337980)
