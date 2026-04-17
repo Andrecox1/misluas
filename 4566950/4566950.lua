8f82b41ebf1629c87580a8416cfc75c86f9bb6be
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:20:25 -0500

    add 4566950/4566950.lua

---
+++ 4566950/4566950.lua
@@ -0,0 +1,12 @@
+-- 4566950's Lua and Manifest Created by Hubcap Manifest
+-- Mawgate
+-- Created: April 17, 2026 at 11:01:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4566950) -- Mawgate
+-- MAIN APP DEPOTS
+addappid(4566951, 1, "49d972638316e98fa51966917a3937f837af23e71870ba941d8a8665224e3e99") -- Depot 4566951
+setManifestid(4566951, "3007591134440756139", 261837276)
