da18aa441c87e50477cb3b9229bc6d0606dd6570
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:25:05 -0500

    add 3800420/3800420.lua

---
+++ 3800420/3800420.lua
@@ -0,0 +1,14 @@
+-- 3800420's Lua and Manifest Created by Hubcap Manifest
+-- Turquoise
+-- Created: April 17, 2026 at 16:24:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3800420) -- Turquoise
+-- MAIN APP DEPOTS
+addappid(3800421, 1, "58e4bf647b1d597604e602c69c8db3a767b397ccaf8dce3993822e4bf9944803") -- Depot 3800421
+setManifestid(3800421, "5233297308940321393", 323753705)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3800422) -- Depot 3800422 (empty depot)
