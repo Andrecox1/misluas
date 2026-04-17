d02bec654e51f02958577311c40303f51fa33061
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:20:36 -0500

    add 4568780/4568780.lua

---
+++ 4568780/4568780.lua
@@ -0,0 +1,14 @@
+-- 4568780's Lua and Manifest Created by Hubcap Manifest
+-- Vertal Strike
+-- Created: April 17, 2026 at 10:49:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4568780) -- Vertal Strike
+-- MAIN APP DEPOTS
+addappid(4568781, 1, "181df425e32949ce8da8fcd75ca8f54cce0712e5fb3a6d8adb7c390b44969177") -- Depot 4568781
+setManifestid(4568781, "8250845491606828133", 2624851621)
+addappid(4568782, 1, "d7a54af6d97838a2304422be03e80ae6a299e5ff84348192264ead8dbf25eac4") -- Depot 4568782
+setManifestid(4568782, "4106320450782551773", 1952929326)
