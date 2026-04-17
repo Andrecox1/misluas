e3db1ceb17908d7121b21d08a272f59b228299ee
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:24:44 -0500

    add 1933960/1933960.lua

---
+++ 1933960/1933960.lua
@@ -0,0 +1,20 @@
+-- 1933960's Lua and Manifest Created by Hubcap Manifest
+-- Venice After Dark
+-- Created: April 17, 2026 at 17:11:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1933960) -- Venice After Dark
+-- MAIN APP DEPOTS
+addappid(1933961, 1, "2a0b18b4e7ec6026053167f6a32b76429b75937880394f06fdde0435eb21ab9a") -- Depot 1933961
+setManifestid(1933961, "960051537187509117", 519804028)
+addappid(1933962, 1, "7551751c7b7845aeefc4be2ae7214221a70ae3d2af23b4c9304ba255b61d9df7") -- Depot 1933962
+setManifestid(1933962, "9123620558814877028", 474952874)
+addappid(1933963, 1, "ec50adc2cbe82fc7663aaffc18781e592db452fc6d11373617cf022d69116c96") -- Depot 1933963
+setManifestid(1933963, "5862792495764424932", 502293522)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
