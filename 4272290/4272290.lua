b06070faeec465e60a76298a9a9f3d2cbca2b598
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 15:56:44 -0500

    add 4272290/4272290.lua

---
+++ 4272290/4272290.lua
@@ -0,0 +1,12 @@
+﻿-- 4272290's Lua and Manifest Created by Hubcap Manifest
+-- Sanctum Ruins
+-- Created: April 17, 2026 at 09:09:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4272290) -- Sanctum Ruins
+-- MAIN APP DEPOTS
+addappid(4272291, 1, "9703ecbe7ff2f5d04ca9ad9a53dd3562cf1909d96e909eed5ed1a1e3b103080a") -- Depot 4272291
+addappid(4272292, 1, "f2872917713d7bc24d7279305933f6c8b2f7e8e01e42fefa9463b1f327116b47") -- Depot 4272292
