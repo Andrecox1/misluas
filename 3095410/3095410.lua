e677ec7dd9695f1a6344d07e0188dd013c902789
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:19:46 -0500

    add 3095410/3095410.lua

---
+++ 3095410/3095410.lua
@@ -0,0 +1,12 @@
+-- 3095410's Lua and Manifest Created by Hubcap Manifest
+-- ZPF
+-- Created: April 17, 2026 at 06:31:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3095410, 1, "0bf73b57ebd26498d8ad2b88c71f927b1da1f915a221ff514c3c21ad1d688b98") -- ZPF
+-- MAIN APP DEPOTS
+addappid(3095411, 1, "2c04a17c437b1410f4c7d520d3eb22d9acf74e915a90a18f79ed6b8551d0a500") -- Depot 3095411
+setManifestid(3095411, "1075688792050060342", 89596894)
