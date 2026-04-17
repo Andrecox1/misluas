bff83e4221304d434a855a938dc60d780f74248d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:20:06 -0500

    add 3053130/3053130.lua

---
+++ 3053130/3053130.lua
@@ -0,0 +1,14 @@
+-- 3053130's Lua and Manifest Created by Hubcap Manifest
+-- Tokyo Trigger
+-- Created: April 17, 2026 at 11:36:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3053130) -- Tokyo Trigger
+-- MAIN APP DEPOTS
+addappid(3053131, 1, "c2bbb97fee6740fa9566379b9e3cc3962d0c8f4c93f4ad74bf9a505792b26cdb") -- Depot 3053131
+setManifestid(3053131, "7215147477508626302", 1903439768)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3053132) -- Depot 3053132 (empty depot)
