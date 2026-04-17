24afb4d0eed8ad3c85ec426cba3f481ae1a2f122
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:20:02 -0500

    add 4301790/4301790.lua

---
+++ 4301790/4301790.lua
@@ -0,0 +1,12 @@
+-- 4301790's Lua and Manifest Created by Hubcap Manifest
+-- Soluna Explorers: Tether Curse of Slimus
+-- Created: April 17, 2026 at 06:33:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4301790) -- Soluna Explorers: Tether Curse of Slimus
+-- MAIN APP DEPOTS
+addappid(4301791, 1, "af48189f67dbeda89fba70874ec4bc9abd38fe85040c23a700527ab1c7e57336") -- Depot 4301791
+setManifestid(4301791, "6296038107910285358", 210307501)
