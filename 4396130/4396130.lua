4a934aa8926f4ada967f7353e31ac0d2d7d1d404
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:07:31 -0500

    add 4396130/4396130.lua

---
+++ 4396130/4396130.lua
@@ -0,0 +1,12 @@
+-- 4396130's Lua and Manifest Created by Hubcap Manifest
+-- The Day I Became a Bird - Soundtrack
+-- Created: April 17, 2026 at 10:55:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4396130) -- The Day I Became a Bird - Soundtrack
+-- MAIN APP DEPOTS
+addappid(4396170, 1, "44c13e825260a895a28b676d16ec46818531f478ec7ceadbfaf55e594a23bc93") -- Depot 4396170
+setManifestid(4396170, "2369928240919588247", 45662602)
