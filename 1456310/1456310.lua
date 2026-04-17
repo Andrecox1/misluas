a0146683e856ae42f122677b30fe74bf532671d3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:24:38 -0500

    add 1456310/1456310.lua

---
+++ 1456310/1456310.lua
@@ -0,0 +1,14 @@
+-- 1456310's Lua and Manifest Created by Hubcap Manifest
+-- Umurangi Generation Macro Soundtrack
+-- Created: April 17, 2026 at 16:13:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1456310) -- Umurangi Generation Macro Soundtrack
+-- MAIN APP DEPOTS
+addappid(1456311, 1, "3f1d466fb373b6b7d68f3b0d08f8e080d392abef33f67f90b1afeaa9bbf8a3ec") -- Umurangi Generation Macro Soundtrack MP3
+setManifestid(1456311, "1826210531390346961", 122987485)
+addappid(1456312, 1, "c2c10f5321497d08984dd973ef35660b9baa7c67be3baac35ba7ed38ea3cb55d") -- Umurangi Generation Macro Soundtrack FLAC
+setManifestid(1456312, "1564119345156551966", 436270261)
