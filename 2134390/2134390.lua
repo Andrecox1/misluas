2e3903d090394fe44d74b156afe69243c6feed43
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:19:10 -0500

    add 2134390/2134390.lua

---
+++ 2134390/2134390.lua
@@ -0,0 +1,12 @@
+-- 2134390's Lua and Manifest Created by Hubcap Manifest
+-- CloudDwellers
+-- Created: April 17, 2026 at 06:29:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2134390) -- CloudDwellers
+-- MAIN APP DEPOTS
+addappid(2134392, 1, "91776889eaf413c1c989e300be8839a67eea8d37b9a1b069d02694b100f77112") -- Depot 2134392
+setManifestid(2134392, "7463489432566171956", 1853269335)
