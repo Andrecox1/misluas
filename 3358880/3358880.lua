19604c7aa51befcc80193df3006a15828dfd7b43
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:05:16 -0500

    add 3358880/3358880.lua

---
+++ 3358880/3358880.lua
@@ -0,0 +1,12 @@
+-- 3358880's Lua and Manifest Created by Hubcap Manifest
+-- Nippon Marathon 2: Daijoubu
+-- Created: April 17, 2026 at 11:04:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3358880) -- Nippon Marathon 2: Daijoubu
+-- MAIN APP DEPOTS
+addappid(3358881, 1, "c1aa0b07c532818fa3a76253c425a39d47e41421dcd991a4db26fb8d79c9d615") -- Depot 3358881
+setManifestid(3358881, "5906816323063318326", 2275637128)
