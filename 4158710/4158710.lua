54e12d9903738d2061af1a828094e59686302bd7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:19:27 -0500

    add 4158710/4158710.lua

---
+++ 4158710/4158710.lua
@@ -0,0 +1,18 @@
+-- 4158710's Lua and Manifest Created by Hubcap Manifest
+-- BLACKOUT ARENA
+-- Created: April 17, 2026 at 06:24:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4158710) -- BLACKOUT ARENA
+-- MAIN APP DEPOTS
+addappid(4158711, 1, "601fe54a04d028a8cd720a75ec5da01c1f9f6d779e06be3697c9d91b71696d3b") -- Depot 4158711
+setManifestid(4158711, "151495288244402952", 32766992792)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
