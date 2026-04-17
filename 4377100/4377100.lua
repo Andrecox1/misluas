3e87cdae27fea508c80e1f5349bd7bff81352aee
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:20:15 -0500

    add 4377100/4377100.lua

---
+++ 4377100/4377100.lua
@@ -0,0 +1,12 @@
+-- 4377100's Lua and Manifest Created by Hubcap Manifest
+-- Spreading eerie curses
+-- Created: April 17, 2026 at 06:25:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4377100) -- Spreading eerie curses
+-- MAIN APP DEPOTS
+addappid(4377101, 1, "bb44009d73b0b59a6b76a0377188e1f0d726ff7fa10a25c720a495e827b90fc6") -- Depot 4377101
+setManifestid(4377101, "3673898176059288808", 1429799377)
