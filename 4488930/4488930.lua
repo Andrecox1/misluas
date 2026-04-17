3c305a7aa61a83f9b153c0b4446c2a898d7111d6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:20:20 -0500

    add 4488930/4488930.lua

---
+++ 4488930/4488930.lua
@@ -0,0 +1,12 @@
+-- 4488930's Lua and Manifest Created by Hubcap Manifest
+-- Smoke & Lottery Simulator
+-- Created: April 17, 2026 at 12:31:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4488930) -- Smoke & Lottery Simulator
+-- MAIN APP DEPOTS
+addappid(4488931, 1, "5f7d613e7543055f6bf4bff87408894d934bf03c2f087916c2af4bae145d203c") -- Depot 4488931
+setManifestid(4488931, "2937382680262509665", 3890577552)
