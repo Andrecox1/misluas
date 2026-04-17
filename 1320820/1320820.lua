be3ac437e1d6360a5fe50189e7a4ca47b4d38807
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:24:31 -0500

    add 1320820/1320820.lua

---
+++ 1320820/1320820.lua
@@ -0,0 +1,14 @@
+-- 1320820's Lua and Manifest Created by Hubcap Manifest
+-- Umurangi Generation Soundtrack
+-- Created: April 17, 2026 at 16:12:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1320820) -- Umurangi Generation Soundtrack
+-- MAIN APP DEPOTS
+addappid(1320821, 1, "f36f7f8d2c3d6ad3270535281cd9e4146257ce0c9ada8a142327a6c7b266ca86") -- Umurangi Generation Soundtrack MP3
+setManifestid(1320821, "7211182682687969569", 213015834)
+addappid(1320822, 1, "2a3c13942db3de7d1e55bd23157d32dc4a7e0ac0de8a85c2f8f87001d62872ee") -- Umurangi Generation Soundtrack FLAC
+setManifestid(1320822, "6282942568794371922", 864128049)
