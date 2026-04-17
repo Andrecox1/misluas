047328b44d07e90f4284da46aa333e670a15f778
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 17:19:19 -0500

    add 3830690/3830690.lua

---
+++ 3830690/3830690.lua
@@ -0,0 +1,15 @@
+﻿-- 3830690's Lua and Manifest Created by Hubcap Manifest
+-- Flee, My Elven Ninja!  3D
+-- Created: April 17, 2026 at 08:34:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(3830690) -- Flee, My Elven Ninja!  3D
+-- MAIN APP DEPOTS
+addappid(3830691, 1, "b7dc6defefd989232eaa64bc1b6251b5e1a5ea073241601dd1d7706eb75376f5") -- Depot 3830691
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Flee, My Elven Ninja 3D - Artbook (AppID: 4209540) - missing depot keys
+-- addappid(4209540)
