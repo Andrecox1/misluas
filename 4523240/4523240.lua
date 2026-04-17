79a4bb1f3f7518161a42d03d48e7ca2b687a4944
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-17 16:20:12 -0500

    add 4523240/4523240.lua

---
+++ 4523240/4523240.lua
@@ -0,0 +1,12 @@
+-- 4523240's Lua and Manifest Created by Hubcap Manifest
+-- The Last Connection
+-- Created: April 17, 2026 at 12:01:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4523240) -- The Last Connection
+-- MAIN APP DEPOTS
+addappid(4523241, 1, "b898d43741228afdd4701c0d0221a2894534aaf0962a22ee550224fffa4cc2cc") -- Depot 4523241
+setManifestid(4523241, "477246424666956795", 1966469237)
