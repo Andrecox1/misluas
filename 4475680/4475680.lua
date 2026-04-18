0e613e157efb12bd34550cb26c99d1c8ee3ba4f0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:44:49 -0500

    add 4475680/4475680.lua

---
+++ 4475680/4475680.lua
@@ -0,0 +1,12 @@
+-- 4475680's Lua and Manifest Created by Hubcap Manifest
+-- 你离开之后 Demo
+-- Created: April 18, 2026 at 13:44:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4475680) -- 你离开之后 Demo
+-- MAIN APP DEPOTS
+addappid(4475681, 1, "6f5d888da90eacdcf9b05ebe2e1cd879d35933a15e24d39a73349daacdf8d970") -- Depot 4475681
+setManifestid(4475681, "7391183591814478748", 369554628)
