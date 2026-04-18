8a611837ba84a028378cce0dcf47756b28f90005
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:47:35 -0500

    add 3428230/3428230.lua

---
+++ 3428230/3428230.lua
@@ -0,0 +1,14 @@
+-- 3428230's Lua and Manifest Created by Hubcap Manifest
+-- Ginger Demo
+-- Created: April 18, 2026 at 09:28:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3428230) -- Ginger Demo
+-- MAIN APP DEPOTS
+addappid(3428232, 1, "6ab31f3e786253edc8ca82d24be87dbdaf7188aa52c38517029a2caac6bb5113") -- Depot 3428232
+setManifestid(3428232, "1538424089206081293", 121503663)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3428231) -- Depot 3428231 (empty depot)
