7e9e8f75b410ddb6769e4747dfd61416110cbb51
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:47:14 -0500

    add 2867290/2867290.lua

---
+++ 2867290/2867290.lua
@@ -0,0 +1,12 @@
+-- 2867290's Lua and Manifest Created by Hubcap Manifest
+-- Demonic Deckmaster Demo
+-- Created: April 18, 2026 at 09:30:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2867290) -- Demonic Deckmaster Demo
+-- MAIN APP DEPOTS
+addappid(2867291, 1, "cb7d86dcd447570f03a096367d8590770914bbbca00e1441ebb91af777f9c036") -- Depot 2867291
+setManifestid(2867291, "6383141615489231907", 31980540)
