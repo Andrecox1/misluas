4765357088f05b01a24dfe841eef40adb15d2339
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:44:41 -0500

    add 4340940/4340940.lua

---
+++ 4340940/4340940.lua
@@ -0,0 +1,12 @@
+-- 4340940's Lua and Manifest Created by Hubcap Manifest
+-- Between
+-- Created: April 18, 2026 at 09:32:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4340940) -- Between
+-- MAIN APP DEPOTS
+addappid(4340941, 1, "cfac74ba084d10d980f51e41626929ca26a990d9e55cbe6f072ccf200df5e306") -- Depot 4340941
+setManifestid(4340941, "9187370000270131902", 367338352)
