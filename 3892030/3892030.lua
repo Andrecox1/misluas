dfc937d62a3311fbde0ca214b79819258a885c8a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:23 -0500

    add 3892030/3892030.lua

---
+++ 3892030/3892030.lua
@@ -0,0 +1,12 @@
+-- 3892030's Lua and Manifest Created by Hubcap Manifest
+-- 5 nights at Timokha 5: Island
+-- Created: April 18, 2026 at 09:38:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3892030) -- 5 nights at Timokha 5: Island
+-- MAIN APP DEPOTS
+addappid(3892031, 1, "efe0f2828fdfe8ee795d7dc75bb90b8c6d108cd5af0dff811d317772a0ae2c72") -- Depot 3892031
+setManifestid(3892031, "6063403757664759865", 1390661805)
