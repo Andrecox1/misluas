98486dcefb5129f5917ba96251967778aa2444e9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:49:06 -0500

    add 4027000/4027000.lua

---
+++ 4027000/4027000.lua
@@ -0,0 +1,12 @@
+-- 4027000's Lua and Manifest Created by Hubcap Manifest
+-- 【重大告知】橘リルリ転生いたします！ Demo
+-- Created: April 18, 2026 at 09:26:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4027000) -- 【重大告知】橘リルリ転生いたします！ Demo
+-- MAIN APP DEPOTS
+addappid(4027001, 1, "3d6c505a008a89446886b5b4c5b7c342cf76143b794e0c15058910176c9bf36a") -- Depot 4027001
+setManifestid(4027001, "581418782861062589", 1112749532)
