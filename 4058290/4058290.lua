2fc157c7baee217dce966e4917f1d8a1ba6ea236
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:49:19 -0500

    add 4058290/4058290.lua

---
+++ 4058290/4058290.lua
@@ -0,0 +1,14 @@
+-- 4058290's Lua and Manifest Created by Hubcap Manifest
+-- Wormies
+-- Created: April 18, 2026 at 09:25:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4058290) -- Wormies
+-- MAIN APP DEPOTS
+addappid(4058291, 1, "618f6c6215de6145bb3b3b55ce968d93f85345b62b34f4dfb9b45c981901b423") -- Depot 4058291
+setManifestid(4058291, "4032676807650110058", 1078370430)
+addappid(4058292, 1, "a00072df623614a2e1fee3eccad2d038fcd27a1c48800019ecb66ca2508d1bdf") -- Depot 4058292
+setManifestid(4058292, "5579302106275178020", 1103028675)
