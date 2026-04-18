112c4dce27eb47a7ad3bd3bfe834ca1cc8dc63c9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:49 -0500

    add 4552230/4552230.lua

---
+++ 4552230/4552230.lua
@@ -0,0 +1,12 @@
+-- 4552230's Lua and Manifest Created by Hubcap Manifest
+-- Make it! Gyoza
+-- Created: April 18, 2026 at 09:34:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4552230) -- Make it! Gyoza
+-- MAIN APP DEPOTS
+addappid(4552231, 1, "4f07d0a6d9008183fb77e7726f81a351e9cf813f0bb748d7f0e2cd2f8bbb9c6c") -- Depot 4552231
+setManifestid(4552231, "8876614791388993595", 142473757)
