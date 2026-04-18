e1cbf92436026deae2aec5c88adf3e34ee7f8026
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:44 -0500

    add 4436270/4436270.lua

---
+++ 4436270/4436270.lua
@@ -0,0 +1,18 @@
+-- 4436270's Lua and Manifest Created by Hubcap Manifest
+-- Hollowborn
+-- Created: April 18, 2026 at 09:33:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4436270) -- Hollowborn
+-- MAIN APP DEPOTS
+addappid(4436271, 1, "4412e7496eff3a12c1e58f67555c39b3a1c311758b46320bf116b029ca9f457b") -- Depot 4436271
+setManifestid(4436271, "2494977877772704267", 2259375570)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
