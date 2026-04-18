b6ba1bbb462f688bc98bf842bd39e608748c59d0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:44:31 -0500

    add 1913310/1913310.lua

---
+++ 1913310/1913310.lua
@@ -0,0 +1,18 @@
+-- 1913310's Lua and Manifest Created by Hubcap Manifest
+-- Undead Tread
+-- Created: April 18, 2026 at 09:32:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1913310) -- Undead Tread
+-- MAIN APP DEPOTS
+addappid(1913311, 1, "525e36e5a5705158b01240e1a5b7239bda034b085aaa181ae257ecd3b51d1857") -- Depot 1913311
+setManifestid(1913311, "6045211201792231426", 3490775218)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
