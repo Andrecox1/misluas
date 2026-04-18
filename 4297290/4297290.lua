afb4b33508c5a4d6207d6a4e53aeef40651df95a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:58 -0500

    add 4297290/4297290.lua

---
+++ 4297290/4297290.lua
@@ -0,0 +1,20 @@
+-- 4297290's Lua and Manifest Created by Hubcap Manifest
+-- Therapy Forest
+-- Created: April 18, 2026 at 09:37:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(4297290) -- Therapy Forest
+-- MAIN APP DEPOTS
+addappid(4297291, 1, "3aa8d1e2c88da3fbe8637f5d2835e8ad09d94f2545c4097de209053ee58472a8") -- Depot 4297291
+setManifestid(4297291, "2749339513449222165", 832000953)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
