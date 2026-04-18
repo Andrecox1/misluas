4424e1d55feeeb0b5ab0eebf4f05f993c40c563c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:43:18 -0500

    add 2957580/2957580.lua

---
+++ 2957580/2957580.lua
@@ -0,0 +1,19 @@
+-- 2957580's Lua and Manifest Created by Hubcap Manifest
+-- Pizza Bandit - Alpha Test
+-- Created: April 18, 2026 at 09:34:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2957580) -- Pizza Bandit - Alpha Test
+addtoken(2957580, "8050902721334552560")
+-- MAIN APP DEPOTS
+addappid(2957581, 1, "7e355f9f083b631e3b6968a99c0aa0b3b222aefd07a814a422d8ef5279ab158f") -- Depot 2957581
+setManifestid(2957581, "7547790144597789172", 5151310420)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
