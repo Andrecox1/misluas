b3a22fd73a1a39bdb0cacffb28d4881c34dbe54c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:45:16 -0500

    add 4435050/4435050.lua

---
+++ 4435050/4435050.lua
@@ -0,0 +1,12 @@
+-- 4435050's Lua and Manifest Created by Hubcap Manifest
+-- Crime Empire Idle Demo
+-- Created: April 18, 2026 at 09:32:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4435050) -- Crime Empire Idle Demo
+-- MAIN APP DEPOTS
+addappid(4435051, 1, "9638768e0fc5f0d0b1715f70aa4f1a4164f4fce6588a93720309177d1aedb449") -- Depot 4435051
+setManifestid(4435051, "2518637249336750540", 617463560)
