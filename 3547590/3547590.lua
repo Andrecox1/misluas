c1812498fd8ad539075abbb81947be57a19d7f6f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:08 -0500

    add 3547590/3547590.lua

---
+++ 3547590/3547590.lua
@@ -0,0 +1,12 @@
+-- 3547590's Lua and Manifest Created by Hubcap Manifest
+-- Forgekeepers Demo
+-- Created: April 18, 2026 at 09:35:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3547590) -- Forgekeepers Demo
+-- MAIN APP DEPOTS
+addappid(3547591, 1, "3c3d06b2fd59e65aaa12eeea3d536e0c10fc4c5cb692187fe0f45cb583bccd45") -- Depot 3547591
+setManifestid(3547591, "704222865728212574", 308455541)
