96f62791616c71f19be0cd4ec205bc8aeac54d37
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:47:28 -0500

    add 4457770/4457770.lua

---
+++ 4457770/4457770.lua
@@ -0,0 +1,12 @@
+-- 4457770's Lua and Manifest Created by Hubcap Manifest
+-- Rift Miner Soundtrack
+-- Created: April 18, 2026 at 09:29:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4457770) -- Rift Miner Soundtrack
+-- MAIN APP DEPOTS
+addappid(4457771, 1, "53fdb0fca8fbdfe9b1762ed5490fe56a55da7513b852422dbfce190bd01be9ef") -- Depot 4457771
+setManifestid(4457771, "5065697363473455116", 181924693)
