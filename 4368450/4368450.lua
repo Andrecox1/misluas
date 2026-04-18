50a732c9ad24c0dc8ec6a693761cc6fa7dc27e9c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:47:40 -0500

    add 4368450/4368450.lua

---
+++ 4368450/4368450.lua
@@ -0,0 +1,12 @@
+-- 4368450's Lua and Manifest Created by Hubcap Manifest
+-- 黎明探险队 Demo
+-- Created: April 18, 2026 at 09:30:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4368450) -- 黎明探险队 Demo
+-- MAIN APP DEPOTS
+addappid(4368451, 1, "676c611920b847f9551fe89d75867f81e4857e58d2abbeca885b5088715314b4") -- Depot 4368451
+setManifestid(4368451, "7098963940743249883", 951412937)
