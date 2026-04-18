c0aa217ed0edd3ccca5fb5900a4c7ea939b1e16b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:47:08 -0500

    add 4369470/4369470.lua

---
+++ 4369470/4369470.lua
@@ -0,0 +1,14 @@
+-- 4369470's Lua and Manifest Created by Hubcap Manifest
+-- ShantyTown Soundtrack
+-- Created: April 18, 2026 at 09:27:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4369470) -- ShantyTown Soundtrack
+-- MAIN APP DEPOTS
+addappid(4369472, 1, "b4ca13630e01af3731064e353ea18ebe10cbd1f604eec864ecb04243f0199633") -- Depot 4369472
+setManifestid(4369472, "7762215143451430038", 1015706859)
+addappid(4369471, 1, "b4e06901cab986a0efb9dd7c9caae4e0cf9fecce3199557b2d2cef76e6b4c51e") -- Depot 4369471
+setManifestid(4369471, "2300789266551610561", 219250832)
