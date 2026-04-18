3cda783c26f3947c2f7064db2ae18e55e6b0abe0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:43:12 -0500

    add 4467000/4467000.lua

---
+++ 4467000/4467000.lua
@@ -0,0 +1,12 @@
+-- 4467000's Lua and Manifest Created by Hubcap Manifest
+-- World of Tanks: HEAT Closed Beta
+-- Created: April 18, 2026 at 09:35:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4467000) -- World of Tanks: HEAT Closed Beta
+-- MAIN APP DEPOTS
+addappid(4467001, 1, "1ca3f060d9a166cf9f0a7a9bda44549db6a2bfa50d73860d7a7c6280a6ac44e2") -- Depot 4467001
+setManifestid(4467001, "6744414805253602636", 26868042476)
