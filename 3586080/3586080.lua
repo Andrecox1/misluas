f4574e0d391059ee38d97ba80863003e7058d4a5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:38 -0500

    add 3586080/3586080.lua

---
+++ 3586080/3586080.lua
@@ -0,0 +1,12 @@
+-- 3586080's Lua and Manifest Created by Hubcap Manifest
+-- Learn Japanese Kana & Vocab with Sushi Demo
+-- Created: April 18, 2026 at 09:36:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3586080) -- Learn Japanese Kana & Vocab with Sushi Demo
+-- MAIN APP DEPOTS
+addappid(3586082, 1, "3d505a505055dcd3bc052a14689fa8b034fe96faa2b02e7fbf9ea4ece74b951e") -- Depot 3586082
+setManifestid(3586082, "6255302081113470078", 1556298541)
