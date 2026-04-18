7e85b335cfd0d7144af24b370f21e7b94afeef37
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:53 -0500

    add 2395640/2395640.lua

---
+++ 2395640/2395640.lua
@@ -0,0 +1,12 @@
+﻿-- 2395640's Lua and Manifest Created by Hubcap Manifest
+-- Death Trick: Double Blind Playtest
+-- Created: April 18, 2026 at 09:33:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2395640) -- Death Trick: Double Blind Playtest
+addtoken(2395640, "12892675875075443490")
+-- MAIN APP DEPOTS
+addappid(2395641, 1, "cd6cb99937cd579c499961f5a8fb8b5295cf167021aca35080f1f78541273c82") -- Depot 2395641
