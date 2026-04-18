de84d581b490201b775da1079fda1bb96ee1c1c2
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:02 -0500

    add 1106040/1106040.lua

---
+++ 1106040/1106040.lua
@@ -0,0 +1,37 @@
+-- 1106040's Lua and Manifest Created by Hubcap Manifest
+-- Wolfenstein: Youngblood Demo
+-- Created: April 18, 2026 at 09:33:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 12
+-- Total DLCs: 0
+-- Shared Depots: 11
+
+-- MAIN APPLICATION
+addappid(1106040, 1, "5a48376a286363ea2ecc564036c94467eea6b93025ded0dc250306e8509aeb17") -- Wolfenstein: Youngblood Demo
+addtoken(1106040, "10663463657130771836")
+-- MAIN APP DEPOTS
+addappid(1106041, 1, "a2ec1bef4e003eb9e88e808919fd9cabde030ade0f1459783832e760201ff75f") -- Depot 1106041
+setManifestid(1106041, "2890484303204850627", 80081560)
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(1056963, 1, "321587ab8aff11d56ea0a4146981e0c85e80f7f177416fac4ad8412e1a2cf0e2") -- Game Resources (Shared from App 1056960)
+setManifestid(1056963, "6674817775478828724", 32081804531)
+addappid(1056964, 1, "93c9b0eea7c3976388d44232fbcf7c409f5b73cbfa9cb92704d24f5278b6ccf0") -- Lightmaps (Shared from App 1056960)
+setManifestid(1056964, "5356925120074271314", 13618981120)
+addappid(1056965, 1, "5b00d3d157d787a53e992abf72c92d87c86bbef33ba2f4d255ac9e663282d3b2") -- Soundbanks (English) (Shared from App 1056960)
+setManifestid(1056965, "8173088392605133358", 132)
+addappid(1056961, 1, "185849a7d07a28dbb77991bdad1c4c01f97e550df4aee74b9989af1fbaa6e8a5") -- Soundbanks (French) (Shared from App 1056960)
+setManifestid(1056961, "8598998661004392767", 132)
+addappid(1056966, 1, "afbe14cc4541a0d67ba96c4c39f7be1e80a91156c0b01a9ae35254fcac6afe46") -- Soundbanks (Italian) (Shared from App 1056960)
+setManifestid(1056966, "6410804406630382438", 132)
+addappid(1056967, 1, "cf38542cdc13d2b27e5b93032e3031db407a3d6c083805fd1358d624fea8b5da") -- Soundbanks (Brazilian Portugese) (Shared from App 1056960)
+setManifestid(1056967, "2668168886041220379", 132)
+addappid(1056968, 1, "4bd71bc863b81546a34cb8b0df828210e2bd7727bb8d481595cc3c39a39549a7") -- Soundbanks (Russian) (Shared from App 1056960)
+setManifestid(1056968, "3385535659947099612", 132)
+addappid(1056969, 1, "b290ec43020913c844e81b540fcf5d8ff1b325c49a29418c4b61abda58d6bc0f") -- Soundbanks (Spanish) (Shared from App 1056960)
+setManifestid(1056969, "8132650484489790188", 132)
+addappid(1056991, 1, "f1e90ccf12f6f13bcee1c518df8abc5ced5565a6bbef36e78e280a7d751ac6bf") -- Soundbanks (Latin Spanish) (Shared from App 1056960)
+setManifestid(1056991, "8593444316544851260", 132)
