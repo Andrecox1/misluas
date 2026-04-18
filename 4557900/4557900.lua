c39d47c0f8ba66068cc795455697a7898c8b94e6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:47:20 -0500

    add 4557900/4557900.lua

---
+++ 4557900/4557900.lua
@@ -0,0 +1,12 @@
+-- 4557900's Lua and Manifest Created by Hubcap Manifest
+-- Coin of Fate
+-- Created: April 18, 2026 at 09:28:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4557900) -- Coin of Fate
+-- MAIN APP DEPOTS
+addappid(4557901, 1, "c0f793050e195543f41ec5a1c42f6c84df769093e851fd3d38f61bf09ed6b412") -- Depot 4557901
+setManifestid(4557901, "6280441016103863112", 1670428443)
