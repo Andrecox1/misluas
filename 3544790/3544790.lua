8ef1e574caddf1f17cc90883381cf289843a86d0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:47:45 -0500

    add 3544790/3544790.lua

---
+++ 3544790/3544790.lua
@@ -0,0 +1,12 @@
+﻿-- 3544790's Lua and Manifest Created by Hubcap Manifest
+-- PAIN-TO-WIN Playtest
+-- Created: April 18, 2026 at 09:27:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3544790) -- PAIN-TO-WIN Playtest
+addtoken(3544790, "350242691408071727")
+-- MAIN APP DEPOTS
+addappid(3544791, 1, "de79301fb35ac7470d7627218cbf28fb899c99bffa2ceeece29eb9c4fcdb585c") -- Depot 3544791
