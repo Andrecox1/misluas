0e3d9bd371abde416945825299f68857a25eabb1
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:46:58 -0500

    add 3571860/3571860.lua

---
+++ 3571860/3571860.lua
@@ -0,0 +1,12 @@
+-- 3571860's Lua and Manifest Created by Hubcap Manifest
+-- Gretel's Honesty グレーテルの本懐 Demo
+-- Created: April 18, 2026 at 09:29:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3571860) -- Gretel's Honesty グレーテルの本懐 Demo
+-- MAIN APP DEPOTS
+addappid(3571861, 1, "6e3ad5f44a4a12b16ba607f181bbbe62f1ef328dc521f28fb4f5962057a9007b") -- Depot 3571861
+setManifestid(3571861, "1036280661641659923", 236984450)
