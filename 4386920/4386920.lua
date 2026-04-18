e825cc0f42d653ee86b837675d090f68a0582399
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:44:59 -0500

    add 4386920/4386920.lua

---
+++ 4386920/4386920.lua
@@ -0,0 +1,12 @@
+-- 4386920's Lua and Manifest Created by Hubcap Manifest
+-- Nihongo Hunter
+-- Created: April 18, 2026 at 09:31:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4386920) -- Nihongo Hunter
+-- MAIN APP DEPOTS
+addappid(4386921, 1, "049367f1b006d0836effd8f685f72acedcff86933de6f1b21cc2a218a7f662d5") -- Depot 4386921
+setManifestid(4386921, "3858761112648456707", 193495851)
