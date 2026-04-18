2c52b3683d26731e519f9556b510281b52041f1d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:42:16 -0500

    add 4040710/4040710.lua

---
+++ 4040710/4040710.lua
@@ -0,0 +1,12 @@
+-- 4040710's Lua and Manifest Created by Hubcap Manifest
+-- Marrow Marrow
+-- Created: April 18, 2026 at 09:38:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4040710) -- Marrow Marrow
+-- MAIN APP DEPOTS
+addappid(4040711, 1, "dfc4d0c5b844274479eb613eee37e9ca7d2c785ace74ca0ca8d16e613428e950") -- Depot 4040711
+setManifestid(4040711, "9021669648786705211", 1368373898)
