d067a87c6179c93bca697348f501af8131716ef9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-18 12:49:14 -0500

    add 4204110/4204110.lua

---
+++ 4204110/4204110.lua
@@ -0,0 +1,12 @@
+-- 4204110's Lua and Manifest Created by Hubcap Manifest
+-- 井神 Demo
+-- Created: April 18, 2026 at 09:24:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4204110) -- 井神 Demo
+-- MAIN APP DEPOTS
+addappid(4204111, 1, "dd1cf4e564ca627e10a6b3c01387e4e17f9dd10b4de3a18d2d41493c1473e05d") -- Depot 4204111
+setManifestid(4204111, "8036962194202128799", 1209316365)
