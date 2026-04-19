38ed7a3756273b991922e8ddcff7c458442b1d09
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:19:44 -0500

    add 4512430/4512430.lua

---
+++ 4512430/4512430.lua
@@ -0,0 +1,12 @@
+-- 4512430's Lua and Manifest Created by Hubcap Manifest
+-- 花城秘事 Soundtrack
+-- Created: April 18, 2026 at 09:24:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4512430) -- 花城秘事 Soundtrack
+-- MAIN APP DEPOTS
+addappid(4512431, 1, "93b96ec53a01e8d299e3d9d2291f312ebe3cc90632becebd7ead78f4ea650905") -- Depot 4512431
+setManifestid(4512431, "1784336856532507255", 35899800)
