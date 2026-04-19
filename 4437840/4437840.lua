a96824cdfc74b0abf7040ca911c5fb0b6ae28eaa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:11:46 -0500

    add 4437840/4437840.lua

---
+++ 4437840/4437840.lua
@@ -0,0 +1,14 @@
+-- 4437840's Lua and Manifest Created by Hubcap Manifest
+-- Neva: Prologue Soundtrack
+-- Created: April 19, 2026 at 00:57:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4437840) -- Neva: Prologue Soundtrack
+-- MAIN APP DEPOTS
+addappid(4437841, 1, "3eadd7293723aef327a3c99cc1ee4c4b59706a5a3a151f88ad7f044fdf881022") -- Depot 4437841
+setManifestid(4437841, "4846622069217555925", 118486912)
+addappid(4437843, 1, "12e9c55165e8eca35a6e6da5af294cf0c651e735f08c000579d443c8547332a0") -- Depot 4437843
+setManifestid(4437843, "7141213374625139698", 852841224)
