7b141edce6e0238cccbbd6e6c385224111787937
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:13:13 -0500

    add 2950610/2950610.lua

---
+++ 2950610/2950610.lua
@@ -0,0 +1,12 @@
+-- 2950610's Lua and Manifest Created by Hubcap Manifest
+-- BeachHead Gold Edition
+-- Created: April 19, 2026 at 01:46:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2950610) -- BeachHead Gold Edition
+-- MAIN APP DEPOTS
+addappid(2950611, 1, "867779d20487fb089cd7aa5a72ff5cbea8c3d080cd843d8bf00bbea0ec23fa5c") -- Depot 2950611
+setManifestid(2950611, "1962458903379340082", 374711426)
