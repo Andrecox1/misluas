a19497f9263a1a3e9cb905cdcf72a6026fa9cf7e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:11:25 -0500

    add 1281860/1281860.lua

---
+++ 1281860/1281860.lua
@@ -0,0 +1,16 @@
+-- 1281860's Lua and Manifest Created by Hubcap Manifest
+-- Frogsong
+-- Created: April 19, 2026 at 08:47:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1281860) -- Frogsong
+-- MAIN APP DEPOTS
+addappid(1281861, 1, "fd3d78af49e915096c5c3f17d341ab04a891a5df7e824da3cc62123aba3c6b30") -- Depot 1281861
+setManifestid(1281861, "2896138865129544509", 1901745868)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Frogsong Art Book (AppID: 3275030) - missing depot keys
+-- addappid(3275030)
