6ebfdb1cded39c86dbc430b35350e19667db58d3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:19:32 -0500

    add 3624330/3624330.lua

---
+++ 3624330/3624330.lua
@@ -0,0 +1,12 @@
+-- 3624330's Lua and Manifest Created by Hubcap Manifest
+-- Medetashi Medetashi
+-- Created: April 18, 2026 at 09:24:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3624330) -- Medetashi Medetashi
+-- MAIN APP DEPOTS
+addappid(3624331, 1, "1001b999de26f0ab96956cf43d374ead6ed69bd3aa31224b5fd20d147d873d22") -- Depot 3624331
+setManifestid(3624331, "6781859473548779279", 401615879)
