1d34ba801621bf4374ad0505ce505a9801fb7349
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:13:36 -0500

    add 3465190/3465190.lua

---
+++ 3465190/3465190.lua
@@ -0,0 +1,12 @@
+-- 3465190's Lua and Manifest Created by Hubcap Manifest
+-- Aerial_Knight's DropShot
+-- Created: April 19, 2026 at 01:41:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3465190) -- Aerial_Knight's DropShot
+-- MAIN APP DEPOTS
+addappid(3465191, 1, "158ba416658b842419b98b9281e6b7f2faac7a8bafadfe0ba5b6d3a37758db44") -- Depot 3465191
+setManifestid(3465191, "3369147827763559625", 6484849838)
