7a9135193558643a29846f2181f700e61651e856
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:15:44 -0500

    add 3864510/3864510.lua

---
+++ 3864510/3864510.lua
@@ -0,0 +1,14 @@
+-- 3864510's Lua and Manifest Created by Hubcap Manifest
+-- Monument Valley 2 - Original Soundtrack
+-- Created: April 19, 2026 at 00:29:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3864510) -- Monument Valley 2 - Original Soundtrack
+-- MAIN APP DEPOTS
+addappid(3864511, 1, "36374fa37a82a33f0abb632b784caa29a18a87741fd9d317a17d682cdbb6cdbb") -- Depot 3864511
+setManifestid(3864511, "8367018612105100224", 169187830)
+addappid(3864512, 1, "916555ef178ed3e1df787c96603dbc1e107c4cabf5c6a70a09d2fa35500221e5") -- Depot 3864512
+setManifestid(3864512, "3234784347394996863", 1118699184)
