e39bebe6aa8788bd98ff04d11f89d3e75bf59d5c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:11:32 -0500

    add 3868620/3868620.lua

---
+++ 3868620/3868620.lua
@@ -0,0 +1,12 @@
+-- 3868620's Lua and Manifest Created by Hubcap Manifest
+-- Low Magic They Said
+-- Created: April 19, 2026 at 12:11:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3868620) -- Low Magic They Said
+-- MAIN APP DEPOTS
+addappid(3868621, 1, "09e4515766203385b56ac320ea18369e4f643e4852dc919135fd35bc1d2c6f86") -- Depot 3868621
+setManifestid(3868621, "1445104557834885739", 12721463749)
