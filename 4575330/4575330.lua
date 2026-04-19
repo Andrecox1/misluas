895bd35d89c5afb7cc38f5335278edce2bdec326
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:17:02 -0500

    add 4575330/4575330.lua

---
+++ 4575330/4575330.lua
@@ -0,0 +1,12 @@
+-- 4575330's Lua and Manifest Created by Hubcap Manifest
+-- 飛龍の拳 コレクション
+-- Created: April 19, 2026 at 00:25:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4575330) -- 飛龍の拳 コレクション
+-- MAIN APP DEPOTS
+addappid(4575331, 1, "42e4a6a1d23a62284d9f37f66d6b6c34b353ddd785f7475c77a6fee5a86bc334") -- Depot 4575331
+setManifestid(4575331, "1217446116383749873", 22534880)
