92026d7b05175de86a8ec93bdfc0763a631b52ea
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:19:39 -0500

    add 4172380/4172380.lua

---
+++ 4172380/4172380.lua
@@ -0,0 +1,11 @@
+﻿-- 4172380's Lua and Manifest Created by Hubcap Manifest
+-- DiceVaders Playtest
+-- Created: April 18, 2026 at 09:22:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4172380) -- DiceVaders Playtest
+-- MAIN APP DEPOTS
+addappid(4172381, 1, "0a4dfd81d57cd297ba75f6cf299821945f6e3d3726e98f1975cdc33e37d30a0d") -- Depot 4172381
