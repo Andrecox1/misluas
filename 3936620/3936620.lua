d3cf2606a7ec9b69c794c24b3fdd849b07571eaa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:13:40 -0500

    add 3936620/3936620.lua

---
+++ 3936620/3936620.lua
@@ -0,0 +1,12 @@
+-- 3936620's Lua and Manifest Created by Hubcap Manifest
+-- 金泉风土记
+-- Created: April 19, 2026 at 06:40:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3936620) -- 金泉风土记
+-- MAIN APP DEPOTS
+addappid(3936621, 1, "1e2e119e1c9d26e93170afe8cec32160d77583885cbfdd3ecd10e89b239bdc20") -- Depot 3936621
+setManifestid(3936621, "1338726929498045779", 980759651)
