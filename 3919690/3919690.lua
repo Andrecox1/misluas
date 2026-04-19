a8b3f2e49ece38013892c33b85670fd4758e45e4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:11:57 -0500

    add 3919690/3919690.lua

---
+++ 3919690/3919690.lua
@@ -0,0 +1,18 @@
+-- 3919690's Lua and Manifest Created by Hubcap Manifest
+-- OPEN FOR BUSINESS
+-- Created: April 19, 2026 at 06:40:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3919690) -- OPEN FOR BUSINESS
+-- MAIN APP DEPOTS
+addappid(3919692, 1, "d100fdab107571f28bdff4ea4ea1eee979e4ae39e83815ab2fd5c27c8caf4973") -- Depot 3919692
+setManifestid(3919692, "5793634426393296509", 8170978559)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
