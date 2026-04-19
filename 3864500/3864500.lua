b319fb9afc3cc4c097d6ca3bd251efbe028cf38f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:15:38 -0500

    add 3864500/3864500.lua

---
+++ 3864500/3864500.lua
@@ -0,0 +1,12 @@
+﻿-- 3864500's Lua and Manifest Created by Hubcap Manifest
+-- Monument Valley - Original Soundtrack
+-- Created: April 19, 2026 at 00:29:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3864500) -- Monument Valley - Original Soundtrack
+-- MAIN APP DEPOTS
+addappid(3864501, 1, "f460eb87646c32ad72f5bc337f6cff845ec4c98450c3dc650e8f2822bed1fef3") -- Depot 3864501
+addappid(3864502, 1, "4f8d76e90ef0e98c745b645ffac9474c8b41616492875b97ec310bb4de4cb388") -- Depot 3864502
