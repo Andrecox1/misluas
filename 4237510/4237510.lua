06ef3caead9dd2eba25639319ee90fb50055ba21
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-19 11:20:03 -0500

    add 4237510/4237510.lua

---
+++ 4237510/4237510.lua
@@ -0,0 +1,12 @@
+-- 4237510's Lua and Manifest Created by Hubcap Manifest
+-- 纸上的魔法使 Demo
+-- Created: April 18, 2026 at 09:24:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4237510) -- 纸上的魔法使 Demo
+-- MAIN APP DEPOTS
+addappid(4237511, 1, "f6bf567a4d71587a8608a7042391519512f25201b2ad59c3d6d0aae3765e716b") -- Depot 4237511
+setManifestid(4237511, "859094331203605275", 1504219449)
