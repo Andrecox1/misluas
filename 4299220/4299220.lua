fab45f94d68948a8014a1c131933ce16c4844dc8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:31:44 -0500

    add 4299220/4299220.lua

---
+++ 4299220/4299220.lua
@@ -0,0 +1,14 @@
+-- 4299220's Lua and Manifest Created by Hubcap Manifest
+-- Coinbox Clicker
+-- Created: April 21, 2026 at 09:00:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4299220) -- Coinbox Clicker
+-- MAIN APP DEPOTS
+addappid(4299222, 1, "faf52d2b94865c9517bb45a65e5cad89ffbbc18de4f6694d28c0aab55450410f") -- Depot 4299222
+setManifestid(4299222, "5584337808041073816", 36291811)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4299221) -- Depot 4299221 (empty depot)
