ab788f6f4b640906c9bd13b19a6f5ec14e677a5b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:35:48 -0500

    add 4257230/4257230.lua

---
+++ 4257230/4257230.lua
@@ -0,0 +1,11 @@
+﻿-- 4257230's Lua and Manifest Created by Hubcap Manifest
+-- Dunk Cookies
+-- Created: April 21, 2026 at 08:59:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4257230) -- Dunk Cookies
+-- MAIN APP DEPOTS
+addappid(4257231, 1, "413467b7c9037c47e402776fe411b26bca7489f56cb6158c0f08dfd7b79b7982") -- Depot 4257231
