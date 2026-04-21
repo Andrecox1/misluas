c36e5c24b5ae5b2513bed4d32d27b678c731a953
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:23:38 -0500

    add 3953340/3953340.lua

---
+++ 3953340/3953340.lua
@@ -0,0 +1,12 @@
+-- 3953340's Lua and Manifest Created by Hubcap Manifest
+-- Meow's Meow
+-- Created: April 21, 2026 at 09:55:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3953340) -- Meow's Meow
+-- MAIN APP DEPOTS
+addappid(3953341, 1, "e1b57e99ac299108c4bccd5d9cd4ded3713bf5d235e67f378b21dd35bbbd9830") -- Depot 3953341
+setManifestid(3953341, "5074694568209767395", 895255500)
