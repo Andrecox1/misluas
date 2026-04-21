c7a2b970cc6fec1d338024c904188ad33c4f9f2b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:48:17 -0500

    add 4157010/4157010.lua

---
+++ 4157010/4157010.lua
@@ -0,0 +1,12 @@
+-- 4157010's Lua and Manifest Created by Hubcap Manifest
+-- Last Shinobi
+-- Created: April 20, 2026 at 07:12:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4157010) -- Last Shinobi
+-- MAIN APP DEPOTS
+addappid(4157011, 1, "2675256cdafd782890ec7483e9e18eb87f5c15a8c4c5f9aeaa1a0b736f4f32c0") -- Depot 4157011
+setManifestid(4157011, "8130168483094340499", 997198720)
