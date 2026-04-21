bd4ba06b38cc2d52cc93160bcee8996c13c47f7c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:44:35 -0500

    add 3719550/3719550.lua

---
+++ 3719550/3719550.lua
@@ -0,0 +1,12 @@
+-- 3719550's Lua and Manifest Created by Hubcap Manifest
+-- The Solitary Existence of a Little Universe
+-- Created: April 20, 2026 at 11:01:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3719550) -- The Solitary Existence of a Little Universe
+-- MAIN APP DEPOTS
+addappid(3719551, 1, "7b8de0e1616162ae02de35d2b2befa1e6d0ba33d07e99fc40c76996819e0fd02") -- Depot 3719551
+setManifestid(3719551, "2983629973277259280", 287665924)
