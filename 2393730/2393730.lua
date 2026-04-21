ed63902748570842c79747e41790643680ad8391
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:39:29 -0500

    add 2393730/2393730.lua

---
+++ 2393730/2393730.lua
@@ -0,0 +1,14 @@
+-- 2393730's Lua and Manifest Created by Hubcap Manifest
+-- Legendary Pilots
+-- Created: April 21, 2026 at 08:32:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2393730) -- Legendary Pilots
+-- MAIN APP DEPOTS
+addappid(2393733, 1, "7923eebe886aaef069b1c57fe68e3053b2ea0725b636c959f33e70799e017101") -- Depot 2393733
+setManifestid(2393733, "5423679188125003697", 507153336)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2393731) -- Depot 2393731 (empty depot)
