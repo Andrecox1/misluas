d9748458f5b3ebc451179d1c3b8ea819238e3af5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:47:56 -0500

    add 1447700/1447700.lua

---
+++ 1447700/1447700.lua
@@ -0,0 +1,18 @@
+-- 1447700's Lua and Manifest Created by Hubcap Manifest
+-- ZpellCatz
+-- Created: April 20, 2026 at 07:10:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(1447700) -- ZpellCatz
+-- MAIN APP DEPOTS
+addappid(1447701, 1, "2a791560c303432ef0673eb981d5f39882c82135349d99775371c5d8ebb234aa") -- Depot 1447701
+setManifestid(1447701, "3343175164982027728", 277953624)
+addappid(1447702, 1, "804788648892f795f700830ad80331955c9cd31c9de03320dd973a5dafabc860") -- Depot 1447702
+setManifestid(1447702, "4904642598641466036", 296542206)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- ZpellCatz Supporter Pack (AppID: 2949670) - missing depot keys
+-- addappid(2949670)
