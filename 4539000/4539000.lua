270464fc803ce7b102e4027ef1ac2db89b1eb1c5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:43:49 -0500

    add 4539000/4539000.lua

---
+++ 4539000/4539000.lua
@@ -0,0 +1,12 @@
+-- 4539000's Lua and Manifest Created by Hubcap Manifest
+-- SoulSwap
+-- Created: April 21, 2026 at 14:12:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4539000) -- SoulSwap
+-- MAIN APP DEPOTS
+addappid(4539001, 1, "8187355496d0fa443d17e252bfca322e2e50dd475a185f6ea9d7a27a8515c93d") -- Depot 4539001
+setManifestid(4539001, "2954673665208400923", 1816564393)
