9bd860ea54df44f15a8c3dad543813b0fe326100
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:45:13 -0500

    add 2415050/2415050.lua

---
+++ 2415050/2415050.lua
@@ -0,0 +1,11 @@
+﻿-- 2415050's Lua and Manifest Created by Hubcap Manifest
+-- Neve
+-- Created: April 20, 2026 at 14:04:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2415050) -- Neve
+-- MAIN APP DEPOTS
+addappid(2415051, 1, "f9628e8b8c117ac9215aa64e965452b57ae00e9acdf043bae88a91480dfad5aa") -- Depot 2415051
