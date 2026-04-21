a0ab4cc10c2eed96a1737073ad61c51e9311bbe7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:47:41 -0500

    add 3497160/3497160.lua

---
+++ 3497160/3497160.lua
@@ -0,0 +1,14 @@
+-- 3497160's Lua and Manifest Created by Hubcap Manifest
+-- Sintopia Original Soundtrack
+-- Created: April 20, 2026 at 07:11:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3497160) -- Sintopia Original Soundtrack
+-- MAIN APP DEPOTS
+addappid(3497161, 1, "83c77f8aa5a7b4885e86f1ae5e5cdf1454b9a687eb391a8927863d2ef17108a5") -- Depot 3497161
+setManifestid(3497161, "2499397345649855967", 303247697)
+addappid(3497162, 1, "7aa42d95fc6d1d8201a9df7c6a3e8afd9f0a40b4680efde10d0a3592a239dbcd") -- Depot 3497162
+setManifestid(3497162, "8087615728976259675", 1522590171)
