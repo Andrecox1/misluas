28d3a3808a8d3371b9e1c069dda5a9bf8819a020
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:42:02 -0500

    add 3956480/3956480.lua

---
+++ 3956480/3956480.lua
@@ -0,0 +1,14 @@
+-- 3956480's Lua and Manifest Created by Hubcap Manifest
+-- Stocks: Main Force Simulator
+-- Created: April 21, 2026 at 05:38:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3956480) -- Stocks: Main Force Simulator
+-- MAIN APP DEPOTS
+addappid(3956481, 1, "882983bd40c589b9b97dd180b8d472447da9a2cf54700e35af59cebc03a5c4f9") -- Depot 3956481
+setManifestid(3956481, "7386847764948525589", 203877443)
+addappid(3956482, 1, "51c59b4e155fc2db3ae7c54974addecd342db0e1ecebe36795412de7d13d8856") -- Depot 3956482
+setManifestid(3956482, "7633197283869198381", 280358721)
