2c741b226366b87fe6ad77e16d6e32ae3455af6d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:34:02 -0500

    add 335300/335300.lua

---
+++ 335300/335300.lua
@@ -0,0 +1,22 @@
+-- 335300's Lua and Manifest Created by Hubcap Manifest
+-- DARK SOULS™ II: Scholar of the First Sin
+-- Created: September 29, 2025 at 06:35:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(335300) -- DARK SOULS™ II: Scholar of the First Sin
+-- MAIN APP DEPOTS
+addappid(335301, 1, "cf0e82e9ee3c3dd620a31184d8fb9d60c9e6641be15cdd5cc6da9960e822384a") -- FSDX11 Content
+setManifestid(335301, "2962083928887153929", 19302833578)
+addappid(335302, 1, "7a10a8968977653c95ac43dc8e7799f02750283f840618910466e264e85f97c1") -- DARK SOULS II SCHOLAR OF THE FIRST SIN JP Depot
+setManifestid(335302, "6445867072010600238", 19309800281)
+-- SHARED DEPOTS (from other apps)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (Shared from App 228980)
+setManifestid(228982, "6413394087650432851", 9688647)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
