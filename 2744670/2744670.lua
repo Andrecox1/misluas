c34a776200344d56bf63099fe17239e2a673e575
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:41:11 -0500

    add 2744670/2744670.lua

---
+++ 2744670/2744670.lua
@@ -0,0 +1,12 @@
+-- 2744670's Lua and Manifest Created by Hubcap Manifest
+-- Capital Dilemma
+-- Created: April 21, 2026 at 09:01:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2744670, 1, "59859947ad74eab28dbd8336ebd8c39e7b1a7872f4d862257a5b95cd7e082614") -- Capital Dilemma
+-- MAIN APP DEPOTS
+addappid(2744671, 1, "ac6df712299216985210aa23c0da1f41c6157e7bc2a76158acaf4244aac800fc") -- Depot 2744671
+setManifestid(2744671, "7174017604668854035", 181507849)
