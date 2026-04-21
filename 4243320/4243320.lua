7949e3293e77e4824fdd1832da16c26273203fce
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:48:13 -0500

    add 4243320/4243320.lua

---
+++ 4243320/4243320.lua
@@ -0,0 +1,20 @@
+-- 4243320's Lua and Manifest Created by Hubcap Manifest
+-- Hacksaw Massacre
+-- Created: April 20, 2026 at 07:12:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4243320) -- Hacksaw Massacre
+-- MAIN APP DEPOTS
+addappid(4243321, 1, "6b9151abb2d83ae065c029f78d210e428815e25fbb6f467b38bf9d69339889d2") -- Depot 4243321
+setManifestid(4243321, "1498073602657029331", 746638709)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4243322) -- Depot 4243322 (empty depot)
