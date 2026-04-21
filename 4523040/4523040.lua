7e916cb902860b4e5082ac0cdca51cffe721d88b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:39:33 -0500

    add 4523040/4523040.lua

---
+++ 4523040/4523040.lua
@@ -0,0 +1,12 @@
+-- 4523040's Lua and Manifest Created by Hubcap Manifest
+-- Judge SIM
+-- Created: April 21, 2026 at 08:57:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4523040) -- Judge SIM
+-- MAIN APP DEPOTS
+addappid(4523041, 1, "4c97377a1ab94cc8e81daf11b48431e66a503e2bba58c5a8408422da0cd6ee8d") -- Depot 4523041
+setManifestid(4523041, "548618815566983113", 931351206)
