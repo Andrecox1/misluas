96c358ef4fe9205c9a97825e7c7bb288b35f6973
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:47:51 -0500

    add 4545870/4545870.lua

---
+++ 4545870/4545870.lua
@@ -0,0 +1,14 @@
+-- 4545870's Lua and Manifest Created by Hubcap Manifest
+-- Empeti - Quest for the Pinnacle
+-- Created: April 20, 2026 at 07:09:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4545870) -- Empeti - Quest for the Pinnacle
+-- MAIN APP DEPOTS
+addappid(4545871, 1, "7ccec411dc597b19eab9ce29c79140becb05387c3da521f73d8b189069da2549") -- Depot 4545871
+setManifestid(4545871, "9109716118386214467", 392848411)
+addappid(4545872, 1, "16e2e0f18c37c6f099ec9e537181c2bad4b8b2550da66780de369f5fec684b58") -- Depot 4545872
+setManifestid(4545872, "3845053372436875847", 484148034)
