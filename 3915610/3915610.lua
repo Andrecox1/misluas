895589d15c51dde51a1c793aa2719d53cf618da0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:25:53 -0500

    add 3915610/3915610.lua

---
+++ 3915610/3915610.lua
@@ -0,0 +1,12 @@
+-- 3915610's Lua and Manifest Created by Hubcap Manifest
+-- Meow Palace: Forbidden City 
+-- Created: April 21, 2026 at 09:25:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3915610) -- Meow Palace: Forbidden City 
+-- MAIN APP DEPOTS
+addappid(3915611, 1, "b0d4e53bf660da2645f949c207238bb9767db265e7e0e421044e44fe6ece8225") -- Depot 3915611
+setManifestid(3915611, "8253361818951247144", 1669305464)
