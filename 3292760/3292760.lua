27ec30673488217701d1ac8ed33257f57fbb19c2
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:31:54 -0500

    add 3292760/3292760.lua

---
+++ 3292760/3292760.lua
@@ -0,0 +1,12 @@
+-- 3292760's Lua and Manifest Created by Hubcap Manifest
+-- The Lone Sword
+-- Created: April 21, 2026 at 09:45:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3292760) -- The Lone Sword
+-- MAIN APP DEPOTS
+addappid(3292761, 1, "708885fd893c11dbf1f2dd40d106a80ec514e66cf5e735e85ada694676dc0c5b") -- Depot 3292761
+setManifestid(3292761, "8629707601704495363", 548253548)
