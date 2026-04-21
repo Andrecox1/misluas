9a93f5d18a828cea84b8ba807db84de5e9e63f37
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:32:15 -0500

    add 4589880/4589880.lua

---
+++ 4589880/4589880.lua
@@ -0,0 +1,12 @@
+-- 4589880's Lua and Manifest Created by Hubcap Manifest
+-- Space Guild
+-- Created: April 21, 2026 at 09:22:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4589880) -- Space Guild
+-- MAIN APP DEPOTS
+addappid(4589881, 1, "27f6275f663162ad4e07747665d13820e58e101d97c4f81d8ce1e800b015e8ee") -- Depot 4589881
+setManifestid(4589881, "5274908825572260028", 5390997264)
