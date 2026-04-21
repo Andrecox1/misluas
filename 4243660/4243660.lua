5cd02993c8a8dd6997f8e7ae19ad147c7768b5ce
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:47:12 -0500

    add 4243660/4243660.lua

---
+++ 4243660/4243660.lua
@@ -0,0 +1,14 @@
+﻿-- 4243660's Lua and Manifest Created by Hubcap Manifest
+-- 颈椎病 Stiff Neck Playtest
+-- Created: April 20, 2026 at 07:09:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4243660) -- 颈椎病 Stiff Neck Playtest
+addtoken(4243660, "13408833863708311236")
+-- MAIN APP DEPOTS
+addappid(4243661, 1, "89a51fd18108e2fb3d40f93f57e46225e0dd21a317bba6d0f2fdca438c5144a6") -- Depot 4243661
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4243663) -- Depot 4243663 (empty depot)
