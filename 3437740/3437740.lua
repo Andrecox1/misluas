4ab69b69514be52d5e97ca287b0786891c333313
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:41:37 -0500

    add 3437740/3437740.lua

---
+++ 3437740/3437740.lua
@@ -0,0 +1,12 @@
+-- 3437740's Lua and Manifest Created by Hubcap Manifest
+-- Butcher's Creek Soundtrack
+-- Created: April 21, 2026 at 06:40:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3437740) -- Butcher's Creek Soundtrack
+-- MAIN APP DEPOTS
+addappid(3437741, 1, "53061af4941821983c806461a26d3352193085292b170daa3049cc4c96024609") -- Depot 3437741
+setManifestid(3437741, "7353371389446864032", 55591189)
