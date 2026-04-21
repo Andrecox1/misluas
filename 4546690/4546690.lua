d4ea3324250315046e1498fdd849c039161c4a37
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:39:38 -0500

    add 4546690/4546690.lua

---
+++ 4546690/4546690.lua
@@ -0,0 +1,12 @@
+-- 4546690's Lua and Manifest Created by Hubcap Manifest
+-- Snail Pit League
+-- Created: April 21, 2026 at 15:21:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4546690) -- Snail Pit League
+-- MAIN APP DEPOTS
+addappid(4546691, 1, "364cc210129cd404ed2bb7bc32ff86aec2873dffa9c82f9234d8f6955f0bc935") -- Depot 4546691
+setManifestid(4546691, "4914341370912668580", 115706712)
