84b2186ed59c1340e96f88d013dd5efc98d43516
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:44:51 -0500

    add 3268160/3268160.lua

---
+++ 3268160/3268160.lua
@@ -0,0 +1,14 @@
+-- 3268160's Lua and Manifest Created by Hubcap Manifest
+-- Survive On The Moon
+-- Created: April 20, 2026 at 12:31:49 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3268160) -- Survive On The Moon
+-- MAIN APP DEPOTS
+addappid(3268161, 1, "3e2f40c30c059133de22ba03e015197f77f3962ab760a210f3f3c3f2d1649cf4") -- Depot 3268161
+setManifestid(3268161, "4930118382245185090", 2297932538)
+addappid(3268162, 1, "790a34359e44cbaf810a96a1a07f685fd3639d8183a91161aee4495b10070be2") -- Depot 3268162
+setManifestid(3268162, "4385605582111890430", 2313724623)
