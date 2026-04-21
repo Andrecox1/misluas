87b3d0b7cc2c7eb3f6916fab8a89f2ea202193ef
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:37:32 -0500

    add 3985980/3985980.lua

---
+++ 3985980/3985980.lua
@@ -0,0 +1,12 @@
+-- 3985980's Lua and Manifest Created by Hubcap Manifest
+-- StudyHub
+-- Created: April 20, 2026 at 20:06:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3985980) -- StudyHub
+-- MAIN APP DEPOTS
+addappid(3985981, 1, "6aeaf32260dc9fe09b1c0a7d01b2e4b48bab9c19a387bad7a4bd4d1872f844b1") -- Depot 3985981
+setManifestid(3985981, "5343675615857569053", 3326074758)
