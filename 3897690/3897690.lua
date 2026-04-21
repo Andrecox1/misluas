b79b6c2264f928b484e96aeab832113e7ef2e33b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:41:18 -0500

    add 3897690/3897690.lua

---
+++ 3897690/3897690.lua
@@ -0,0 +1,12 @@
+-- 3897690's Lua and Manifest Created by Hubcap Manifest
+-- Fries Drive
+-- Created: April 21, 2026 at 08:55:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3897690, 1, "158a691748c9fc85363561a63e924fc38e43a6ba46e3f38e4330d9466bc2e145") -- Fries Drive
+-- MAIN APP DEPOTS
+addappid(3897691, 1, "a36e3aaae00c94c70ef466ff465c16d7f2485b77d3db62208361569f966df82d") -- Depot 3897691
+setManifestid(3897691, "7244760590009045382", 1925688298)
