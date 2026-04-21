1711691e36a1f429a944f53c1cfa63c017cb3528
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:31:59 -0500

    add 3265700/3265700.lua

---
+++ 3265700/3265700.lua
@@ -0,0 +1,12 @@
+-- 3265700's Lua and Manifest Created by Hubcap Manifest
+-- Vampire Crawlers
+-- Created: April 21, 2026 at 10:56:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3265700) -- Vampire Crawlers
+-- MAIN APP DEPOTS
+addappid(3265701, 1, "1e7525ea73ba61b029745bf11a16c0f88090e25fd8a3841cf7c06879636857f8") -- Depot 3265701
+setManifestid(3265701, "2378599380885040241", 1047674601)
