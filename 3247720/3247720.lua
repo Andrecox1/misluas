7e9ae991ed809aa7b4ca6cf8c5de4fb6e81595aa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:43:43 -0500

    add 3247720/3247720.lua

---
+++ 3247720/3247720.lua
@@ -0,0 +1,12 @@
+-- 3247720's Lua and Manifest Created by Hubcap Manifest
+-- Cosmos point
+-- Created: April 21, 2026 at 14:12:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3247720) -- Cosmos point
+-- MAIN APP DEPOTS
+addappid(3247721, 1, "0926b729bf88261599d647dede8e82e68f2e11d2f2f999feb30bb9da099cab12") -- Depot 3247721
+setManifestid(3247721, "5453006627207125243", 632369889)
