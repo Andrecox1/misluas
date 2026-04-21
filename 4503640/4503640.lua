d430588578970c710cf0949e4f97537967ae3434
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:43:54 -0500

    add 4503640/4503640.lua

---
+++ 4503640/4503640.lua
@@ -0,0 +1,16 @@
+-- 4503640's Lua and Manifest Created by Hubcap Manifest
+-- Alchemist's Garden
+-- Created: April 21, 2026 at 14:12:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4503640) -- Alchemist's Garden
+-- MAIN APP DEPOTS
+addappid(4503642, 1, "d85ecab084b75086d493e6bfe88c2d02710d9181100f2f5c08dbf7aaa546c07f") -- Depot 4503642
+setManifestid(4503642, "194856815964815292", 154916753)
+addappid(4503643, 1, "9576518f6cf8f1bae9c551d283fa0b22b4207b76ef2fe50b0567dd848fb8f154") -- Depot 4503643
+setManifestid(4503643, "2242365528052488125", 175364317)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4503641) -- Depot 4503641 (empty depot)
