339deeb553b2245a185278c69b8318fb810089f5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:37:40 -0500

    add 3661660/3661660.lua

---
+++ 3661660/3661660.lua
@@ -0,0 +1,16 @@
+-- 3661660's Lua and Manifest Created by Hubcap Manifest
+-- Tessel Run
+-- Created: April 20, 2026 at 19:23:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3661660) -- Tessel Run
+-- MAIN APP DEPOTS
+addappid(3661661, 1, "85d50f08da4187cd3e75535c676cb5048ef80b060dd8794e7200f74c94ee4286") -- Depot 3661661
+setManifestid(3661661, "7533270680269127373", 127635397)
+addappid(3661662, 1, "9875f7b006de709ab16604a708f50ce82dc12180859b7371e02898b2f811e555") -- Depot 3661662
+setManifestid(3661662, "4437145014569410257", 144281495)
+addappid(3661663, 1, "37dd19d2f158ade1558c6cd47c6eb5edfa2797fb2e475f7880c9ff8441cb5577") -- Depot 3661663
+setManifestid(3661663, "945470963204647167", 120942072)
