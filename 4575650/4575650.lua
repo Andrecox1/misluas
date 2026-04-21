11e73e3e34c2bafe0f9930acd695ad5c6ba06811
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:47:15 -0500

    add 4575650/4575650.lua

---
+++ 4575650/4575650.lua
@@ -0,0 +1,16 @@
+-- 4575650's Lua and Manifest Created by Hubcap Manifest
+-- Cosy Puzzle Cabin
+-- Created: April 20, 2026 at 07:12:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4575650) -- Cosy Puzzle Cabin
+-- MAIN APP DEPOTS
+addappid(4575651, 1, "a29e750a6f7affa7f6255fd665fb51d448b4e9f1d23ad4c16409f2bf173477e7") -- Depot 4575651
+setManifestid(4575651, "8625839572051644454", 336549473)
+addappid(4575652, 1, "01ec1ea9d0eb67cdec57698e80a5b93486e6395d1b07ffa2977d6ff901c8b857") -- Depot 4575652
+setManifestid(4575652, "3170870983847659094", 420521476)
+addappid(4575653, 1, "1477cc6cee5aa9516793fa8bf9172406173307fdabe38d34960fcfa29fab4f87") -- Depot 4575653
+setManifestid(4575653, "379468920916131190", 370210757)
