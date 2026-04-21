071b6dfcbc39c0d3b99252c61bba8b8d121e7f1b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:39:42 -0500

    add 4460470/4460470.lua

---
+++ 4460470/4460470.lua
@@ -0,0 +1,12 @@
+-- 4460470's Lua and Manifest Created by Hubcap Manifest
+-- Factory Balls Go!
+-- Created: April 21, 2026 at 10:53:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4460470) -- Factory Balls Go!
+-- MAIN APP DEPOTS
+addappid(4460471, 1, "9014487c2a8557d47895df43f26236f952b982c8910f4629c22629861a482fa7") -- Depot 4460471
+setManifestid(4460471, "1231803920553765608", 38703546)
