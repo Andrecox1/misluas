9ff6e22641678e047dc02dfd4327f3715fb89a46
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:41:49 -0500

    add 4110460/4110460.lua

---
+++ 4110460/4110460.lua
@@ -0,0 +1,12 @@
+-- 4110460's Lua and Manifest Created by Hubcap Manifest
+-- Movie Business
+-- Created: April 21, 2026 at 03:18:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4110460) -- Movie Business
+-- MAIN APP DEPOTS
+addappid(4110461, 1, "5202d527f07dc3470596a02b6a8d2af59fdb7c1a915ea74ce1c5db25a241e814") -- Depot 4110461
+setManifestid(4110461, "8589855048192920962", 2944427222)
