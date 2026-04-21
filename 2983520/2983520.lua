1f6d44192e9ac22adb16954e22713a7d81372811
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:44:30 -0500

    add 2983520/2983520.lua

---
+++ 2983520/2983520.lua
@@ -0,0 +1,12 @@
+-- 2983520's Lua and Manifest Created by Hubcap Manifest
+-- Tile Tales: Pirate
+-- Created: April 20, 2026 at 08:30:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2983520) -- Tile Tales: Pirate
+-- MAIN APP DEPOTS
+addappid(2983521, 1, "13c98dc807b448760baceb06af94a0ce8b78a53d41a7dfe13f05ce9be2264ba8") -- Depot 2983521
+setManifestid(2983521, "8371600897000746123", 618533532)
