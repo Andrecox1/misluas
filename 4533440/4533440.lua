5b05e2327c33fdb966cdbad5237a07aa3910f2b3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:38:37 -0500

    add 4533440/4533440.lua

---
+++ 4533440/4533440.lua
@@ -0,0 +1,12 @@
+-- 4533440's Lua and Manifest Created by Hubcap Manifest
+-- Locks 'n' Leaps
+-- Created: April 20, 2026 at 14:44:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4533440) -- Locks 'n' Leaps
+-- MAIN APP DEPOTS
+addappid(4533441, 1, "2086d106bafa704478327725844090eea43a6088203e2fa84e7f95f41abb20b7") -- Depot 4533441
+setManifestid(4533441, "7498632118243581261", 79311395)
