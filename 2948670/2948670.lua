fba986a34d0820e7364f7cc71f8f5b5057ef3db4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:46:49 -0500

    add 2948670/2948670.lua

---
+++ 2948670/2948670.lua
@@ -0,0 +1,14 @@
+-- 2948670's Lua and Manifest Created by Hubcap Manifest
+-- ZpellCatz Soundtrack
+-- Created: April 20, 2026 at 07:11:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2948670) -- ZpellCatz Soundtrack
+-- MAIN APP DEPOTS
+addappid(2948671, 1, "8136019a2adb953dce56e6f34fabcc9b39f6bfc4a76ee243514cababd42da5c5") -- Depot 2948671
+setManifestid(2948671, "5987922220057073343", 55298155)
+addappid(2948672, 1, "1a863c58a0eea7d9e1235d8ef6cdf9b192f975004c40e8a25bf986d71b751aa9") -- Depot 2948672
+setManifestid(2948672, "9084318232649872982", 185366736)
