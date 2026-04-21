12344bb268057249c3f819739cb8ee24deca3670
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:38:41 -0500

    add 4216670/4216670.lua

---
+++ 4216670/4216670.lua
@@ -0,0 +1,12 @@
+-- 4216670's Lua and Manifest Created by Hubcap Manifest
+-- Typing Farmer
+-- Created: April 20, 2026 at 17:16:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4216670) -- Typing Farmer
+-- MAIN APP DEPOTS
+addappid(4216671, 1, "a29fb95d81ca2716a5dd1119da1b52747844c57cade18b1a386673099cc7c665") -- Depot 4216671
+setManifestid(4216671, "4771604247979916730", 225024639)
