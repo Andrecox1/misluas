8a77b3000f8654a8c55600f34d5178d2490b776d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:41:54 -0500

    add 3138230/3138230.lua

---
+++ 3138230/3138230.lua
@@ -0,0 +1,12 @@
+-- 3138230's Lua and Manifest Created by Hubcap Manifest
+-- Lap of the Gods: A Visual Novel
+-- Created: April 21, 2026 at 08:45:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3138230) -- Lap of the Gods: A Visual Novel
+-- MAIN APP DEPOTS
+addappid(3138231, 1, "1e07f27f1c0ce381ecb51de37d269c76f3d6c46ea131b1584989da201e8364d1") -- Depot 3138231
+setManifestid(3138231, "7537607993147782358", 5573906685)
