da6980064117d92164e9eb57c4671097a354d29d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:37:48 -0500

    add 3879850/3879850.lua

---
+++ 3879850/3879850.lua
@@ -0,0 +1,12 @@
+-- 3879850's Lua and Manifest Created by Hubcap Manifest
+-- Catharage
+-- Created: April 20, 2026 at 18:04:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3879850) -- Catharage
+-- MAIN APP DEPOTS
+addappid(3879851, 1, "dd6251d87f25d6b9313565ad2ca17298766923a6d623c2c10b351620dc373bb0") -- Depot 3879851
+setManifestid(3879851, "5682862014945538007", 525217391)
