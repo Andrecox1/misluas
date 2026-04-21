a5f3de1bba5838b8b0abad29f66f4dcd345d4124
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:38:47 -0500

    add 4346760/4346760.lua

---
+++ 4346760/4346760.lua
@@ -0,0 +1,18 @@
+-- 4346760's Lua and Manifest Created by Hubcap Manifest
+-- Dark México
+-- Created: April 20, 2026 at 20:36:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4346760) -- Dark México
+-- MAIN APP DEPOTS
+addappid(4346761, 1, "564aa2bfe0707b13d1424bda74ef86cd3c941b98b57f511f36e536437b5a5d68") -- Depot 4346761
+setManifestid(4346761, "6772073327383626321", 3345260902)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
