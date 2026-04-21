a3d78378f7eaa1787f2f2c70314fd9223c84b004
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:32:04 -0500

    add 4540160/4540160.lua

---
+++ 4540160/4540160.lua
@@ -0,0 +1,12 @@
+-- 4540160's Lua and Manifest Created by Hubcap Manifest
+-- Claim Denied!
+-- Created: April 21, 2026 at 09:03:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4540160) -- Claim Denied!
+-- MAIN APP DEPOTS
+addappid(4540161, 1, "9babbe4c32c1778b384ab4aa834f4a7dc4608c208baf06805e8ccdd1eb5835e8") -- Depot 4540161
+setManifestid(4540161, "2324019221316755488", 1819714160)
