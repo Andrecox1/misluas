7a1e3902b7b9fa6adb5c03492b2b577af1177051
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:46:55 -0500

    add 2595040/2595040.lua

---
+++ 2595040/2595040.lua
@@ -0,0 +1,18 @@
+-- 2595040's Lua and Manifest Created by Hubcap Manifest
+-- Ship's Cat
+-- Created: April 20, 2026 at 07:11:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2595040) -- Ship's Cat
+-- MAIN APP DEPOTS
+addappid(2595041, 1, "8aef6422432ebd34d3c8b0fcb38cab87ee48f8072870b7fcd1e64cdf7f78fe1b") -- Depot 2595041
+setManifestid(2595041, "191379673587430393", 7654271002)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
