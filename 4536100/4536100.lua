6bac68eff31ad7c4f64349c910d95be67d424c0d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:45:09 -0500

    add 4536100/4536100.lua

---
+++ 4536100/4536100.lua
@@ -0,0 +1,12 @@
+-- 4536100's Lua and Manifest Created by Hubcap Manifest
+-- Beer Gunner
+-- Created: April 20, 2026 at 11:14:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4536100) -- Beer Gunner
+-- MAIN APP DEPOTS
+addappid(4536101, 1, "1fd1c433f421d970e0c3787939fc64ed6b75fb75c060580c6b92afb7e8403ab9") -- Depot 4536101
+setManifestid(4536101, "4193151296794197461", 74485396)
