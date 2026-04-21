f3e7280fa0f0002ecc6d038dd16e707c57f0f128
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:31:36 -0500

    add 4224800/4224800.lua

---
+++ 4224800/4224800.lua
@@ -0,0 +1,18 @@
+-- 4224800's Lua and Manifest Created by Hubcap Manifest
+-- Police Chief Simulator: Prologue - Early Days
+-- Created: April 21, 2026 at 15:21:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4224800) -- Police Chief Simulator: Prologue - Early Days
+-- MAIN APP DEPOTS
+addappid(4224801, 1, "9ed166e2210bcae3635cf911a3d7af5e1c474714dd33f46bfd7cbc48941cb9a8") -- Depot 4224801
+setManifestid(4224801, "5451683520468723668", 15075519633)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
