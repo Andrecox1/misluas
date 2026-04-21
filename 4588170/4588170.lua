9d0260897e5ef0381e5c0aa79080367eba6721ad
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-21 14:41:43 -0500

    add 4588170/4588170.lua

---
+++ 4588170/4588170.lua
@@ -0,0 +1,12 @@
+-- 4588170's Lua and Manifest Created by Hubcap Manifest
+-- Stay with Me
+-- Created: April 21, 2026 at 12:07:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4588170) -- Stay with Me
+-- MAIN APP DEPOTS
+addappid(4588171, 1, "f19de457ea5c270e6d4bf5634724628b61f349f305ff168af0030a6a9f008306") -- Depot 4588171
+setManifestid(4588171, "1622967409402437573", 1409347649)
