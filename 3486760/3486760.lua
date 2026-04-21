441151f0dbbe04843395796e70641e8fbc49f330
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:44:39 -0500

    add 3486760/3486760.lua

---
+++ 3486760/3486760.lua
@@ -0,0 +1,14 @@
+-- 3486760's Lua and Manifest Created by Hubcap Manifest
+-- Reignbreaker Soundtrack
+-- Created: April 20, 2026 at 12:05:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3486760) -- Reignbreaker Soundtrack
+-- MAIN APP DEPOTS
+addappid(3486761, 1, "79ba2c5eef89167f4961c89b3c81c74b7407fb2487bfa89a78c88ef9a95caa82") -- Depot 3486761
+setManifestid(3486761, "4943255833472218603", 70046553)
+addappid(3486762, 1, "fd87ce6b9ed674334d7dd954d01bf9610bf685ca1f535150cad951a3fa3fcc41") -- Depot 3486762
+setManifestid(3486762, "7358171473805440935", 411063238)
