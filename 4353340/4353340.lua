43c66bf713e2fbb1a80605ea686d43fbe10ccfd9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:38:31 -0500

    add 4353340/4353340.lua

---
+++ 4353340/4353340.lua
@@ -0,0 +1,12 @@
+-- 4353340's Lua and Manifest Created by Hubcap Manifest
+-- Super Meme Clicker
+-- Created: April 20, 2026 at 20:35:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4353340) -- Super Meme Clicker
+-- MAIN APP DEPOTS
+addappid(4353341, 1, "b2df00db04b4171b0b49b2e674c9cf049d07c75d106c46003b4e3f67afa6ddf2") -- Depot 4353341
+setManifestid(4353341, "602377981803015024", 2423196338)
