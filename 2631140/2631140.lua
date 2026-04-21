1beecb4e8036b48715b7500fee50a9bd62c03028
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-20 19:37:18 -0500

    add 2631140/2631140.lua

---
+++ 2631140/2631140.lua
@@ -0,0 +1,18 @@
+-- 2631140's Lua and Manifest Created by Hubcap Manifest
+-- Weed Shop 4: Highland
+-- Created: April 20, 2026 at 18:08:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2631140) -- Weed Shop 4: Highland
+-- MAIN APP DEPOTS
+addappid(2631141, 1, "9021674532278b88d8be5946d792752222af9e90caa6e7623719e519640abc4f") -- Depot 2631141
+setManifestid(2631141, "8253234798911980975", 8652197001)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
