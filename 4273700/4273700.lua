afe45efc70491a8410ae5c97d0c31b17bb6d79b2
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-28 21:49:27 -0500

    add 4273700/4273700.lua

---
+++ 4273700/4273700.lua
@@ -0,0 +1,15 @@
+-- 4273700's Lua and Manifest Created by Hubcap Manifest
+-- ShotSun Man & His Beauties
+-- Created: April 28, 2026 at 14:34:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4273700) -- ShotSun Man & His Beauties
+-- MAIN APP DEPOTS
+addappid(4273701, 1, "dfa4ce34db88ffecb5cf2516b1a267590ffcffebbb02321b83769694c3bf178f") -- Depot 4273701
+setManifestid(4273701, "7648467888071699222", 972530585)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4280260) -- DLC 4280260 (unreleased)
