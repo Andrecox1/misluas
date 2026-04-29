ad3812488ee8e0e84ebc496eae8b22839938cfbe
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-28 21:48:58 -0500

    add 3573410/3573410.lua

---
+++ 3573410/3573410.lua
@@ -0,0 +1,11 @@
+﻿-- 3573410's Lua and Manifest Created by Hubcap Manifest
+-- SHEROK's Case ;Recollection
+-- Created: April 28, 2026 at 14:40:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3573410) -- SHEROK's Case ;Recollection
+-- MAIN APP DEPOTS
+addappid(3573411, 1, "fa37619daaf4d26abcad3820046d8e78184ff3126924bec065a1a61352fbc8f8") -- Depot 3573411
