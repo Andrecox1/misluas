4eeb2a0595f0d859471e92d79e78017466d73ffa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-28 21:50:19 -0500

    add 4319270/4319270.lua

---
+++ 4319270/4319270.lua
@@ -0,0 +1,18 @@
+-- 4319270's Lua and Manifest Created by Hubcap Manifest
+-- Ambroise Niflette & the Gleaned Bell Demo
+-- Created: April 28, 2026 at 16:49:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4319270) -- Ambroise Niflette & the Gleaned Bell Demo
+-- MAIN APP DEPOTS
+addappid(4319271, 1, "edbf96ec4b1bdfdf51a3754268566b46d9959ad3fa9cbcbc9af35ba4f7a4d39e") -- Depot 4319271
+setManifestid(4319271, "5814209746017703926", 2268098546)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
