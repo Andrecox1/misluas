f995e59dbc01f05fb1c5784d8b4680eb179a50dd
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-28 21:49:58 -0500

    add 4377220/4377220.lua

---
+++ 4377220/4377220.lua
@@ -0,0 +1,15 @@
+-- 4377220's Lua and Manifest Created by Hubcap Manifest
+-- The charming and beautiful proprietress
+-- Created: April 28, 2026 at 14:35:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(4377220) -- The charming and beautiful proprietress
+-- MAIN APP DEPOTS
+addappid(4377221, 1, "759fe53193ceb0f4769b9c951821fae68cc18c4164b20a90bf4a5942f061e177") -- Depot 4377221
+setManifestid(4377221, "6001143871092903617", 836300543)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4580400) -- DLC 4580400 (unreleased)
