b17b13a0dba722a534849987d061c24205452f67
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-28 21:49:47 -0500

    add 3672990/3672990.lua

---
+++ 3672990/3672990.lua
@@ -0,0 +1,25 @@
+-- 3672990's Lua and Manifest Created by Hubcap Manifest
+-- Made in Glory Hole: Lewd Painters Never Die
+-- Created: April 28, 2026 at 14:34:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(3672990) -- Made in Glory Hole: Lewd Painters Never Die
+-- MAIN APP DEPOTS
+addappid(3672991, 1, "bd997965f38f2b125644d570be5cf388f666ec54350def053ef14a22335276d3") -- Depot 3672991
+setManifestid(3672991, "6736955687055268881", 1096384807)
+-- DLCS WITH DEDICATED DEPOTS
+-- Made in Glory Hole Adult Art Pack (AppID: 3775480)
+addappid(3775480)
+addappid(3775481, 1, "7feb481a22e28498fdaa9b7e7876353346324b19725efcb135bf8902dfed66f0") -- Made in Glory Hole Adult Art Pack - Depot 3775481
+setManifestid(3775481, "7581188629740738756", 54370030)
+-- Made in Glory Hole Guide (AppID: 3775490)
+addappid(3775490)
+addappid(3775492, 1, "998b3bd71808a63ecf47c45a2f310cbd647f2dd4f53229b06febd3571f2e2597") -- Made in Glory Hole Guide - Depot 3775492
+setManifestid(3775492, "4004168831392225672", 2148895)
+-- Made in Glory Hole Boobs Out (AppID: 3775500)
+addappid(3775500)
+addappid(3775501, 1, "a8d9573dd75c1990b2781af6d7f70c78af37c32a352fea2bffdabbb2551fde24") -- Made in Glory Hole Boobs Out - Depot 3775501
+setManifestid(3775501, "3236892846394797153", 40624861)
