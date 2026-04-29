48b1f71a8e2bfada65eca11937b3e2e4f4c93302
Andrecox1 <andresfelipear3357963@gmail.com>
2026-04-28 21:50:13 -0500

    add 4533090/4533090.lua

---
+++ 4533090/4533090.lua
@@ -0,0 +1,12 @@
+-- 4533090's Lua and Manifest Created by Hubcap Manifest
+-- Algorithm Prescription Demo
+-- Created: April 28, 2026 at 16:49:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4533090) -- Algorithm Prescription Demo
+-- MAIN APP DEPOTS
+addappid(4533091, 1, "c9ccbd28a6c2c6321353a45a01dbeb98c2fcc58ec0c577312caf7298b1bd1ffb") -- Depot 4533091
+setManifestid(4533091, "6594577340549188235", 1047528491)
