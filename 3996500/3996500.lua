f262a5b562a3244823b6d2a2cd91c14b3321cdd0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-10 10:14:20 -0500

    add 3996500/3996500.lua

---
+++ 3996500/3996500.lua
@@ -0,0 +1,12 @@
+-- 3996500's Lua and Manifest Created by Hubcap Manifest
+-- Polly's Puzzle Box Playtest
+-- Created: May 10, 2026 at 04:40:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3996500) -- Polly's Puzzle Box Playtest
+-- MAIN APP DEPOTS
+addappid(3996503, 1, "754a4747ffc0b75ad338045291a6f7a770612ca24ab17ccd3facac170f5c1d15") -- Depot 3996503
+setManifestid(3996503, "8699922659497249672", 2095056935)
