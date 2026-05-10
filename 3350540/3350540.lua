050b4f5a416981cd88130d2184e734173e7b6540
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-10 10:15:58 -0500

    add 3350540/3350540.lua

---
+++ 3350540/3350540.lua
@@ -0,0 +1,11 @@
+﻿-- 3350540's Lua and Manifest Created by Hubcap Manifest
+-- Mythical Matchmaker Demo
+-- Created: May 10, 2026 at 04:39:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3350540) -- Mythical Matchmaker Demo
+-- MAIN APP DEPOTS
+addappid(3350541, 1, "675f46427961ae78f39879c0b5a250e1f1b0f2b5cfb348be29fcc77ef0f9b14d") -- Depot 3350541
