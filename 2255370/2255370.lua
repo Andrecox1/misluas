7422e784cf2acf2e6d9b504da5f0200e98e88e0c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-10 10:16:08 -0500

    add 2255370/2255370.lua

---
+++ 2255370/2255370.lua
@@ -0,0 +1,20 @@
+-- 2255370's Lua and Manifest Created by Hubcap Manifest
+-- Directive 8020
+-- Created: May 10, 2026 at 10:54:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2255370) -- Directive 8020
+-- MAIN APP DEPOTS
+addappid(2255378, 1, "f60876d9515aa299bf429dc12729bc5cff3582e82dcc6846811997afbc752238") -- Depot 2255378
+setManifestid(2255378, "7820918036086506985", 38003958581)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2255379) -- Depot 2255379 (empty depot)
