6b03e6f165a3f26720afb3f60d0c1e784d71a36e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-10 10:15:52 -0500

    add 4334590/4334590.lua

---
+++ 4334590/4334590.lua
@@ -0,0 +1,14 @@
+-- 4334590's Lua and Manifest Created by Hubcap Manifest
+-- 息风谷战略 原声音乐集
+-- Created: May 10, 2026 at 04:40:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4334590) -- 息风谷战略 原声音乐集
+-- MAIN APP DEPOTS
+addappid(4334592, 1, "2a5b9653f2657ba167ccdca415f5fa93face237e10aa8dd8da1ae7ddceaa5872") -- Depot 4334592
+setManifestid(4334592, "6618581582143281595", 72290628)
+addappid(4334591, 1, "350670de5174646e5110f12a6f6213e2d9b6438e029dd13b9c27dcff3bcc1334") -- Depot 4334591
+setManifestid(4334591, "534403546266850802", 625541844)
