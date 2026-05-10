24358d2cc755f66f3e1126451b2105a3830e49d3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-10 10:16:03 -0500

    add 2109920/2109920.lua

---
+++ 2109920/2109920.lua
@@ -0,0 +1,12 @@
+-- 2109920's Lua and Manifest Created by Hubcap Manifest
+-- Sovereign Syndicate Demo
+-- Created: May 10, 2026 at 04:39:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2109920) -- Sovereign Syndicate Demo
+-- MAIN APP DEPOTS
+addappid(2109921, 1, "136cccf43b591e42f6f9cd2ee0112fabea36b86d0b64cbefafb0d5a17a643394") -- Depot 2109921
+setManifestid(2109921, "8448700359472009976", 2674574539)
