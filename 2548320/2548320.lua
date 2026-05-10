c6593b99b708ce067a95ba408c8867617aa69a99
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-10 10:13:58 -0500

    add 2548320/2548320.lua

---
+++ 2548320/2548320.lua
@@ -0,0 +1,12 @@
+-- 2548320's Lua and Manifest Created by Hubcap Manifest
+-- Imprecision Demo
+-- Created: May 10, 2026 at 04:40:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2548320) -- Imprecision Demo
+-- MAIN APP DEPOTS
+addappid(2548321, 1, "589599bea8abc2d3974980092a17611d5f8b5705f3f3fe7760a72b799f95d96a") -- Depot 2548321
+setManifestid(2548321, "8405687346050428396", 666765291)
