58bd62c21b727064f54cdf78b2e060d267afc354
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-10 10:14:30 -0500

    add 4086860/4086860.lua

---
+++ 4086860/4086860.lua
@@ -0,0 +1,12 @@
+-- 4086860's Lua and Manifest Created by Hubcap Manifest
+-- Aquanore
+-- Created: May 10, 2026 at 04:40:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4086860) -- Aquanore
+-- MAIN APP DEPOTS
+addappid(4086861, 1, "92096ff66d9f9c97f148ece859941cb2559561565af153771de4432255a7fc08") -- Depot 4086861
+setManifestid(4086861, "8987921626853295034", 120768708)
