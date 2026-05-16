f9ee75683f39c0c8e4b53627a8a060b78c24676e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-16 16:30:17 -0500

    add 3949040/3949040.lua

---
+++ 3949040/3949040.lua
@@ -0,0 +1,18 @@
+-- 3949040's Lua and Manifest Created by Hubcap Manifest
+-- RV There Yet?
+-- Created: April 20, 2026 at 10:51:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3949040, 1, "4af18553dbd752cd4d8084f32b7cdb37d59b1cc4d946360519d03546911b7e9c") -- RV There Yet?
+-- MAIN APP DEPOTS
+addappid(3949041, 1, "98435e5f712a11c88e455130e718254b2745a33f7443b2034d634f54af0da031") -- Depot 3949041
+setManifestid(3949041, "6388475935469519170", 2222971680)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
