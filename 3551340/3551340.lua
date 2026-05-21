3756351b1f3c9d3512b539063db4c69eb58ca2e6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-21 10:07:56 -0500

    add 3551340/3551340.lua

---
+++ 3551340/3551340.lua
@@ -0,0 +1,18 @@
+-- 3551340's Lua and Manifest Created by Hubcap Manifest
+-- Football Manager 26
+-- Created: April 02, 2026 at 05:29:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(3551340, 1, "052016d88278aada4bc94cc5d3fbef162105991acf65634e80e5fb0fb5fd3304") -- Football Manager 26
+-- MAIN APP DEPOTS
+addappid(3551341, 1, "a4f9c46a0797978fa0b278dac08a0f473872fc74c6b6d9bf45b30d56e33a42c7") -- Depot 3551341
+--setManifestid(3551341, "7759922098115499616", 10)
+addappid(3551342, 1, "31edad03a3daebca77afad83adb899f2ff6611d949738ad3d15e708da8f2c7ca") -- Depot 3551342
+--setManifestid(3551342, "2316048148012738763", 8042242041)
+addappid(3551343, 1, "5ff6a006dff3e4ea6364310a8390ffc5f4754c3928150ea5719009c21074a607") -- Depot 3551343
+--setManifestid(3551343, "379356579208158339", 8151598671)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3551410) -- Football Manager 26 In-Game Editor
