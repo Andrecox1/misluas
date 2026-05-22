449b7a8c44be715b0267df8279e5a96d3b217583
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-22 17:52:27 -0500

    add 3552140/3552140.lua

--- 3552140/3552140.lua
+++ 3552140/3552140.lua
@@ -1,3 +1,18 @@
-addappid(3552140)
-addappid(3552141,0,"8527bf46fe5454e6c8956a042fb1aee9a9792d7f4c73ebe85ab6924dfcc1c0fe")
---setManifestid(3552141,"1271640024634250041")
+-- 3552140's Lua and Manifest Created by Hubcap Manifest
+-- Retro Rewind - Video Store Simulator
+-- Created: May 15, 2026 at 04:35:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3552140) -- Retro Rewind - Video Store Simulator
+-- MAIN APP DEPOTS
+addappid(3552141, 1, "8527bf46fe5454e6c8956a042fb1aee9a9792d7f4c73ebe85ab6924dfcc1c0fe") -- Depot 3552141
+--setManifestid(3552141, "144037210659795879", 2138681161)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
