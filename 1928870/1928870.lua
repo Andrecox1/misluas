9018cc78354fa010b9f344ad1c32ea96e8bad745
Andrecox1 <andresfelipear3357963@gmail.com>
2026-05-27 18:41:02 -0500

    add 1928870/1928870.lua

---
+++ 1928870/1928870.lua
@@ -0,0 +1,14 @@
+-- 1928870's Lua and Manifest Created by Hubcap Manifest
+-- Minecraft Legends
+-- Created: September 30, 2025 at 00:55:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1928870) -- Minecraft Legends
+-- MAIN APP DEPOTS
+addappid(1928871, 1, "77e4ff98428ea4b5db197ad78037b2c0b2ab66399777fa157d1bc84e99fdb289") -- Depot 1928871
+setManifestid(1928871, "4592995720016410867", 12247047565)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2187940) -- Minecraft Legends Deluxe DLC
