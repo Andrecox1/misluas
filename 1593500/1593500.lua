05dc3dfdce8a73f071931d28349dab6d34a5f4aa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-04 22:42:30 -0500

    add 1593500/1593500.lua

--- 1593500/1593500.lua
+++ 1593500/1593500.lua
@@ -1,3 +1,16 @@
-addappid(1593500)
-addappid(1593501,0,"77f7390a115063bccc41240f71e0bf27d42c6a7c0424657756fe3f99f26a287a")
-setManifestid(1593501,"2535153423663269664")
+-- 1593500's Lua and Manifest Created by Hubcap Manifest
+-- God of War
+-- Created: September 29, 2025 at 14:54:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1593500) -- God of War
+-- MAIN APP DEPOTS
+addappid(1593501, 1, "77f7390a115063bccc41240f71e0bf27d42c6a7c0424657756fe3f99f26a287a") -- God of War Content
+--setManifestid(1593501, "2535153423663269664", 68973454991)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+--setManifestid(228988, "6645201662696499616", 29212173)
