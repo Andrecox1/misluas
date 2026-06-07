746987b3dae1e3f8db6230bb4759aad5ad05708b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-07 13:24:06 -0500

    add 526870/526870.lua

---
+++ 526870/526870.lua
@@ -0,0 +1,18 @@
+-- 526870's Lua and Manifest Created by Hubcap Manifest
+-- Satisfactory
+-- Created: June 02, 2026 at 12:23:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(526870, 1, "f8d1243ead37652491a8d284a79ee2b814ad878208ec9276f3d5d8b1528a11e7") -- Satisfactory
+-- MAIN APP DEPOTS
+addappid(526871, 1, "ae0ab673da3c9d0f444b1cd61fe8f2235d43e6c0401684a78f4fbdfc8f0d9bc1") -- ApatoaCorp Content
+setManifestid(526871, "5430847489370887438", 30330131848)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3170070) -- Early Access supporter pack
