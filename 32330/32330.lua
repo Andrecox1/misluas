eb114d7458abe33433daa5892de0b0b33675716e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-07 18:00:25 -0500

    add 32330/32330.lua

--- 32330/32330.lua
+++ 32330/32330.lua
@@ -9,12 +9,12 @@
 addappid(32330, 1, "ae7bf39df04d53a06d1467cd450d0ae298f0aa1b5237264fadc334d08e54ed6a") -- LEGOⓇ Indiana Jones™: The Original Adventures
 -- MAIN APP DEPOTS
 addappid(32331, 1, "3f030450b19be8520dd1d7dd2b76b84ccaa0e2bef6df534d098d136e7f5f80cd") -- LEGO Indiana Jones: The Original Adventures_content
---setManifestid(32331, "5952564723665979214", 3964356845)
+setManifestid(32331, "5952564723665979214", 3964356845)
 addappid(32332, 1, "9f578d10670fb80d7fd902ddbb192a96aec96613a52dd50c58335be09e75be56") -- LEGO Indiana Jones: The Original Adventures Spanish
---setManifestid(32332, "2501864487432370764", 1323887)
+setManifestid(32332, "2501864487432370764", 1323887)
 addappid(32333, 1, "40a083260fdecac3b811ddb1ad4853c80c3e0cf4c368fc2658f74114c0d11261") -- LEGO Indiana Jones: The Original Adventures German
---setManifestid(32333, "6501304229755146686", 1320741)
+setManifestid(32333, "6501304229755146686", 1320741)
 addappid(32335, 1, "2658a2a617662f6f40983442ad390aeba625b569e3a9ad52a0e80f203521abd6") -- LEGO Indiana Jones: The Original Adventures Italian
---setManifestid(32335, "1217194974168217355", 1347126)
+setManifestid(32335, "1217194974168217355", 1347126)
 addappid(32336, 1, "a1ca8cc26eead94b7be9d756564118d2d87e66ebc22ae5001f7ba0e17b187831") -- LEGO Indiana Jones: The Original Adventures Danish
---setManifestid(32336, "7921700809484652698", 1595094)
+setManifestid(32336, "7921700809484652698", 1595094)
