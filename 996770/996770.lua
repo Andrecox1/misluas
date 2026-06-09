88c4f7b93faa14f9e4f614c38e6e986cf7714cb6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-09 10:40:05 -0500

    add 996770/996770.lua

---
+++ 996770/996770.lua
@@ -0,0 +1,19 @@
+-- 996770's Lua and Manifest Created by Hubcap Manifest
+-- Moving Out
+-- Created: September 30, 2025 at 02:04:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(996770) -- Moving Out
+-- MAIN APP DEPOTS
+addappid(996771, 1, "249bfc47285eca22bef9935db1c8a613f07db0a16fe05e0ff608510221013721") -- Moving Out Content
+--setManifestid(996771, "5191439892301511493", 731070955)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+--setManifestid(228986, "8782296191957114623", 29759921)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1249280) -- Moving Out - The Employees of the Month Pack
+addappid(1464740) -- Moving Out - Movers in Paradise
