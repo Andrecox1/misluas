ee98bd45d0a2ce4a77a81999f696dba4bf9318b8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-09 10:40:19 -0500

    add 1641700/1641700.lua

---
+++ 1641700/1641700.lua
@@ -0,0 +1,19 @@
+-- 1641700's Lua and Manifest Created by Hubcap Manifest
+-- Moving Out 2
+-- Created: October 09, 2025 at 09:22:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(1641700) -- Moving Out 2
+-- MAIN APP DEPOTS
+addappid(1641701, 1, "87425c1557b863d4091a52a61a146d1634d5de17464e6e2107c05b2806ffd4ce") -- Depot 1641701
+--setManifestid(1641701, "9050846351133172747", 1425579272)
+-- DLCS WITH DEDICATED DEPOTS
+-- Moving Out 2 - Digital Artbook (AppID: 2502500)
+addappid(2502500)
+addappid(2502500, 1, "4f8bd8296b8af0d840f53c6d1f93d57613db76ade437a0105e5c4fabc4475410") -- Moving Out 2 - Digital Artbook - Depot 2502500
+--setManifestid(2502500, "3137666977931661277", 168222902)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2410580) -- Moving Out 2 - F.A.R.Tastic Four DLC
