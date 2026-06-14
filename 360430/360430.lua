bbfe3bbbd836bf69cb75050e77434da7870d746d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-14 17:37:01 -0500

    add 360430/360430.lua

---
+++ 360430/360430.lua
@@ -0,0 +1,55 @@
+-- 360430's Lua and Manifest Created by Hubcap Manifest
+-- Mafia III: Definitive Edition
+-- Created: September 29, 2025 at 23:17:23 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 15
+-- Total DLCs: 6
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(360430) -- Mafia III: Definitive Edition
+-- MAIN APP DEPOTS
+addappid(360431, 1, "4a3e72723286eab2b8e99756608b5e7e189945dac142d4db09e2641bcc60eae8") -- Retail Content
+--setManifestid(360431, "3532314879832368080", 52354034025)
+addappid(360432, 1, "1244262974460b4777d46674d5b9c0b4562810f162bf0dd526d29db46cfd6f40") -- Retail Executables
+--setManifestid(360432, "8773375485519535397", 993902828)
+addappid(360438, 1, "9cc9b741c2bf74a35787dab0db3c733085d867eb1b60ae5f2f5ddce99c3facdd") -- Mac Retail Content
+--setManifestid(360438, "2467089648767970112", 47180472108)
+addappid(360439, 1, "8468c3e4cc98f66cdb0829e2da136c7ec2a00d2ccffc2c6c6753040cab176213") -- Mac Retail Executables
+--setManifestid(360439, "2746032780764026926", 384297511)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+--setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- .NET 4.5.2 Redist (Shared from App 228980)
+--setManifestid(229004, "5220958916987797232", 70000464)
+-- DLCS WITH DEDICATED DEPOTS
+-- Mafia III Judge, Jury and Executioner Weapons Pack (AppID: 511890)
+addappid(511890)
+addappid(511890, 1, "629e0b865b1822be42ca68d57c673a35661ce023442fded770e0388c63355c8d") -- Mafia III Judge, Jury and Executioner Weapons Pack - Judge, Jury and Executioner Weapons Pack (511890)
+--setManifestid(511890, "454834834847228076", 4285)
+addappid(511898, 1, "fcbcd4dd944fbac6e43ad4e5a24b5b49232353293a7afa5cdf2a9340b213badf") -- Mafia III Judge, Jury and Executioner Weapons Pack - Mac Judge, Jury, and Executioner Weapons Pack
+--setManifestid(511898, "5304880233194808812", 4285)
+-- Mafia III Family Kick-Back Pack (AppID: 511892)
+addappid(511892)
+addappid(511892, 1, "d42487684b1cfe24dda0c1b58dee70794bf0ff8e154d4c780e2b1b2fa06dd24f") -- Mafia III Family Kick-Back Pack - Family Kick-Back Pack (511892)
+--setManifestid(511892, "992891886538164515", 6352)
+addappid(511899, 1, "6b42977a58e581138a7b83a7b0f376971cbef0938280a627ad62d0af4ad32374") -- Mafia III Family Kick-Back Pack - Mac Family Kick-Back Pack
+--setManifestid(511899, "9009976994094612939", 6352)
+-- Mafia III Faster, Baby (AppID: 598020)
+addappid(598020)
+addappid(598020, 1, "bfbf614d0bf361b7a9a01c2131b1562dcda664bdaf0f0cf3327e6a00dfbdd4f5") -- Mafia III Faster, Baby - Mafia III: Faster, Baby! (598020) Depot
+--setManifestid(598020, "8199463638102391193", 2911824009)
+addappid(598029, 1, "d31679d286f13684adf1234b8418621ea8722da3ab886b992ae6f8dbf539576b") -- Mafia III Faster, Baby - Mac Faster, Baby!
+--setManifestid(598029, "4685441623310950674", 2741756974)
+-- Mafia III Stones Unturned (AppID: 598021)
+addappid(598021)
+addappid(598021, 1, "84b841715b6a1064475d21d2ee14302cdbf40ff3bbdd0f6a8c2382f7bd3355fe") -- Mafia III Stones Unturned - Mafia III: Stones Unturned (598021) Depot
+--setManifestid(598021, "5920706206030683877", 1792825557)
+-- Mafia III Sign of the Times (AppID: 598022)
+addappid(598022)
+addappid(598022, 1, "f2d9fd1f15e2ee238b32f524799d913c8166a4d09498a68d9451a78c44d8af3f") -- Mafia III Sign of the Times - Mafia III: Sign of the Times (598022) Depot
+--setManifestid(598022, "4632933338714674769", 1061594276)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(511891) -- Mafia III Season Pass
