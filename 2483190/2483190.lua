0cd361e2af2349274b93b66a4c0588c249390d29
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-17 21:50:47 -0500

    add 2483190/2483190.lua

--- 2483190/2483190.lua
+++ 2483190/2483190.lua
@@ -1,41 +1,37 @@
 -- 2483190's Lua and Manifest Created by Hubcap Manifest
 -- Forza Horizon 6
--- Created: May 30, 2026 at 12:14:43 EDT
+-- Created: June 16, 2026 at 19:47:10 EDT
 -- Website: https://hubcapmanifest.com/
 -- Total Depots: 3
--- Total DLCs: 13 (3 excluded)
+-- Total DLCs: 16 (3 excluded)
 -- Shared Depots: 2
 
 -- MAIN APPLICATION
 addappid(2483190, 1, "bca2f0f9973d929c2c6bae884058b8bf7934e9ecc8ff34d584450dfbb3215040") -- Forza Horizon 6
-addtoken(2483190, "2264898939686399139")
 -- MAIN APP DEPOTS
 addappid(2483191, 1, "f2a09f0a31b4df681ec0d2d572f368dfa4cd6c37ae67ebdfd591cfbe1334fcd9") -- Depot 2483191
-setManifestid(2483191, "4540831676847287897", 167126587531)
+--setManifestid(2483191, "2737887849937269827", 167126579000)
 -- SHARED DEPOTS (from other apps)
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
-setManifestid(228989, "3514306556860204959", 39590283)
+--setManifestid(228989, "3514306556860204959", 39590283)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
-setManifestid(228990, "1829726630299308803", 102931551)
+--setManifestid(228990, "1829726630299308803", 102931551)
 -- DLCS WITHOUT DEDICATED DEPOTS
 addappid(4439260) -- Forza Horizon 6 Welcome Pack
-addtoken(4439260, "5416694940851378001")
 addappid(4439270) -- Forza Horizon 6 Premium VIP
-addtoken(4439270, "10281334471861719873")
 addappid(4439280) -- Forza Horizon 6 Time Attack Car Pack
-addtoken(4439280, "4638210807385755524")
 addappid(4439300) -- Forza Horizon 6 Ferrari J50
 addappid(4439750) -- Forza Horizon 6 Treasure Map
 addappid(4439790) -- Forza Horizon 6 Car Pass
-addtoken(4439790, "17274680636771417474")
 addappid(4439800) -- Forza Horizon 6 1990 Nissan 12 Skyline GT-R (BNR32 Gr.A) JTC
 addappid(4439810) -- Forza Horizon 6 2024 Koenigsegg Gemera
+addappid(4439820) -- Forza Horizon 6 1972 Datsun 269 Attacking the Clock Racing 240Z All Carbon Hill Climb Beast
+addappid(4439830) -- Forza Horizon 6 2008 Honda Civic Type R (FD2)
 addappid(4440380) -- Forza Horizon 6 VIP Membership
-addtoken(4440380, "13331842867689162222")
 addappid(4444140) -- Forza Horizon 6 TBDS2
 addappid(4444150) -- Forza Horizon 6 1962 Peel P50 Trolli Edition
 addappid(4520350) -- Forza Horizon 6 Expansions Bundle
-addtoken(4520350, "7048682700970800609")
+addappid(4520360) -- Forza Horizon 6 Premium Upgrade Bundle
 addappid(4562050) -- Forza Horizon 6 Crunchyroll Voucher
 -- EXCLUDED DLCS:
 -- UNRELEASED DLCS (COMMENTED OUT)
