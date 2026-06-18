daf1dbe2d112011c0bb18836a9cf6c40a394e16f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-18 12:51:48 -0500

    add 521890/521890.lua

---
+++ 521890/521890.lua
@@ -0,0 +1,18 @@
+-- 521890's Lua and Manifest Created by Hubcap Manifest
+-- Hello Neighbor
+-- Created: September 29, 2025 at 17:16:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(521890, 1, "a435c6dd7fc5ad1c4fda215293efd2efd71f9c63e48e9eb0c96b9de022379587") -- Hello Neighbor
+-- MAIN APP DEPOTS
+addappid(521895, 1, "dfdc4fe90929b42105c21652ff271204a22e73f3e8dc82bb776c33551a460cf6") -- Hello Neighbor Alpha 4
+--setManifestid(521895, "872403870045558848", 5141130307)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+--setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
