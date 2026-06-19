f90757430a9f724e1ea61f090f87b63ce825d585
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-19 13:34:22 -0500

    add 3643170/3643170.lua

---
+++ 3643170/3643170.lua
@@ -0,0 +1,12 @@
+-- 3643170's Lua and Manifest Created by Hubcap Manifest
+-- Roadside Research
+-- Created: May 22, 2026 at 07:48:08 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3643170, 1, "489dc625b46f28cdb73ac15226c22e793cf580ad0e737df89623e257bb6a609c") -- Roadside Research
+-- MAIN APP DEPOTS
+addappid(3643171, 1, "7d044644abebe1ae2cc08b7c0f2e4bb004c3616d81ffe2b4bf0e8ed2107fda67") -- Depot 3643171
+--setManifestid(3643171, "4719770257904384767", 1805048922)
