7a48dcaed1c0387e7f4d2d7e5a1207925d541479
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-21 17:53:04 -0500

    Add files via upload

---
+++ 254700/254700.lua
@@ -0,0 +1,33 @@
+-- 254700's Lua and Manifest Created by Hubcap Manifest
+-- Resident Evil 4 (2005)
+-- Created: January 18, 2026 at 17:47:00 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(254700, 1, "ccc758ff227055ea493fd214673a52f7c733298a582dda0430d43b81cbbf5e6a") -- Resident Evil 4 (2005)
+-- MAIN APP DEPOTS
+addappid(254702, 1, "058a874bbbf796de8ae730aad32795572400bfc1404392b872a3302908e81889") -- Resident Evil 4 German
+setManifestid(254702, "7568911904646837022", 11890989716)
+addappid(254703, 1, "ed74776e6c965e3350288b1d290aae4e39c94c62e8173e014c085027e5b9e07d") -- Resident Evil 4 Japanese
+setManifestid(254703, "3343076949474248393", 11890986652)
+addappid(254701, 1, "138bb1e5f555bb1881214eced9844b83b643695fc8726786a42b42a3f5c27902") -- Resident Evil 4 WW
+setManifestid(254701, "6999377187357974260", 11890989716)
+addappid(254704, 1, "870c561ed9cbeb8f5733db64c240b45b609f1b8440890d999af03e206733d31d") -- resident evil 4 / biohazard 4 Test Depot
+setManifestid(254704, "7927591321049438693", 13905876112)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Resident Evil 4 Soundtrack (AppID: 272260)
+addappid(272260)
+addappid(272260, 1, "887fbc832b41f133430f20d78b365b07c724dd04c51ddcc48f879d907c4fd055") -- Resident Evil 4 Soundtrack - Resident Evil 4 Soundtrack (272260) Depot
+setManifestid(272260, "8170509752846139490", 334315892)
+-- Resident Evil 4 Art Book (AppID: 272261)
+addappid(272261)
+addappid(272261, 1, "e899c415aef8a533e80abb6c72b6bf3602f0b9b064c1bbb381a655667c576336") -- Resident Evil 4 Art Book - Resident Evil 4 Art Book (272261) Depot
+setManifestid(272261, "4849919482799769104", 285207900)
