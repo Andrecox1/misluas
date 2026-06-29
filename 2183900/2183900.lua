069e22a248e98c2fd0a8e8205bdf9d3f268e0085
Andrecox1 <andresfelipear3357963@gmail.com>
2026-06-29 11:04:45 -0500

    add 2183900/2183900.lua

--- 2183900/2183900.lua
+++ 2183900/2183900.lua
@@ -1,27 +1,48 @@
-addappid(2183900)
-addappid(3202690,0,"00c47f82b08192958b11307abb87c755003eb8dfae75063373e379c296b26083")
---setManifestid(3202690,"1991092350350856742")
-addappid(3212040,0,"e3818ce0f203d78eed2647e6364bd19a702fd8d7a01230f77a038e0fd26b0ffb")
---setManifestid(3212040,"7457655780363166458")
-addappid(2183901,0,"8df236a6a5084039477362b31157e7e0eb66554a744762d6782c544ab64d0c4e")
---setManifestid(2183901,"8936086223865720696")
-addappid(3871340)
-addappid(3274400)
-addappid(3274440)
-addappid(2792180)
-addappid(3871330)
-addappid(3871320)
-addappid(2792190)
-addappid(3871360)
-addappid(3871350)
-addappid(3871370)
-addappid(2792150)
-addappid(3274430)
-addappid(3212020)
-addappid(3274410)
-addappid(2792280)
-addappid(2792140)
-addappid(3871380)
-addappid(2486150)
-addappid(2792170)
-addappid(4247850)
+-- 2183900's Lua and Manifest Created by Hubcap Manifest
+-- Warhammer 40,000: Space Marine 2
+-- Created: June 09, 2026 at 04:56:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 22
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2183900, 1, "227e48320b1cf15063a473da684cea19ebcd76548c0d50a505cdd0a78864ed0a") -- Warhammer 40,000: Space Marine 2
+addtoken(2183900, "12779763340560861411")
+-- MAIN APP DEPOTS
+addappid(2183901, 1, "8df236a6a5084039477362b31157e7e0eb66554a744762d6782c544ab64d0c4e") -- Depot 2183901
+setManifestid(2183901, "4683060309526592401", 94668370878)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Warhammer 40,000 Space Marine 2 - 4K Texture Pack (AppID: 3202690)
+addappid(3202690)
+addappid(3202690, 1, "00c47f82b08192958b11307abb87c755003eb8dfae75063373e379c296b26083") -- Warhammer 40,000 Space Marine 2 - 4K Texture Pack - Depot 3202690
+setManifestid(3202690, "3650187865694441064", 126493059600)
+-- Warhammer 40,000 Space Marine 2 - Digital Artbook (AppID: 3212040)
+addappid(3212040)
+addappid(3212040, 1, "e3818ce0f203d78eed2647e6364bd19a702fd8d7a01230f77a038e0fd26b0ffb") -- Warhammer 40,000 Space Marine 2 - Digital Artbook - Depot 3212040
+setManifestid(3212040, "7457655780363166458", 192137587)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2486150) -- Warhammer 40,000 Space Marine 2 - Macragges Chosen
+addappid(2792140) -- Warhammer 40,000 Space Marine 2 - Ultramarines Champion Pack
+addappid(2792150) -- Warhammer 40,000 Space Marine 2 - Ultramarines Cosmetic Pack
+addappid(2792170) -- Warhammer 40,000 Space Marine 2 - Dark Angels Chapter Pack
+addappid(2792180) -- Warhammer 40,000 Space Marine 2 - Space Wolves Chapter Pack
+addappid(2792190) -- Warhammer 40,000 Space Marine 2 - White Scars Chapter Pack
+addappid(2792280) -- Warhammer 40,000 Space Marine 2 - Season Pass
+addappid(3274400) -- Warhammer 40,000 Space Marine 2 - Raven Guard Cosmetic Pack
+addappid(3274410) -- Warhammer 40,000 Space Marine 2 - Salamanders Champion Pack
+addappid(3274430) -- Warhammer 40,000 Space Marine 2 - Imperial Fists Champion Pack
+addappid(3274440) -- Warhammer 40,000 Space Marine 2 - Blood Angels Cosmetic Pack
+addappid(3871320) -- Warhammer 40,000 Space Marine 2 - Season Pass 2
+addappid(3871330) -- Warhammer 40,000 Space Marine 2 - Black Templars Champion Pack
+addappid(3871340) -- Warhammer 40,000 Space Marine 2 - Imperial Fists Cosmetic Pack
+addappid(3871350) -- Warhammer 40,000 Space Marine 2 - Blood Angels Champion Pack
+addappid(3871360) -- Warhammer 40,000 Space Marine 2 - Salamanders Cosmetic Pack
+addappid(3871370) -- Warhammer 40,000 Space Marine 2 - Raven Guard Champion Pack
+addappid(3871380) -- Warhammer 40,000 Space Marine 2 - Carcharodons Cosmetic Pack
+addappid(3871390) -- Warhammer 40,000 Space Marine 2 - Iron Hands Chapter Pack
+addtoken(3871390, "15555146775881414615")
+addappid(4247850) -- Warhammer 40,000 Space Marine 2 - Chapter Voice Pack 1
