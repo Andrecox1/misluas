c1ae27b8db45823ef09577d1802d499e69e8b565
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-02 00:50:23 -0500

    add 3724610/3724610.lua

---
+++ 3724610/3724610.lua
@@ -0,0 +1,12 @@
+-- 3724610's Lua and Manifest Created by Hubcap Manifest
+-- 星际美食家 Playtest
+-- Created: July 02, 2026 at 01:20:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3724610) -- 星际美食家 Playtest
+-- MAIN APP DEPOTS
+addappid(3724616, 1, "acb5cf02076e87f543716739c444d1e1ccbb8d910e9e43990adf2c177cb8cb42") -- Depot 3724616
+setManifestid(3724616, "3722805534673992678", 8199034273)
