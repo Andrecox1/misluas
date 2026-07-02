332fe8b087545e5b3f0ee3a5494478ec01776459
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-02 00:50:29 -0500

    add 4820660/4820660.lua

---
+++ 4820660/4820660.lua
@@ -0,0 +1,12 @@
+-- 4820660's Lua and Manifest Created by Hubcap Manifest
+-- BREKEKEKEX Demo
+-- Created: July 01, 2026 at 21:59:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4820660) -- BREKEKEKEX Demo
+-- MAIN APP DEPOTS
+addappid(4820661, 1, "da4b7069d0702d237415637448ecc9a73e6524244507f85d3897560d05f5361c") -- Depot 4820661
+setManifestid(4820661, "8742943490482159796", 1726404010)
