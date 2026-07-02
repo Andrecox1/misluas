3fdfbc21a625fb9c93fcbb8c8fc29444bc9dfb37
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-02 01:02:32 -0500

    add 4734440/4734440.lua

---
+++ 4734440/4734440.lua
@@ -0,0 +1,12 @@
+-- 4734440's Lua and Manifest Created by Hubcap Manifest
+-- City of Atlantis Demo
+-- Created: July 01, 2026 at 20:43:30 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4734440) -- City of Atlantis Demo
+-- MAIN APP DEPOTS
+addappid(4734441, 1, "20f367352eda23b11c8ac6d6c8ec708180c60e5728393cbdc894d09d54ab3a7f") -- Depot 4734441
+setManifestid(4734441, "1409798884164095159", 5577099401)
