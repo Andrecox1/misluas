8311ecf98614b9da9474d749971109d5b2a005dd
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:36 -0500

    add 4515810/4515810.lua

---
+++ 4515810/4515810.lua
@@ -0,0 +1,12 @@
+-- 4515810's Lua and Manifest Created by Hubcap Manifest
+-- Prospice Playtest
+-- Created: July 03, 2026 at 07:44:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4515810) -- Prospice Playtest
+-- MAIN APP DEPOTS
+addappid(4515811, 1, "57b8cb5e987f36500da2adf2dc6413715392021e50c110903ff38050360f966e") -- Depot 4515811
+setManifestid(4515811, "7934868102857752155", 1952355546)
