4c041a1d95f111f95961a06841f15774bedefaff
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:07:20 -0500

    add 699130/699130.lua

--- 699130/699130.lua
+++ 699130/699130.lua
@@ -1,19 +1,19 @@
--- 699130's Lua and Manifest Created by Morrenus
+-- 699130's Lua and Manifest Created by Hubcap Manifest
 -- World War Z
--- Created: March 11, 2026 at 07:50:25 EDT
--- Website: https://manifest.morrenus.xyz/
+-- Created: June 30, 2026 at 08:19:12 EDT
+-- Website: https://hubcapmanifest.com/
 -- Total Depots: 2
--- Total DLCs: 20
+-- Total DLCs: 22
 -- Shared Depots: 1
 
 -- MAIN APPLICATION
 addappid(699130, 1, "cea3b55dec0169ec0c0c08823356b8c1ad1b3b5e5b56f00dd2bd9459abff53dd") -- World War Z
 -- MAIN APP DEPOTS
 addappid(699131, 1, "8e1c37df6ec32e30f09ea256f8732eb98b7955ec82c51c49960f0d078d057fbb") -- World War Z Content
---setManifestid(699131, "666683415766431411", 77824136960)
+setManifestid(699131, "3615152122155594123", 78011282176)
 -- SHARED DEPOTS (from other apps)
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
---setManifestid(228988, "6645201662696499616", 29212173)
+setManifestid(228988, "6645201662696499616", 29212173)
 -- DLCS WITHOUT DEDICATED DEPOTS
 addappid(1640650) -- World War Z Explorer Weapons Pack
 addappid(1708020) -- World War Z Zeke Hunter Weapons Pack
@@ -35,5 +35,7 @@ addappid(3930030) -- World War Z Vibrant Halloween Skin Pack
 addappid(3930040) -- World War Z Raging Phoenix Skin Pack
 addappid(4057370) -- World War Z x The Walking Dead
 addappid(4197300) -- World War Z - Lunar New Year Skin Pack
+addappid(4383600) -- World War Z - Chibi Bunko Skin Pack
+addappid(4383610) -- World War Z - Chibi Daniela Skin Pack
 -- EMPTY DEPOTS (no content on any branch)
 -- addappid(699132) -- Depot 699132 (empty depot)
