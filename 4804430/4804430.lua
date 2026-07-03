41cf702db89f5ea7f077f279a4809231dc90b435
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:31 -0500

    add 4804430/4804430.lua

---
+++ 4804430/4804430.lua
@@ -0,0 +1,12 @@
+-- 4804430's Lua and Manifest Created by Hubcap Manifest
+-- Re:メタモルフォーゼ・キャンディーナ
+-- Created: July 03, 2026 at 17:44:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4804430) -- Re:メタモルフォーゼ・キャンディーナ
+-- MAIN APP DEPOTS
+addappid(4804431, 1, "e3df3c39df8bd9836fa02680335e89dbb48cc9d860dcf1e878154670180618f0") -- Depot 4804431
+setManifestid(4804431, "8653688398641206969", 264351552)
