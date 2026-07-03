f4c383e23d2c3ae80f033932953b6e75a9f3a25e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:21 -0500

    add 4855380/4855380.lua

---
+++ 4855380/4855380.lua
@@ -0,0 +1,12 @@
+-- 4855380's Lua and Manifest Created by Hubcap Manifest
+-- Idle Regenerator
+-- Created: July 03, 2026 at 19:09:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4855380) -- Idle Regenerator
+-- MAIN APP DEPOTS
+addappid(4855381, 1, "c8cfd69a8be546bc65c57455da7d572d4e36ad6cc5e53c61fa1b20e0199fbc5c") -- Depot 4855381
+setManifestid(4855381, "8485975809047574217", 1337008048)
