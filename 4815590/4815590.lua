5a6e0c3b895beb5371935dafab73b2018589456c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:52 -0500

    add 4815590/4815590.lua

---
+++ 4815590/4815590.lua
@@ -0,0 +1,12 @@
+-- 4815590's Lua and Manifest Created by Hubcap Manifest
+-- Koatl The Defender ULTIMATE Demo
+-- Created: July 03, 2026 at 18:54:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4815590) -- Koatl The Defender ULTIMATE Demo
+-- MAIN APP DEPOTS
+addappid(4815591, 1, "d1d65d2ebc7f476c03f81fdef5710c895e18b10483fd337972c65821a37fd805") -- Depot 4815591
+setManifestid(4815591, "3073334304504412423", 1258051866)
