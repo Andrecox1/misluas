534e4e30707403809c35ff668afca96b4d3baa6a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:26 -0500

    add 4266000/4266000.lua

---
+++ 4266000/4266000.lua
@@ -0,0 +1,12 @@
+-- 4266000's Lua and Manifest Created by Hubcap Manifest
+-- PortalVR Motion
+-- Created: July 03, 2026 at 14:02:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4266000) -- PortalVR Motion
+-- MAIN APP DEPOTS
+addappid(4266001, 1, "fff68cb45f2f2a6466cf684de30f060ce9a13bcdc5ebada078e0131751ba313e") -- Depot 4266001
+setManifestid(4266001, "8651164129502362850", 346147677)
