8438f673ebcfc1643c74f0408a79b36180b34d83
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:47 -0500

    add 4313920/4313920.lua

---
+++ 4313920/4313920.lua
@@ -0,0 +1,12 @@
+-- 4313920's Lua and Manifest Created by Hubcap Manifest
+-- Queen's Blade Re:Build Demo
+-- Created: July 03, 2026 at 18:54:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4313920) -- Queen's Blade Re:Build Demo
+-- MAIN APP DEPOTS
+addappid(4313921, 1, "bb5247270ddb43a5b4a5099372bf650647e4229d88e08fc69ae11c1690b893e3") -- Depot 4313921
+setManifestid(4313921, "6662895389665890802", 1151425597)
