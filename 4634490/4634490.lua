cc555df09dadb7d897fb29257a18ea3d669e828f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:42 -0500

    add 4634490/4634490.lua

---
+++ 4634490/4634490.lua
@@ -0,0 +1,16 @@
+-- 4634490's Lua and Manifest Created by Hubcap Manifest
+-- Master Healer Kale with useless party
+-- Created: July 03, 2026 at 06:16:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4634490, 1, "67ded34476d66756b3793c4139aae2ddc1e312471eab868bb275472171a7f11e") -- Master Healer Kale with useless party
+-- MAIN APP DEPOTS
+addappid(4634491, 1, "e598f38cfb25f78a16f54658fe45e371e9a1a2e4da81044e32c653e65ba0e770") -- Depot 4634491
+setManifestid(4634491, "6896337563033765764", 153992706)
+addappid(4634492, 1, "bbb293da092b134c1d5af8fdf3a67c5f814beffb485cf51a5da9ed3216380bc7") -- Depot 4634492
+setManifestid(4634492, "4727227321516381929", 172171643)
+addappid(4634493, 1, "b1fe8194280bf0ae3ad08130463991f436ae013b6a3d738cc6b579f218397a9e") -- Depot 4634493
+setManifestid(4634493, "8829899734040365584", 78366938)
