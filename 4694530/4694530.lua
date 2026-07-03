f5d038a5659008ade2bef47b363dd6dcd086f9ba
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:57 -0500

    add 4694530/4694530.lua

---
+++ 4694530/4694530.lua
@@ -0,0 +1,12 @@
+-- 4694530's Lua and Manifest Created by Hubcap Manifest
+-- Under Tower Idle
+-- Created: July 03, 2026 at 14:02:47 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4694530) -- Under Tower Idle
+-- MAIN APP DEPOTS
+addappid(4694531, 1, "1c249eb53a88d1e3f28c9e446d3f22551c509be6aaed25b52eb90204a1fb3d5d") -- Depot 4694531
+setManifestid(4694531, "4493958481644974411", 2298461655)
