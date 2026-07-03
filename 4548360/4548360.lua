6bf254463a54a080f9118b2f42cc59736120eef8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:15 -0500

    add 4548360/4548360.lua

---
+++ 4548360/4548360.lua
@@ -0,0 +1,12 @@
+-- 4548360's Lua and Manifest Created by Hubcap Manifest
+-- Colonus Demo
+-- Created: July 03, 2026 at 18:03:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4548360) -- Colonus Demo
+-- MAIN APP DEPOTS
+addappid(4548361, 1, "c1e56ab80a2788abb3efa5f86a1e6a15216f6e5184aa99b2450cd32e3448ffce") -- Depot 4548361
+setManifestid(4548361, "3425976589705502334", 9317123390)
