1ed3b82d0ef19c67ee393767d15c98b347eb68d7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:07 -0500

    add 4661360/4661360.lua

---
+++ 4661360/4661360.lua
@@ -0,0 +1,12 @@
+-- 4661360's Lua and Manifest Created by Hubcap Manifest
+-- Slay the Party Demo
+-- Created: July 03, 2026 at 15:41:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4661360) -- Slay the Party Demo
+-- MAIN APP DEPOTS
+addappid(4661361, 1, "4c733c050b827e280af84632a6812fd1414ac51b50289c96ffd068fe4a0b0a65") -- Depot 4661361
+setManifestid(4661361, "5682127603577144464", 589377568)
