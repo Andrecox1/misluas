37e4f3ce223a6ff45874483710f73ba1ae5ad4ec
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:36 -0500

    add 4066250/4066250.lua

---
+++ 4066250/4066250.lua
@@ -0,0 +1,12 @@
+-- 4066250's Lua and Manifest Created by Hubcap Manifest
+-- Magical Thief Silky Demo
+-- Created: July 03, 2026 at 18:53:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4066250) -- Magical Thief Silky Demo
+-- MAIN APP DEPOTS
+addappid(4066251, 1, "98f306996c7fe4ef2fff82874e7f9a5dba787efc6a83c02af367f6c179b905d9") -- Depot 4066251
+setManifestid(4066251, "4568975925789351911", 5311386153)
