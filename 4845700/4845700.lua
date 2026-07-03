c360f4becd84107289aabcd1a3dcd5c74f39369f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:54 -0500

    add 4845700/4845700.lua

---
+++ 4845700/4845700.lua
@@ -0,0 +1,12 @@
+-- 4845700's Lua and Manifest Created by Hubcap Manifest
+-- Football Commissioner
+-- Created: July 03, 2026 at 18:21:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4845700) -- Football Commissioner
+-- MAIN APP DEPOTS
+addappid(4845701, 1, "3a430814a80bfc16f4ab02d7c7d872595c7c34dac2d66a6dc7b673efbf897efb") -- Depot 4845701
+setManifestid(4845701, "846506518826062670", 68977245)
