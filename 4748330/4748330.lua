440c76f212fd2fffbf03dd3d3e510de8e2891d30
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:49 -0500

    add 4748330/4748330.lua

---
+++ 4748330/4748330.lua
@@ -0,0 +1,14 @@
+-- 4748330's Lua and Manifest Created by Hubcap Manifest
+-- Luna Meridian
+-- Created: July 03, 2026 at 19:09:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4748330) -- Luna Meridian
+-- MAIN APP DEPOTS
+addappid(4748331, 1, "4c60ab69c9d4698cb5148737a62c6bbadfbc0892a831fdd26d878e40d10a38a1") -- Depot 4748331
+setManifestid(4748331, "3818033656109809708", 268558302)
+addappid(4748332, 1, "ff63df781342164ba5afeabbad607e15a986254cf346cbbfa7ff1b71d30066b6") -- Depot 4748332
+setManifestid(4748332, "1922498251242574591", 285600351)
