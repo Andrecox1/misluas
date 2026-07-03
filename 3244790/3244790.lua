e58a1534cf24f236b8777736928c9f04317bce1c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:43 -0500

    add 3244790/3244790.lua

---
+++ 3244790/3244790.lua
@@ -0,0 +1,12 @@
+-- 3244790's Lua and Manifest Created by Hubcap Manifest
+-- Sojourn Past Demo
+-- Created: July 03, 2026 at 18:54:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3244790) -- Sojourn Past Demo
+-- MAIN APP DEPOTS
+addappid(3244791, 1, "b32fc9d078d3867e583e06b7fa9aa3fba04859c63912fd7bf14c3e5b9e4e794b") -- Depot 3244791
+setManifestid(3244791, "4548760060463503684", 2616933309)
