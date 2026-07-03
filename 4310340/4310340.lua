e076d49344c6a41e0a32b551cfcb3df9ee476654
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:19 -0500

    add 4310340/4310340.lua

---
+++ 4310340/4310340.lua
@@ -0,0 +1,14 @@
+-- 4310340's Lua and Manifest Created by Hubcap Manifest
+-- UNDERGROUNDED Soundtrack
+-- Created: July 03, 2026 at 06:16:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4310340) -- UNDERGROUNDED Soundtrack
+-- MAIN APP DEPOTS
+addappid(4310341, 1, "6e2bc558c412b9de3f6fadce4c9447107fed7b8427df208f36881e410c1b33b2") -- Depot 4310341
+setManifestid(4310341, "1251120184034340581", 35594963)
+addappid(4310342, 1, "fdbdbf32a2f6d0c84d9ab995beed21a9de94e07842c1de18b7708161a8a37099") -- Depot 4310342
+setManifestid(4310342, "6805238244253788175", 489025852)
