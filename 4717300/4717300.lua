60c3f578eb8afef42d49241f25496a36127f66d4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:16 -0500

    add 4717300/4717300.lua

---
+++ 4717300/4717300.lua
@@ -0,0 +1,12 @@
+-- 4717300's Lua and Manifest Created by Hubcap Manifest
+-- Shards of the Heart Soundtrack
+-- Created: July 03, 2026 at 17:44:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4717300) -- Shards of the Heart Soundtrack
+-- MAIN APP DEPOTS
+addappid(4717301, 1, "a7938f17cac1b44f3f810621e6f200253c790cb57b3c0fd91b18bac98096ce4c") -- Depot 4717301
+setManifestid(4717301, "2458856963614418966", 98424250)
