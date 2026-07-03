fd1deeee298a062b3673aae6aa4d90ea1a624621
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:02 -0500

    add 4802920/4802920.lua

---
+++ 4802920/4802920.lua
@@ -0,0 +1,12 @@
+-- 4802920's Lua and Manifest Created by Hubcap Manifest
+-- Colossus - Eternal Blight Demo
+-- Created: July 03, 2026 at 18:54:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4802920) -- Colossus - Eternal Blight Demo
+-- MAIN APP DEPOTS
+addappid(4802921, 1, "e8cf27a457569722e378b5c5be95f3d3cd78d7b3be35b7a5cd526899e12ce9a3") -- Depot 4802921
+setManifestid(4802921, "5320431022096453115", 688353137)
