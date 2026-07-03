19d5445cdc7212b0d45e563466454ef63b3e2ab2
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:09:53 -0500

    add 4856190/4856190.lua

---
+++ 4856190/4856190.lua
@@ -0,0 +1,12 @@
+-- 4856190's Lua and Manifest Created by Hubcap Manifest
+-- Tech Store & Repair Simulator
+-- Created: July 03, 2026 at 11:44:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4856190) -- Tech Store & Repair Simulator
+-- MAIN APP DEPOTS
+addappid(4856191, 1, "ac0883f94df1a87d804f20aaba82f3e782beda3725b49db183bc04fd1557de6e") -- Depot 4856191
+setManifestid(4856191, "9090005025722659711", 657593146)
