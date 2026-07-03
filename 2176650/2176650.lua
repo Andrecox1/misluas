55cab9b27681fe9eb4371b0a249121c2705f6f63
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:11:12 -0500

    add 2176650/2176650.lua

---
+++ 2176650/2176650.lua
@@ -0,0 +1,12 @@
+-- 2176650's Lua and Manifest Created by Hubcap Manifest
+-- Shards of the Heart
+-- Created: July 03, 2026 at 17:44:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2176650) -- Shards of the Heart
+-- MAIN APP DEPOTS
+addappid(2176652, 1, "c5a3a964394d7542383ac1d7ffa66db0ade99fe6dfd6f59354198dea207c44e7") -- Depot 2176652
+setManifestid(2176652, "259623683691817174", 1649630621)
