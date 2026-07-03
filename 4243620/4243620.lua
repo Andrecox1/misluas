1a6214d45a06cac2e403f91b739344f856d64cad
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-03 18:10:31 -0500

    add 4243620/4243620.lua

---
+++ 4243620/4243620.lua
@@ -0,0 +1,12 @@
+-- 4243620's Lua and Manifest Created by Hubcap Manifest
+-- The Starborn Return Demo
+-- Created: July 03, 2026 at 18:54:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4243620) -- The Starborn Return Demo
+-- MAIN APP DEPOTS
+addappid(4243621, 1, "7e7b8a7ca276136736d707e089b65ca7c0cd2612396ac30b0721fbc15d838b8c") -- Depot 4243621
+setManifestid(4243621, "752519574221456334", 7111031216)
