f51d4b75b2c1dc2aee13ad0c23f484f7a85661cd
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-04 12:28:53 -0500

    connection-fix: 1097840/1097840.lua

---
+++ 1097840/1097840.lua
@@ -0,0 +1,50 @@
+-- 1097840's Lua and Manifest Created by Hubcap Manifest
+-- Gears 5
+-- Created: September 29, 2025 at 14:17:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 15
+-- Total DLCs: 4
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1097840) -- Gears 5
+-- MAIN APP DEPOTS
+addappid(1097841, 1, "a32bf1f0b01aede39565f572e55ac4eb9203c816ed1da9909016bc7d58e79179") -- Gears 5 Content
+setManifestid(1097841, "8300652891860260502", 92733313533)
+addappid(1097842, 1, "277060b697b3b53717961a151aa2d756da27408eff991bd9405341e33aa620a0") -- Gears 5 English
+setManifestid(1097842, "1278269335921614067", 0)
+addappid(1097843, 1, "d95f93c630156239f1b219c521e29bc8e7cadbe8cfe7100f40cfd2678c18a905") -- Gears 5 German
+setManifestid(1097843, "289857167781562119", 884916224)
+addappid(1097844, 1, "58018367e436c09744604962fbdbb6622899f3e8c72f3364c685d2e88827f7dc") -- Gears 5 Spanish (LATAM)
+setManifestid(1097844, "5030833450944321384", 912441344)
+addappid(1097845, 1, "f09ed7b6e5a4f7946355b6f1421d5801fa3e6873fc6c1081d9364bf07b857768") -- Gears 5 Spanish (Spain)
+setManifestid(1097845, "9210807277749980928", 894136320)
+addappid(1097846, 1, "a2f7686b1740e936a9bc0aa5da71658140f4478e28c02d43ba21e522c33dde22") -- Gears 5 French
+setManifestid(1097846, "886384122577741960", 855023616)
+addappid(1097847, 1, "c7a3f60aad17d5833fb12cb6f37b825142a8174acb38d8a965e18fd4e880cbda") -- Gears 5 Italian
+setManifestid(1097847, "6793667326824331971", 907022336)
+addappid(1097848, 1, "47eed7b7477a3279e852a24863e36bbf5a975feb33a1afe2a9584e7a271b8787") -- Gears 5 Portuguese (BR)
+setManifestid(1097848, "7954227374585976715", 878452736)
+addappid(1097849, 1, "6c1644bce2482c316964c80116638204e50a3c34f2db5fa3d4ce7e8f05431180") -- Gears 5 Chinese (Traditional)
+setManifestid(1097849, "7934319328911186639", 881025024)
+addappid(1097850, 1, "4f33ff5426301948943b9cca183b9e68665516de89f82ee286ddf9fe971f2b66") -- Gears 5 Korean
+setManifestid(1097850, "5108281121503607889", 0)
+-- SHARED DEPOTS (from other apps)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Gears 5 - Ultra-HD Texture Pack (AppID: 1117250)
+addappid(1117250)
+addappid(1117250, 1, "3939235be4baf3947292c37e03a73070906c2c8d5c10528f332176ec404d0e6f") -- Gears 5 - Ultra-HD Texture Pack - Gears 5 - Ultra Texture Pack (1117250) Depot
+setManifestid(1117250, "1873475681924073619", 28167188480)
+-- Gears 5 - Hivebusters (AppID: 1415070)
+addappid(1415070)
+addappid(1415070, 1, "0874063be1d07d165c3f8a9355ca545fca5c348fdd0f40b34854f9e07483e211") -- Gears 5 - Hivebusters - Gears 5 - DLC (1415070) Depot
+setManifestid(1415070, "2779633928483226347", 8634064896)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1105950) -- Gears 5 - Ultimate Edition DLC Content
+addappid(1105951) -- Gears 5 - Pre-Purchase Bonus DLC Content
