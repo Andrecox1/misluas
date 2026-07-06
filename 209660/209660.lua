6a87457f0428876e4d22ca1aebcd58edf44b1f04
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-06 13:00:12 -0500

    connection-fix: 209660/209660.lua

---
+++ 209660/209660.lua
@@ -0,0 +1,182 @@
+-- 209660's Lua and Manifest Created by Hubcap Manifest
+-- Call of Duty: Advanced Warfare - Multiplayer
+-- Created: September 29, 2025 at 02:45:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 53
+-- Total DLCs: 57
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(209660) -- Call of Duty: Advanced Warfare - Multiplayer
+-- MAIN APP DEPOTS
+addappid(310340, 1, "4072a6a14b5998eda7ffbba632394dea777d5f44a9d36927eb1214733c011e3f") -- Call of Duty: Advanced Warfare - MP Binary
+setManifestid(310340, "3027530553092813409", 29457373)
+addappid(310341, 1, "2a0cfc30d4b2f07b8040bd5dcffb6c544bea8ddc20699de09bf59839bc3bbcc2") -- Call of Duty: Advanced Warfare - MP Common
+setManifestid(310341, "7286983045129950292", 4551243744)
+addappid(310342, 1, "d19d838295f0fa2b629eb334303ee4b7171a07b07c860274ef34e70a93cc7c39") -- Call of Duty: Advanced Warfare - Shared Common
+setManifestid(310342, "1115818459498978172", 4722797205)
+addappid(310343, 1, "504aed523c5fe5e6db1bc7b4ce4c3bdff0be2197b83a2d83fa8d09107370b4e0") -- Call of Duty: Advanced Warfare - Shared English
+setManifestid(310343, "4656459945361775496", 1150863494)
+addappid(310344, 1, "efcc682954491cd6167f2090bf467fa17baf441b31c9285b1e9d1af546f0b5a8") -- Call of Duty: Advanced Warfare - Shared French
+setManifestid(310344, "7897001590783896843", 1234816169)
+addappid(310345, 1, "22462a37c5cc11dd6468528a3ac25aea94aee67ff697016ad3e3e47918587cf8") -- Call of Duty: Advanced Warfare - Shared Italian
+setManifestid(310345, "9099835492590448163", 1282318897)
+addappid(310346, 1, "0d0d1fb6cd7e6c431f6796ddf7659ada697192aba37caa63654f5013873f9ff2") -- Call of Duty: Advanced Warfare - Shared German
+setManifestid(310346, "4175828202668702430", 1401938615)
+addappid(310347, 1, "7534a652f5c8717fbfc2c82332716a05c717af0afb1bd4cc65d464ac0d437aef") -- Call of Duty: Advanced Warfare - Shared Spanish
+setManifestid(310347, "8041230264893351577", 1274295249)
+addappid(310348, 1, "c7827829d1ea13e56de9776592d881a5474689d2d4d1fa709fd6575eea550f38") -- Call of Duty: Advanced Warfare - Shared Russian
+setManifestid(310348, "518742459434836003", 1265519787)
+addappid(310349, 1, "31f8bac832cb478681b7c84216f2aca991558d965def6c4b96a9aae2006c6fff") -- Call of Duty: Advanced Warfare - Shared Polish
+setManifestid(310349, "5769306992028928151", 1125452658)
+addappid(310351, 1, "35718ade3b8e80c2236659d193e654196bc72ce6f3089c698f8c3367f0ded041") -- Call of Duty: Advanced Warfare - Shared Common Imagefiles
+setManifestid(310351, "1373781635064905469", 15625354048)
+addappid(310352, 1, "a4212875b935db46151a2fafa7ad4b59c8beea26349428005c2b906aa93a4adc") -- Call of Duty: Advanced Warfare - Shared Common Soundfiles
+setManifestid(310352, "3190197747840803077", 10165564759)
+addappid(310353, 1, "b7db64eb8bd8aa419742ae7b3a0ea92271e79ec5e888e2144951883e505031d8") -- Call of Duty: Advanced Warfare - Shared Common Imagefiles5
+setManifestid(310353, "1786374612997832970", 13733513972)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Call of Duty Advanced Warfare - Atlas Gorge Map (AppID: 318790)
+addappid(318790)
+addappid(318790, 1, "95f85b8cf80026b376f15bbdf1b5950b7bd5e5eab8f68938cfa83820d6ad22cd") -- Call of Duty Advanced Warfare - Atlas Gorge Map - Call of Duty: Advanced Warfare - Atlas Gorge Map (318790) Depot
+setManifestid(318790, "1858867507944457961", 552046078)
+-- Call of Duty Advanced Warfare - Havoc Map Pack (AppID: 318791)
+addappid(318791)
+addappid(318791, 1, "e01036bffd498c767f53ddee2bd6ae270b5f60658fe5cbf51196baf3983ee21a") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc Map Pack (318791) Depot
+setManifestid(318791, "1768298366781761218", 6400716381)
+addappid(347770, 1, "0a38bcbc386644113ff86afa82a3fc0d3ef30b7ace7e9b6f2f2b49738a1df86c") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc English
+setManifestid(347770, "392449385480523482", 255508821)
+addappid(347771, 1, "4a6698743b5e049a41953e90bd55949acebf3a9f4e8f281479c40f83bb1af01a") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc French
+setManifestid(347771, "3314416001015546544", 218368694)
+addappid(347772, 1, "6313577dd4279bae0eea1bb52857672bca5bcb39e8b8293d79df7c5785e424dd") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc Italian
+setManifestid(347772, "94956196535018115", 274685344)
+addappid(347773, 1, "784ae9cc20561982eaa80d30e528d7525d336eced0f9fe2b75fc64de50ae59ce") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc German
+setManifestid(347773, "1023154477557006801", 308010212)
+addappid(347774, 1, "408798ed73ad008d56b4d50b108d7f4438d6a8802004ccd72e818a3f257a703a") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc Spanish
+setManifestid(347774, "3957741259512798010", 255117981)
+addappid(347775, 1, "3d4c07b46750b18853225ff208a02ddbc54432dbfd06a37df8de9aa9a1e9887f") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc Russian
+setManifestid(347775, "2782106135952647496", 334128483)
+addappid(347776, 1, "50420e4ca2955267101bf47f6c848e7caa9c3a09fac21bf8dbd8d1f397bada65") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc Polish
+setManifestid(347776, "1837819544278475801", 255508821)
+addappid(347777, 1, "ef13694941247c5edec6010b5a83320c693b33f5e6826248489e9323d1cbc671") -- Call of Duty Advanced Warfare - Havoc Map Pack - Call of Duty: Advanced Warfare - Havoc Japanese
+setManifestid(347777, "474175569052515799", 255508821)
+-- Call of Duty Advanced Warfare - Ascendance Map Pack (AppID: 318792)
+addappid(318792)
+addappid(318792, 1, "711239e1abe6943d01223b88d610bf22c0c72711b9856503e7bd1c2149ed2892") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance Map Pack (318792) Depot
+setManifestid(318792, "5542785062311499763", 5433792241)
+addappid(317661, 1, "1cf01d3554d5661d082565b57124389502847c623cb01fcbd8553c0ca1708504") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance English
+setManifestid(317661, "4974123719394549476", 275660883)
+addappid(317662, 1, "56f93ba8d34ac491cdb949c0ba315cde321a270f6cb18b9e1acdcceb4675b7e0") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance French
+setManifestid(317662, "64346122590985239", 231332322)
+addappid(317663, 1, "aa3edc00cc98e1ba9c0bfd966affb07b0665840d0e47c944ca43790ecb78d518") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance Italian
+setManifestid(317663, "356177234792779318", 291855694)
+addappid(317664, 1, "0c96f9f1994192c8ec8d2d83a2eb0608f71a871ed49a5ce2e1a4ee8396669e0c") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance German
+setManifestid(317664, "8480471138799758813", 340458229)
+addappid(317665, 1, "8ef90a7247720753a3cc91285d5993ed5b834f29c90a6180dfb4272f4b8d7457") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance Spanish
+setManifestid(317665, "2384865611222065689", 276024835)
+addappid(317666, 1, "a28bb8d2755ff583b1b2bfb09c076a09e351bc6645dbfe9a944e0a191378749d") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance Russian
+setManifestid(317666, "6245220386026814238", 359833785)
+addappid(317667, 1, "437445d2d7d6fbcca518fc181b63f6ef801f823e0d275ad2ae755a02543e798e") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance Polish
+setManifestid(317667, "2158361324073601944", 275660883)
+addappid(317668, 1, "c0d6208835a5920d3f58865a912fdab76977f4b6db0389f2bc883e818171223f") -- Call of Duty Advanced Warfare - Ascendance Map Pack - Call of Duty: Advanced Warfare - Ascendance Japanese
+setManifestid(317668, "2169729678511789913", 275660883)
+-- Call of Duty Advanced Warfare - Supremacy Map Pack (AppID: 318793)
+addappid(318793)
+addappid(318793, 1, "da88b4d1e96ec1fc5192a03c4b90b9fe8e2611e2645cbb655ad1d68446270d49") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy Map Pack (318793) Depot
+setManifestid(318793, "2176153660918059816", 5443003138)
+addappid(324231, 1, "4613a7d2818a0e457d6e723891a2ca8f2189a7a055e6de64210b4ddd971c6279") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy English
+setManifestid(324231, "2538506173373403673", 270287410)
+addappid(324232, 1, "97ebf46505cc2a5db92c6a0089b7b61f2e974006b34c76e3f1e603a125972231") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy French
+setManifestid(324232, "3114571272841566902", 232797667)
+addappid(324233, 1, "3cd6755d868bf5f433c3987c43ec0a588bc46634711ca20647b04f7fe7960808") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy Italian
+setManifestid(324233, "3162429864754162241", 283694041)
+addappid(324234, 1, "c22c7a956e0bb4c26b81ec8ebc1da68671e5bfc235f5997dc2c891947aad6925") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy German
+setManifestid(324234, "4812724123055684709", 326212279)
+addappid(324235, 1, "b92fa18482b961ac743a08a83457ac055ef5ce8a023eefdef1b55c871172165f") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy Spanish
+setManifestid(324235, "3130606459462990314", 285797765)
+addappid(324236, 1, "fa3dd9a8aa8f75752db27629a64b9081f77866274caf13562dcb985eef9bd6d2") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy Russian
+setManifestid(324236, "2796631653398813720", 317056454)
+addappid(324237, 1, "a4d79ae1f8defe546d5713c092b516a6bbdd5984627efeea12a2bf86e7af3a3f") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy Polish
+setManifestid(324237, "1223770653333683354", 270287410)
+addappid(324238, 1, "496868059686cb636cfa03e1b4feec7698fd9727cd70d96232670fbb1b6f2e7c") -- Call of Duty Advanced Warfare - Supremacy Map Pack - Call of Duty: Advanced Warfare - Supremacy Japanese
+setManifestid(324238, "2026236127874058503", 270287410)
+-- Call of Duty Advanced Warfare - Reckoning Map Pack (AppID: 318794)
+addappid(318794)
+addappid(318794, 1, "a59c63c61136c476387596791b0b505c9e92441a1e1cd79fe70ff85814e43433") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning Map Pack (318794) Depot
+setManifestid(318794, "901586032961765427", 5897168238)
+addappid(334461, 1, "f657dbdd72a2a0271cc5c3381a2dbe37128c792ae7dd03c2542721f7335c3422") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning English
+setManifestid(334461, "7877729103290591318", 376820284)
+addappid(334462, 1, "3d5b527fe89fd491f8ce40666d020d8d879d70ed4dcddda335b53b3ac668aba2") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning French
+setManifestid(334462, "2889345427151274804", 341858933)
+addappid(334463, 1, "36d2c0d079e6f37609fb8af261d623adc6c98928cd43afbe530ea1e6f48a7f35") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning Italian
+setManifestid(334463, "7604787201923219397", 392578881)
+addappid(334464, 1, "84f81e5d0d9f8a16cc5c753ab229ed4c0e4065fadc5313265fd081ea0b819f15") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning German
+setManifestid(334464, "4690471876936054489", 459062267)
+addappid(334465, 1, "79797325716dd65c451e7c63d74b95f3e7b0ebff03fa15cd1caf2237228416f0") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning Spanish
+setManifestid(334465, "3164960295677674124", 413194850)
+addappid(334466, 1, "082b9b0e0f750025e3de5bee7dc0864ae5e95982a603cf8a8ad1a2937c5b1a03") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning Russian
+setManifestid(334466, "8381701963760406591", 424575773)
+addappid(334467, 1, "19d0137fdc8b2a82b1e533abb17d00dc21bf50a073d2ac2b1294f01605a2462e") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning Polish
+setManifestid(334467, "5183992592900461375", 376820284)
+addappid(334468, 1, "9ec13c3f294f05e1dd381a33d304de8a3db4ec51f83f6e9acc94b26d62958f65") -- Call of Duty Advanced Warfare - Reckoning Map Pack - Call of Duty: Advanced Warfare - Reckoning Japanese
+setManifestid(334468, "6286407237860024022", 376820284)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(317660) -- Call of Duty Advanced Warfare - Season Pass
+addappid(318795) -- Call of Duty Advanced Warfare - Digital Edition Personalization Pack
+addappid(318796) -- Call of Duty Advanced Warfare - Adv. Arsenal Weapon - EM1 Quantum
+addappid(318797) -- Call of Duty Advanced Warfare - Adv. Arsenal Exo - Bullet Brass
+addappid(318798) -- Call of Duty Advanced Warfare - Day Zero Weapon - AK-12G
+addappid(318799) -- Call of Duty Advanced Warfare - Day Zero Weapon - Crossbow-B2
+addappid(318800) -- Call of Duty Advanced Warfare - Atlas Digital Pack Content
+addappid(318801) -- Call of Duty Advanced Warfare - Weapon - BAL-27 AE
+addappid(318802) -- Call of Duty Advanced Warfare - Weapon - Atlas 45 AE
+addappid(318803) -- Call of Duty Advanced Warfare - Exo - Atlas
+addappid(318804) -- Call of Duty Advanced Warfare - Helmet - Atlas
+addappid(318805) -- Call of Duty Advanced Warfare - Playercard - Atlas
+addappid(324230) -- Call of Duty Advanced Warfare - Exo - Fuel Up For Battle
+addappid(331060) -- Call of Duty Advanced Warfare - Magma Personalization Pack 
+addappid(331061) -- Call of Duty Advanced Warfare - Lightning Personalization Pack
+addappid(331062) -- Call of Duty Advanced Warfare - Creature Personalization Pack
+addappid(331063) -- Call of Duty Advanced Warfare - Nanotech Personalization Pack
+addappid(331064) -- Call of Duty Advanced Warfare - Magma Premium Personalization Pack
+addappid(331065) -- Call of Duty Advanced Warfare - Lightning Premium Personalization Pack
+addappid(331066) -- Call of Duty Advanced Warfare - Creature Premium Personalization Pack
+addappid(331067) -- Call of Duty Advanced Warfare - Nanotech Premium Personalization Pack
+addappid(331068) -- Call of Duty Advanced Warfare - Exo - Steampunk
+addappid(331069) -- Call of Duty Advanced Warfare - Exo - Panda
+addappid(331070) -- Call of Duty Advanced Warfare - Exo - Barong
+addappid(331071) -- Call of Duty Advanced Warfare - Exo - Hot Rod
+addappid(331072) -- Call of Duty Advanced Warfare - Flag Pack - United States
+addappid(333080) -- Call of Duty Advanced Warfare - Flag Pack - United Kingdom
+addappid(333081) -- Call of Duty Advanced Warfare - Flag Pack - Canada
+addappid(333082) -- Call of Duty Advanced Warfare - Flag Pack - France
+addappid(333083) -- Call of Duty Advanced Warfare - Flag Pack - Germany
+addappid(333084) -- Call of Duty Advanced Warfare - Flag Pack - Australia
+addappid(333087) -- Call of Duty Advanced Warfare - Flag Pack - Italy
+addappid(333088) -- Call of Duty Advanced Warfare - Flag Pack - Spain
+addappid(334220) -- Call of Duty Advanced Warfare - Flag Pack - Netherlands
+addappid(334221) -- Call of Duty Advanced Warfare - Flag Pack - Japan
+addappid(343590) -- Call of Duty Advanced Warfare - Black Ops III Pre-Purchase Bonus Pack
+addappid(343591) -- Call of Duty Advanced Warfare - Tiki Personalization Pack
+addappid(343592) -- Call of Duty Advanced Warfare - Backdraft Personalization Pack
+addappid(343593) -- Call of Duty Advanced Warfare - Lagoon Personalization Pack
+addappid(343594) -- Call of Duty Advanced Warfare - Psychedelic Personalization Pack
+addappid(343595) -- Call of Duty Advanced Warfare - Weapon - Ohm
+addappid(343596) -- Call of Duty Advanced Warfare - X-Ray Personalization Pack
+addappid(343597) -- Call of Duty Advanced Warfare - Disco Personalization Pack
+addappid(343598) -- Call of Duty Advanced Warfare - Aces Personalization Pack
+addappid(343599) -- Call of Duty Advanced Warfare - Jackpot Personalization Pack
+addappid(343600) -- Call of Duty Advanced Warfare - Championship Premium Personalization Pack
+addappid(343610) -- Call of Duty Advanced Warfare - Extra Create A Class Slots
+addappid(343611) -- Call of Duty Advanced Warfare - Extra Armory Slots 1
+addappid(343612) -- Call of Duty Advanced Warfare - Extra Armory Slots 2
+addappid(343613) -- Call of Duty Advanced Warfare - Extra Armory Slots 3
+addappid(343614) -- Call of Duty Advanced Warfare - Extra Armory Slots 4
+addappid(343615) -- Call of Duty Advanced Warfare - Extra Armory Slots 5
