1ed84106b89bcc42acaef1628273feee20d1be6b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-12 17:04:21 -0500

    connection-fix: 2416450/2416450.lua

--- 2416450/2416450.lua
+++ 2416450/2416450.lua
@@ -1,6 +1,6 @@
 -- 2416450's Lua and Manifest Created by Hubcap Manifest
 -- MOUSE: P.I. For Hire
--- Created: June 18, 2026 at 00:09:27 EDT
+-- Created: July 09, 2026 at 02:29:45 EDT
 -- Website: https://hubcapmanifest.com/
 -- Total Depots: 4
 -- Total DLCs: 1 (1 excluded)
@@ -10,10 +10,10 @@
 addappid(2416450, 1, "56df9203dd3706f1816684fd80aec6fd14bacaf541f8274f7d84eb9ae924b04e") -- MOUSE: P.I. For Hire
 -- MAIN APP DEPOTS
 addappid(2416451, 1, "c52f8369321c8357293962eacf7c7f416c2ea912c3d2640703880a12c964732a") -- Depot 2416451
-setManifestid(2416451, "677879456208603530", 10477976073)
+setManifestid(2416451, "7328998315841231589", 10416797330)
 -- SHARED DEPOTS (from other apps)
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
-setManifestid(228989, "3514306556860204959", 39590283)
+setManifestid(228989, "5753583882400741046", 25674515)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
 setManifestid(228990, "1829726630299308803", 102931551)
 -- DLCS WITH DEDICATED DEPOTS
