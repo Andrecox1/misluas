a1374fa396bc454f4fc90a2ec1e9947abec5a621
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-22 13:24:48 -0500

    connection-fix: 2483190/2483190.lua

--- 2483190/2483190.lua
+++ 2483190/2483190.lua
@@ -1,16 +1,16 @@
 -- 2483190's Lua and Manifest Created by Hubcap Manifest
 -- Forza Horizon 6
--- Created: July 13, 2026 at 14:06:31 EDT
+-- Created: July 20, 2026 at 13:09:13 EDT
 -- Website: https://hubcapmanifest.com/
 -- Total Depots: 3
--- Total DLCs: 20 (3 excluded)
+-- Total DLCs: 22 (2 excluded)
 -- Shared Depots: 2
 
 -- MAIN APPLICATION
 addappid(2483190, 1, "bca2f0f9973d929c2c6bae884058b8bf7934e9ecc8ff34d584450dfbb3215040") -- Forza Horizon 6
 -- MAIN APP DEPOTS
 addappid(2483191, 1, "f2a09f0a31b4df681ec0d2d572f368dfa4cd6c37ae67ebdfd591cfbe1334fcd9") -- Depot 2483191
-setManifestid(2483191, "2541360848233493756", 167216227751)
+setManifestid(2483191, "4699035931366716177", 167216440344)
 -- SHARED DEPOTS (from other apps)
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
 setManifestid(228989, "5753583882400741046", 25674515)
@@ -20,6 +20,8 @@ setManifestid(228990, "1829726630299308803", 102931551)
 addappid(4439260) -- Forza Horizon 6 Welcome Pack
 addappid(4439270) -- Forza Horizon 6 Premium VIP
 addappid(4439280) -- Forza Horizon 6 Time Attack Car Pack
+addappid(4439290) -- Forza Horizon 6 Italian Passion Car Pack
+addtoken(4439290, "7027096395897994901")
 addappid(4439300) -- Forza Horizon 6 Ferrari J50
 addappid(4439750) -- Forza Horizon 6 Treasure Map
 addappid(4439790) -- Forza Horizon 6 Car Pass
@@ -37,11 +39,10 @@ addappid(4520350) -- Forza Horizon 6 Expansions Bundle
 addappid(4520360) -- Forza Horizon 6 Premium Upgrade
 addappid(4562050) -- Forza Horizon 6 Crunchyroll Voucher
 addappid(4610350) -- Forza Horizon 6 1998 Nissan Skyline GT-R 40th Anniversary
+addappid(4797360) -- Forza Horizon 6 2003 Aston Martin DB7 GT
 -- EXCLUDED DLCS:
 -- UNRELEASED DLCS (COMMENTED OUT)
 -- addappid(4439240) -- DLC 4439240 (unreleased)
 -- addtoken(4439240, "10618922691696453695")
 -- addappid(4439250) -- DLC 4439250 (unreleased)
 -- addtoken(4439250, "6382430189070780048")
--- addappid(4439290) -- DLC 4439290 (unreleased)
--- addtoken(4439290, "7027096395897994901")
