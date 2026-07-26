850f6f66032b80e1c3b4b9d268565ad17adb5995
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-26 14:49:59 -0500

    connection-fix: 374320/374320.lua

--- 374320/374320.lua
+++ 374320/374320.lua
@@ -1,15 +1,38 @@
-addappid(374320)
-addappid(442010)
+-- 374320's Lua and Manifest Created by Hubcap Manifest
+-- DARK SOULS™ III
+-- Created: December 08, 2025 at 21:59:28 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 4 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(374320, 1, "e5f7bec9ae6e7f8ad0a875a225ae10055236b0aebad8ca7cf395f8aa7e8102b2") -- DARK SOULS™ III
+-- MAIN APP DEPOTS
+addappid(374321, 1, "1e1f904451f44a8549008ac8152430dc18288dd22c8edda058afa86b926a4162") -- DARK SOULS III Content
+setManifestid(374321, "6652575487689169560", 21819457695)
+addappid(374322, 1, "2e8b33176e957bf7c6ed4ad3c34847d1eef1ee846e900e1b4a2b4fc3a51b6c09") -- DARK SOULS III JP Depot
+setManifestid(374322, "6130303443397132373", 735396028)
+-- SHARED DEPOTS (from other apps)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- DARK SOULS III - Original Soundtrack (AppID: 455380)
 addappid(455380)
+addappid(455380, 1, "ee1942ee70f7707eb40415a87cd933fedf4de04aeab6f69421e106ff4b8d3228") -- DARK SOULS III - Original Soundtrack - DARK SOULS™ III - Original Soundtrack
+setManifestid(455380, "398045694410355072", 102202542)
+-- DARK SOULS III - Ashes of Ariandel (AppID: 506970)
 addappid(506970)
+addappid(506970, 1, "2dc303f9e1fc1f45f385b5d262b3b481ed10f11d38e5ca44edf7096a4f280385") -- DARK SOULS III - Ashes of Ariandel - DARK SOULS III - DLC1 (506970) Depot
+setManifestid(506970, "2441039608905796121", 1684575628)
+-- DARK SOULS III - The Ringed City (AppID: 506971)
 addappid(506971)
-addappid(374321,0,"1e1f904451f44a8549008ac8152430dc18288dd22c8edda058afa86b926a4162")
---setManifestid(374321,"6652575487689169560")
-addappid(374322,0,"2e8b33176e957bf7c6ed4ad3c34847d1eef1ee846e900e1b4a2b4fc3a51b6c09")
---setManifestid(374322,"6130303443397132373")
-addappid(455380,0,"ee1942ee70f7707eb40415a87cd933fedf4de04aeab6f69421e106ff4b8d3228")
---setManifestid(455380,"398045694410355072")
-addappid(506970,0,"2dc303f9e1fc1f45f385b5d262b3b481ed10f11d38e5ca44edf7096a4f280385")
---setManifestid(506970,"2441039608905796121")
-addappid(506971,0,"0ce07245c46df4c8b1c5d7bef627f59f34b9fde6ea903ec21ea879cf6d9c10d5")
---setManifestid(506971,"266039425715139370")
+addappid(506971, 1, "0ce07245c46df4c8b1c5d7bef627f59f34b9fde6ea903ec21ea879cf6d9c10d5") -- DARK SOULS III - The Ringed City - DARK SOULS III - DLC2 (506971) Depot
+setManifestid(506971, "266039425715139370", 3145050927)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(442010) -- DARK SOULS III - Season Pass
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(435470) -- DARK SOULS™ III - Original Soundtrack (unreleased)
