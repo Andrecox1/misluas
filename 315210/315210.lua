73969f99d658d7ad7d61777415dcd31b9059ff97
Andrecox1 <andresfelipear3357963@gmail.com>
2026-07-29 15:45:54 -0500

    connection-fix: 315210/315210.lua

---
+++ 315210/315210.lua
@@ -0,0 +1,37 @@
+-- 315210's Lua and Manifest Created by Hubcap Manifest
+-- Suicide Squad: Kill the Justice League
+-- Created: September 30, 2025 at 20:44:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 10
+-- Total DLCs: 4
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(315210) -- Suicide Squad: Kill the Justice League
+-- MAIN APP DEPOTS
+addappid(315211, 1, "fabb38272098c3396b2e9f2e47f227b066bb5213920e64570afdac89330de21f") -- Depot 315211
+setManifestid(315211, "4443834196174197060", 103185062412)
+addappid(315212, 1, "e61a5741667835bdbabe5aebc4348b520f97ed4156dbe3fd607e21a0093fcda0") -- Depot 315212
+setManifestid(315212, "262943211739857545", 6285815875)
+addappid(315213, 1, "1e45ecc87c25598487a2fc6aad943085a92d9a220160bf9ed500e24c9669961f") -- Depot 315213
+setManifestid(315213, "780674397389142294", 6576171401)
+addappid(315214, 1, "60dc3874da94039f2347261a4d0a3bd1060c1517abadc1872e54887c945cd494") -- Depot 315214
+setManifestid(315214, "3390447772311628478", 6218206199)
+addappid(315215, 1, "e49e77edadfd05a3d449e867449d8c08a73ca9ff247841043dd594e427ef1c17") -- Depot 315215
+setManifestid(315215, "6878152482888731645", 6053525831)
+addappid(315216, 1, "cc2767e57c58cc4ce064181e68edb5a71427e4bc638e57ab71ac5b4f089c5b3f") -- Depot 315216
+setManifestid(315216, "7361648533418468010", 6234670055)
+addappid(315217, 1, "92f2682dbe683871e849b0dc13ec3e397c03d3683720af203527c36e2bd83ab6") -- Depot 315217
+setManifestid(315217, "520277345523201444", 6233480843)
+addappid(315218, 1, "0a751ccef3454275907a0e4349eb73ddf25f9c4cfadf6574927bf1cc50f4a387") -- Depot 315218
+setManifestid(315218, "4075749034952776096", 6224522274)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2474090) -- Suicide Squad Kill the Justice League - Preorder Content
+addappid(2474100) -- Suicide Squad Kill the Justice League - Deluxe Edition Content
+addappid(2474110) -- Suicide Squad Kill the Justice League - Digital Deluxe Edition Content
+addappid(2716350) -- Suicide Squad Kill the Justice League - Digital Deluxe Edition Upgrade
