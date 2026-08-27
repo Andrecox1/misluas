00bb08e21d49aa22805730414951a082b85caa78
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-27 14:42:46 -0500

    2275300

---
+++ 2275300/2275300.lua
@@ -0,0 +1,12 @@
+-- 2275300's Lua and Manifest Created by Hubcap Manifest
+-- Folk Emerging
+-- Created: August 27, 2026 at 13:31:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2275300) -- Folk Emerging
+-- MAIN APP DEPOTS
+addappid(2275301, 1, "9b910623b214f0a20eba6b70fa2898998cebb9b993ff0d500336e835ef3e6e09") -- Depot 2275301
+setManifestid(2275301, "6192901926185790366", 1964301777)
