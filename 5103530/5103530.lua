ff449b4d59dbe928213e4131a71c2e850a4d2e28
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:35 -0500

    add 5103530/5103530.lua

---
+++ 5103530/5103530.lua
@@ -0,0 +1,12 @@
+-- 5103530's Lua and Manifest Created by Hubcap Manifest
+-- Scam Artist Demo
+-- Created: August 27, 2026 at 23:58:36 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5103530) -- Scam Artist Demo
+-- MAIN APP DEPOTS
+addappid(5103531, 1, "14cec5acef65000e7b4c69ea813ed3a3d9af6cae1f7603cf9c541be9e9bd92a7") -- Depot 5103531
+setManifestid(5103531, "1680002123011933507", 107562948)
