34c382c3edfc5b3b66ad3d8bf68a584e33f30458
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:46:06 -0500

    add 5024840/5024840.lua

---
+++ 5024840/5024840.lua
@@ -0,0 +1,15 @@
+-- 5024840's Lua and Manifest Created by Hubcap Manifest
+-- 上传器/Uploader
+-- Created: August 28, 2026 at 11:46:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(5024840) -- 上传器/Uploader
+-- MAIN APP DEPOTS
+addappid(5024841, 1, "ba5253fbc742d4b66e9d269155322d9680f4c3c60489d53b467b37b25cd7b61c") -- Depot 5024841
+setManifestid(5024841, "9026687468880184516", 675968866)
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(5034780) -- 获取ACCESS访问密钥/Access to ACCESS (unreleased)
