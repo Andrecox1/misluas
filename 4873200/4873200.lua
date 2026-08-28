bfd5030ff731ab607392b24acd611962ad299d90
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:04 -0500

    add 4873200/4873200.lua

---
+++ 4873200/4873200.lua
@@ -0,0 +1,16 @@
+-- 4873200's Lua and Manifest Created by Hubcap Manifest
+-- Mr Twiddle's Wee Diddy Farm
+-- Created: August 28, 2026 at 05:50:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4873200) -- Mr Twiddle's Wee Diddy Farm
+-- MAIN APP DEPOTS
+addappid(4873201, 1, "8cc842da3a284b1ab1885a81c02d365ef0452ed0e3595d7bd4ba937d60e78196") -- Depot 4873201
+setManifestid(4873201, "1548536377276408487", 283737048)
+addappid(4873202, 1, "f72e39f1e44730502d870e79c8fe4e705e2eb7abc3a76361e2aefc611fd071c1") -- Depot 4873202
+setManifestid(4873202, "1392081064988136317", 250874096)
+addappid(4873203, 1, "0a79b9e87410fea9b65e0f4bc0bd2bda418da9217603a326dfdd3d2699a61c22") -- Depot 4873203
+setManifestid(4873203, "5763754809786434962", 368362468)
