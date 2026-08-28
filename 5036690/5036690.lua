72edc31d29537a824573cb008cc6646a650742a5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:30 -0500

    add 5036690/5036690.lua

---
+++ 5036690/5036690.lua
@@ -0,0 +1,12 @@
+-- 5036690's Lua and Manifest Created by Hubcap Manifest
+-- The Yard: Escape from Prison Demo
+-- Created: August 28, 2026 at 12:47:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5036690) -- The Yard: Escape from Prison Demo
+-- MAIN APP DEPOTS
+addappid(5036691, 1, "c563d1739e6c2567e2d4040f028d25187705dc0d06877a1f1b9c46ed2e63c9e7") -- Depot 5036691
+setManifestid(5036691, "4149045247579965201", 485636253)
