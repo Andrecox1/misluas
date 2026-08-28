2ef57690d15b165e188b9c96add1367b4ad1a8db
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:46:43 -0500

    add 1142040/1142040.lua

---
+++ 1142040/1142040.lua
@@ -0,0 +1,20 @@
+-- 1142040's Lua and Manifest Created by Hubcap Manifest
+-- For Stella
+-- Created: August 28, 2026 at 09:35:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1142040) -- For Stella
+-- MAIN APP DEPOTS
+addappid(1142041, 1, "7e32f48dac7b1a7fc9450a539cf17374adba92a56dc3522f17b7fef4b8101fdf") -- Depot 1142041
+setManifestid(1142041, "118416519322441378", 1518469694)
+addappid(1142042, 1, "3c0f42d95a7a0ec9939e712659c5cbcba2b5012ca5efe3a5d2c6d0aef5b955b6") -- Depot 1142042
+setManifestid(1142042, "8588860375198889587", 1675151740)
+addappid(1142043, 1, "894302795087f05f56d12ff705364e1ae226e52129ad6d6582bf0f115e3a727b") -- Depot 1142043
+setManifestid(1142043, "7416775421237523697", 1514541259)
+addappid(1142044, 1, "2e1a83dd23b69500bd3151dda7c98a7cd8ac7217f659f573de6b345bf6e4641d") -- Depot 1142044
+setManifestid(1142044, "6977805517043133141", 1671226856)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1142045) -- Depot 1142045 (empty depot)
