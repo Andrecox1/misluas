7ce5a937d0e3c0e66f3e5b2696de8c6d2ae8965a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:12 -0500

    add 4922900/4922900.lua

---
+++ 4922900/4922900.lua
@@ -0,0 +1,18 @@
+-- 4922900's Lua and Manifest Created by Hubcap Manifest
+-- Beast Market
+-- Created: August 28, 2026 at 09:04:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 5
+
+-- MAIN APPLICATION
+addappid(4922900) -- Beast Market
+-- MAIN APP DEPOTS
+addappid(4922901, 1, "26ef34966e013b9e7646b5b147c37f71cd0d9227c3c3401c43bd8e97a601d2bc") -- Depot 4922901
+setManifestid(4922901, "4990838959675895440", 788657393)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4975120) -- Beast Market - Hunters Bedroll
+addappid(4975130) -- Beast Market - Grand Warehouse
+addappid(4975140) -- Beast Market - Tactics Computer
+addappid(4975150) -- Beast Market - Homestead Charter
+addappid(4975160) -- Beast Market - Workshop Assistant
