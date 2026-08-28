f1f174c578aabd35062fc198ecc49597d67af004
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:46:37 -0500

    add 4298500/4298500.lua

---
+++ 4298500/4298500.lua
@@ -0,0 +1,12 @@
+-- 4298500's Lua and Manifest Created by Hubcap Manifest
+-- Last Breath
+-- Created: August 28, 2026 at 09:35:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4298500) -- Last Breath
+-- MAIN APP DEPOTS
+addappid(4298501, 1, "8f6a36d642f08f18e19f5bdb7525cb614acf9c9fb31298a9346cd2f09384b786") -- Depot 4298501
+setManifestid(4298501, "1217670027503249949", 862098827)
