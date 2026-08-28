f4179c3d10f4ab019d3a6ef7cbf08ad472d1c32e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:37 -0500

    add 4993110/4993110.lua

---
+++ 4993110/4993110.lua
@@ -0,0 +1,12 @@
+-- 4993110's Lua and Manifest Created by Hubcap Manifest
+-- Ceramic · Stainless Steel · Neurotransmitter
+-- Created: August 28, 2026 at 03:01:57 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4993110) -- Ceramic · Stainless Steel · Neurotransmitter
+-- MAIN APP DEPOTS
+addappid(4993111, 1, "a0634e1ebe8230de1029339704bcb8c5ef437605c92f1482b52e1427e245e33b") -- Depot 4993111
+setManifestid(4993111, "8715500199226042327", 366594030)
