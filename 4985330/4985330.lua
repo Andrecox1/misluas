cb00775fbe4acc86f1e9a3e8a39c9fcf0c2731e6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:30 -0500

    add 4985330/4985330.lua

---
+++ 4985330/4985330.lua
@@ -0,0 +1,12 @@
+-- 4985330's Lua and Manifest Created by Hubcap Manifest
+-- Arena Blues - Requiem for 35
+-- Created: August 28, 2026 at 00:59:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4985330) -- Arena Blues - Requiem for 35
+-- MAIN APP DEPOTS
+addappid(4985331, 1, "5bcdd17d0ea898f6a9001fcd65bb5beba9d44155516b978e62c15292e9dc34c4") -- Depot 4985331
+setManifestid(4985331, "6752790575575984886", 811570824)
