c3e24610b35183a05c0f7cdfb00a9262f46b5c25
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:22 -0500

    add 4891040/4891040.lua

---
+++ 4891040/4891040.lua
@@ -0,0 +1,12 @@
+-- 4891040's Lua and Manifest Created by Hubcap Manifest
+-- Gods Shall Tremble
+-- Created: August 28, 2026 at 12:47:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4891040) -- Gods Shall Tremble
+-- MAIN APP DEPOTS
+addappid(4891041, 1, "443f2ac614463e31b43e8e1114dd9d60a45836f36d171a0bf4a804e84d73c1f4") -- Depot 4891041
+setManifestid(4891041, "8034399302845951191", 37986175)
