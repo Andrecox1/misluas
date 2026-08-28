c4d79c6f60929202c9567aefa965f87c8c084fdd
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:13 -0500

    add 3345550/3345550.lua

---
+++ 3345550/3345550.lua
@@ -0,0 +1,12 @@
+-- 3345550's Lua and Manifest Created by Hubcap Manifest
+-- Tactical Annihilation
+-- Created: August 28, 2026 at 02:59:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3345550) -- Tactical Annihilation
+-- MAIN APP DEPOTS
+addappid(3345551, 1, "7277d58af52f3336bf130bd779a43d817079b1f432bc43304fcff89826fb1e08") -- Depot 3345551
+setManifestid(3345551, "7713619013650073572", 1994640029)
