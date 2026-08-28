80ed7af0c24fc9858156472fbe8a41829f653bfa
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:52 -0500

    add 5076950/5076950.lua

---
+++ 5076950/5076950.lua
@@ -0,0 +1,12 @@
+-- 5076950's Lua and Manifest Created by Hubcap Manifest
+-- Crazy Bird Hunt Demo
+-- Created: August 27, 2026 at 23:58:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5076950) -- Crazy Bird Hunt Demo
+-- MAIN APP DEPOTS
+addappid(5076951, 1, "69e26f016ea3d8d83443ef95355d91949f91bdb4b086b0508afac731c4ec9a48") -- Depot 5076951
+setManifestid(5076951, "5045892693146269728", 235224559)
