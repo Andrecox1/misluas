fa69182c62d0b5a6caf98218109f78015340fc81
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:05 -0500

    add 3894600/3894600.lua

---
+++ 3894600/3894600.lua
@@ -0,0 +1,12 @@
+-- 3894600's Lua and Manifest Created by Hubcap Manifest
+-- Saga of Yurina
+-- Created: August 28, 2026 at 03:00:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3894600) -- Saga of Yurina
+-- MAIN APP DEPOTS
+addappid(3894601, 1, "19694c714b75b757e5e320eb22a2694fb251da9dd06c48076d13b83c95d00c43") -- Depot 3894601
+setManifestid(3894601, "2187923554109321440", 414416983)
