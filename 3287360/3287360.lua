40b9a07373e65054d80602c245c642a62813980b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:18 -0500

    add 3287360/3287360.lua

---
+++ 3287360/3287360.lua
@@ -0,0 +1,16 @@
+-- 3287360's Lua and Manifest Created by Hubcap Manifest
+-- Projects II: Silent Valley
+-- Created: August 28, 2026 at 02:59:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3287360) -- Projects II: Silent Valley
+-- MAIN APP DEPOTS
+addappid(3287361, 1, "33a3f1f13534199b36bdd38e7e122d21fb1b9182a269dafd78238a92b9b30195") -- Depot 3287361
+setManifestid(3287361, "7866217182016558680", 1725347310)
+addappid(3287362, 1, "865d84b8d35a92aa20ce8b2b03432d4521950307bbbb4249dc60cad5cb80aa9e") -- Depot 3287362
+setManifestid(3287362, "2563552342864443590", 1738100848)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(5170380) -- Depot 5170380 (empty depot)
