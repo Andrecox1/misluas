15a2291721fc0a214109f97a9c863238205a43e6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:26 -0500

    add 4584700/4584700.lua

---
+++ 4584700/4584700.lua
@@ -0,0 +1,12 @@
+-- 4584700's Lua and Manifest Created by Hubcap Manifest
+-- Island Off Outer Dancing
+-- Created: August 28, 2026 at 01:01:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4584700) -- Island Off Outer Dancing
+-- MAIN APP DEPOTS
+addappid(4584701, 1, "7f0da1d7dcc813cb3fbd8df14f0027d9b1c4f2edd76c3439ab96fdfbb4974baa") -- Depot 4584701
+setManifestid(4584701, "5239065752916619931", 2649876282)
