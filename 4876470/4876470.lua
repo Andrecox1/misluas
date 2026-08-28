f1de4b7e53d999351c642e20d54d7649fb43b1ee
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:34 -0500

    add 4876470/4876470.lua

---
+++ 4876470/4876470.lua
@@ -0,0 +1,12 @@
+-- 4876470's Lua and Manifest Created by Hubcap Manifest
+-- Gnat Pack Demo
+-- Created: August 28, 2026 at 03:03:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4876470) -- Gnat Pack Demo
+-- MAIN APP DEPOTS
+addappid(4876471, 1, "3747fd649600c34ad6584d4229fcead3d221c819a7d3bcebcae02bf288c26146") -- Depot 4876471
+setManifestid(4876471, "5078684409313779775", 128541576)
