fc5a8ec039c4c0a5682af83bc5949b0d792eeae5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:42 -0500

    add 4950050/4950050.lua

---
+++ 4950050/4950050.lua
@@ -0,0 +1,12 @@
+-- 4950050's Lua and Manifest Created by Hubcap Manifest
+-- The Magical girl Childhood friend lives Next Door
+-- Created: August 28, 2026 at 05:05:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4950050, 1, "46f8bfbfd9e8b50bfe281882d2da57d4c97d0fa33198039a32a7862487f9b415") -- The Magical girl Childhood friend lives Next Door
+-- MAIN APP DEPOTS
+addappid(4950051, 1, "b7154f5489d1587ce037418a86b769fa3d7bde58ff91cd6862fe59cfad823f80") -- Depot 4950051
+setManifestid(4950051, "441236418964410900", 3117219866)
