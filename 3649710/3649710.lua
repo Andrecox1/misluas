9eb3832e9a07fcc583acabad89fe7d787dd07672
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:09 -0500

    add 3649710/3649710.lua

---
+++ 3649710/3649710.lua
@@ -0,0 +1,12 @@
+-- 3649710's Lua and Manifest Created by Hubcap Manifest
+-- Bluff Roulette
+-- Created: August 28, 2026 at 03:00:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3649710, 1, "53507a113d041e3338993ea1853ad4ff8f84c1ee35ef2e7410f4ac203e1c99f4") -- Bluff Roulette
+-- MAIN APP DEPOTS
+addappid(3649711, 1, "ed1e8f353cacf504f8c1ac6b6ebafa4cc5720e1c82297f6ec9968049ce2e4d94") -- Depot 3649711
+setManifestid(3649711, "5988007312067876339", 531036419)
