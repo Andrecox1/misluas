b9aeb224100a329749e0d4021f14f1e04cc89a7e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 10:43:14 -0500

    add 4676750/4676750.lua

---
+++ 4676750/4676750.lua
@@ -0,0 +1,12 @@
+-- 4676750's Lua and Manifest Created by Hubcap Manifest
+-- 点击地牢 Tap Dungeon
+-- Created: August 28, 2026 at 11:29:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4676750) -- 点击地牢 Tap Dungeon
+-- MAIN APP DEPOTS
+addappid(4676751, 1, "22e105025451a02291eda686ca17f91eecbe35dbff35a9019a73a64d2a1091bd") -- Depot 4676751
+setManifestid(4676751, "8823413989344839704", 438983195)
