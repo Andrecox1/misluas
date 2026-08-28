2ec9c5d3d2257cf19ed9d1e3c683068a9219b730
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:47:00 -0500

    add 2263050/2263050.lua

---
+++ 2263050/2263050.lua
@@ -0,0 +1,12 @@
+-- 2263050's Lua and Manifest Created by Hubcap Manifest
+-- Skyland
+-- Created: August 28, 2026 at 12:46:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2263050) -- Skyland
+-- MAIN APP DEPOTS
+addappid(2263051, 1, "83d3dc5d42ad7e5976cac9eca43bc67c48fe6b047564d3ed961679a3b2a3b5aa") -- Depot 2263051
+setManifestid(2263051, "6395982101209121816", 42912958)
