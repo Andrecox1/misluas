c2153edb983fe18200d75fa31454e3c18950a1fd
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:00 -0500

    add 4341260/4341260.lua

---
+++ 4341260/4341260.lua
@@ -0,0 +1,18 @@
+-- 4341260's Lua and Manifest Created by Hubcap Manifest
+-- WebCam: Behind The Screen
+-- Created: August 28, 2026 at 03:00:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4341260, 1, "3c446936205508f6f6f4583d45b40aa88c12e76c530a09c0f56921f9c426715f") -- WebCam: Behind The Screen
+-- MAIN APP DEPOTS
+addappid(4341261, 1, "fe9cbb5dc0323d133589a0ee47c89a00709c5d224e1a498934327e5608bba505") -- Depot 4341261
+setManifestid(4341261, "3869797416102308070", 3596556256)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
