9a2207b288ac3968037d35e5ae11694aaf9f59e3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:48:46 -0500

    add 5085280/5085280.lua

---
+++ 5085280/5085280.lua
@@ -0,0 +1,14 @@
+-- 5085280's Lua and Manifest Created by Hubcap Manifest
+-- Dust Vein Demo
+-- Created: August 28, 2026 at 12:48:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5085280) -- Dust Vein Demo
+-- MAIN APP DEPOTS
+addappid(5085287, 1, "8112bbfe8141e5117e4b38cea3f04b68707844b07b156d0058de9b1360b6c916") -- Depot 5085287
+setManifestid(5085287, "4540309438493146504", 1798664866)
+addappid(5085281, 1, "a55b0d5bea8d6c4d0f69df1d03dc771c039111428bda4cf26429481f3405af52") -- Depot 5085281
+setManifestid(5085281, "7293859548688360375", 1798664866)
