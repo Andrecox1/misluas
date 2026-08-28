ff5f67c071f2598fdde4e1e2588cfcd0274e5c58
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 11:46:33 -0500

    add 4529120/4529120.lua

---
+++ 4529120/4529120.lua
@@ -0,0 +1,12 @@
+-- 4529120's Lua and Manifest Created by Hubcap Manifest
+-- Alice's Gaze
+-- Created: August 28, 2026 at 09:35:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4529120, 1, "f2b35865af0ad8fba58c85f2eebe46d825158cdfb8062e07a82c6e76fcd3b018") -- Alice's Gaze
+-- MAIN APP DEPOTS
+addappid(4529121, 1, "059a8acd1babf801a92d41e4273b5b3b1a023cd35e3ff0ef0480b5db5458776d") -- Depot 4529121
+setManifestid(4529121, "7281948126511761819", 171765136)
