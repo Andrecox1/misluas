132c5dbc403e63e420ecabaacba6a402fe04c106
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:41 -0500

    add 2163180/2163180.lua

---
+++ 2163180/2163180.lua
@@ -0,0 +1,12 @@
+-- 2163180's Lua and Manifest Created by Hubcap Manifest
+-- ODDADA Demo
+-- Created: August 28, 2026 at 14:49:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2163180) -- ODDADA Demo
+-- MAIN APP DEPOTS
+addappid(2163181, 1, "67d69fbe5e49f65e2c4dc61418d86ae9ff68a9c99539b5e46542dc76c286176d") -- Depot 2163181
+setManifestid(2163181, "386810829050056868", 929790715)
