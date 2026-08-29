5c77e8be22f079eafc5861cb75300ad82bc91211
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:34 -0500

    add 4405490/4405490.lua

---
+++ 4405490/4405490.lua
@@ -0,0 +1,12 @@
+-- 4405490's Lua and Manifest Created by Hubcap Manifest
+-- Pocket Flight Demo
+-- Created: August 28, 2026 at 14:49:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4405490) -- Pocket Flight Demo
+-- MAIN APP DEPOTS
+addappid(4405491, 1, "66d0d9f1b10045b9f2d47f05df6015355816df1c07e7c8888cdc7acf1d2c18b1") -- Depot 4405491
+setManifestid(4405491, "7142592224127226419", 1074771892)
