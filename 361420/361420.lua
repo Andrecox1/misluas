8198d6d493ff3bda6f248260150324cdf06e73b6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:33:07 -0500

    361420

---
+++ 361420/361420.lua
@@ -0,0 +1,27 @@
+-- 361420's Lua and Manifest Created by Hubcap Manifest
+-- ASTRONEER
+-- Created: August 12, 2026 at 18:44:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 8
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(361420, 1, "42b70c4530261e89bf4e49edb02f7f08fad27c78a425e15ea124dcf8543e0e1a") -- ASTRONEER
+-- MAIN APP DEPOTS
+addappid(361422, 1, "471756d013ea0594ce2fe60fa33915663d90cf64b086b5b992b85f5fc73d5283") -- Astroneer Windows
+setManifestid(361422, "500421776047685533", 3215396255)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2566990) -- ASTRONEER Essential Bundle
+addappid(2567000) -- ASTRONEER Suit Bundle
+addappid(2567010) -- ASTRONEER Athleisure Bundle
+addappid(2876620) -- ASTRONEER Evolution Bundle
+addappid(3042220) -- Astroneer Glitchwalkers
+addappid(3315070) -- ASTRONEER Glitchwalkers - Deluxe Cosmetic Pack
+addappid(3884830) -- Astroneer Megatech
+addappid(4144820) -- Astroneer Megatech - Deluxe Cosmetic Pack
