32fc2759f4ea5bd5fa094325d314a20c57e74b54
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-29 09:24:30 -0500

    add 5053820/5053820.lua

---
+++ 5053820/5053820.lua
@@ -0,0 +1,14 @@
+-- 5053820's Lua and Manifest Created by Hubcap Manifest
+-- Mimic Party
+-- Created: August 29, 2026 at 01:10:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(5053820, 1, "3687a910eb76c9e3eccddcb2ce6cc97c4bc5667ef8e20e668cbedc150283773a") -- Mimic Party
+-- MAIN APP DEPOTS
+addappid(5053821, 1, "3bf0ccc9e3c7c0ff61adcadc28e45680676fe6f9a82c9c4ad55c417c532c35e6") -- Depot 5053821
+setManifestid(5053821, "9007192332013530735", 628072475)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(5168110) -- All Characters
