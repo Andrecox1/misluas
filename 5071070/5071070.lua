6092f9ae9c68c879ea1855b1eeadd14beee0e89b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:25 -0500

    add 5071070/5071070.lua

---
+++ 5071070/5071070.lua
@@ -0,0 +1,12 @@
+-- 5071070's Lua and Manifest Created by Hubcap Manifest
+-- City 33
+-- Created: August 27, 2026 at 23:58:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5071070) -- City 33
+-- MAIN APP DEPOTS
+addappid(5071071, 1, "ea06b3380f81b3ccef0e4be806a71362dfcdc2885e4d761362d068c208e3ba9c") -- Depot 5071071
+setManifestid(5071071, "6553936472447951157", 85588141)
