8892fa4912ea2e7fc641ad69d6433575f847fd6e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:06 -0500

    add 4819820/4819820.lua

---
+++ 4819820/4819820.lua
@@ -0,0 +1,12 @@
+-- 4819820's Lua and Manifest Created by Hubcap Manifest
+-- SOLIPSISTIC
+-- Created: August 28, 2026 at 14:36:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4819820) -- SOLIPSISTIC
+-- MAIN APP DEPOTS
+addappid(4819821, 1, "0c75c727f4f9a38b897da71a32b0e24a6c9aef92f86b396bc304e2380453576a") -- Depot 4819821
+setManifestid(4819821, "3681877322621165262", 3068577567)
