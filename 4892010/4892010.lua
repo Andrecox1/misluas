e5766ee06eb3179f2c93d6dc7e9e34e2d7556899
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:20 -0500

    add 4892010/4892010.lua

---
+++ 4892010/4892010.lua
@@ -0,0 +1,12 @@
+-- 4892010's Lua and Manifest Created by Hubcap Manifest
+-- Bomb Farm
+-- Created: August 28, 2026 at 21:36:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4892010) -- Bomb Farm
+-- MAIN APP DEPOTS
+addappid(4892011, 1, "666a0eef6fc31f1ac561320e184f18ff27213f33fda84eb6bb9a2b1a4afeb338") -- Depot 4892011
+setManifestid(4892011, "5337673480750106025", 648191124)
