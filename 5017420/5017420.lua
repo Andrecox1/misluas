013bdc71be8ec4ca0f80c98add8319da88c615c6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:21 -0500

    add 5017420/5017420.lua

---
+++ 5017420/5017420.lua
@@ -0,0 +1,12 @@
+-- 5017420's Lua and Manifest Created by Hubcap Manifest
+-- Beekeeper Simulator Demo
+-- Created: August 28, 2026 at 12:53:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5017420) -- Beekeeper Simulator Demo
+-- MAIN APP DEPOTS
+addappid(5017421, 1, "7028b6fa68734ab876aa4811086565d303038031763475c7f317972f3e484251") -- Depot 5017421
+setManifestid(5017421, "1167925445995644996", 557162894)
