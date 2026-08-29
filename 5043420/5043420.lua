3e3df29f9be70e729a66e558989082200140ab2d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:45 -0500

    add 5043420/5043420.lua

---
+++ 5043420/5043420.lua
@@ -0,0 +1,12 @@
+-- 5043420's Lua and Manifest Created by Hubcap Manifest
+-- 8bit Salvage Demo
+-- Created: August 27, 2026 at 23:57:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5043420) -- 8bit Salvage Demo
+-- MAIN APP DEPOTS
+addappid(5043421, 1, "68d199d6ab279171451737a749442dd89555e0e1b33253f83d7b52d4e5c46a4b") -- Depot 5043421
+setManifestid(5043421, "2862227762420154797", 259974526)
