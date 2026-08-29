1cf3342711358c6768eec09cb855334fe18d46e7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:30 -0500

    add 4882890/4882890.lua

---
+++ 4882890/4882890.lua
@@ -0,0 +1,12 @@
+-- 4882890's Lua and Manifest Created by Hubcap Manifest
+-- Command Doctrine
+-- Created: August 28, 2026 at 21:36:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4882890) -- Command Doctrine
+-- MAIN APP DEPOTS
+addappid(4882891, 1, "4a4aff806c7775a7945319db06f1abaf26db48a8114e34fbc17752a496361557") -- Depot 4882891
+setManifestid(4882891, "6017973238974166882", 679430679)
