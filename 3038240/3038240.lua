520414c9c7e9145460a2d286a31625b2c9cbbbf8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:55 -0500

    add 3038240/3038240.lua

---
+++ 3038240/3038240.lua
@@ -0,0 +1,12 @@
+-- 3038240's Lua and Manifest Created by Hubcap Manifest
+-- The Echo Paradox Demo
+-- Created: August 28, 2026 at 14:49:07 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3038240) -- The Echo Paradox Demo
+-- MAIN APP DEPOTS
+addappid(3038241, 1, "1aa0022f63eb4ad4d3dd02c99e0021dad3ba631bc9df3c8f5d93ce2d2efb181f") -- Depot 3038241
+setManifestid(3038241, "2541452612461616701", 6470219202)
