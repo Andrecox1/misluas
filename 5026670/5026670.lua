fc3eae9a8615886cc046ad140ecb80c29100c6ca
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:53 -0500

    add 5026670/5026670.lua

---
+++ 5026670/5026670.lua
@@ -0,0 +1,12 @@
+-- 5026670's Lua and Manifest Created by Hubcap Manifest
+-- No One Prays in Ovruch
+-- Created: August 27, 2026 at 23:57:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5026670) -- No One Prays in Ovruch
+-- MAIN APP DEPOTS
+addappid(5026671, 1, "a511f2510c2b56437c6bcb7574fe4b7bbacd9d3a9202b0f918b06337e77269dc") -- Depot 5026671
+setManifestid(5026671, "8968718046979905812", 3170463742)
