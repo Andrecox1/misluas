b49f545974bd09cc0f59dcbadd0f8e515155b62d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:02 -0500

    add 3197790/3197790.lua

---
+++ 3197790/3197790.lua
@@ -0,0 +1,12 @@
+-- 3197790's Lua and Manifest Created by Hubcap Manifest
+-- Exer Gale Demo
+-- Created: August 28, 2026 at 14:48:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3197790) -- Exer Gale Demo
+-- MAIN APP DEPOTS
+addappid(3197792, 1, "1f7fcd5051c8209ad704711a77cde24391c28cea3a9aa0fdf6fdef0e2ecd61d3") -- Depot 3197792
+setManifestid(3197792, "4929331422485876099", 805491999)
