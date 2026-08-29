0d5fcc3b2af579348db760ff57c3b502fa3acb2a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:38 -0500

    add 3521070/3521070.lua

---
+++ 3521070/3521070.lua
@@ -0,0 +1,12 @@
+-- 3521070's Lua and Manifest Created by Hubcap Manifest
+-- Founders Legacy Demo
+-- Created: August 28, 2026 at 14:49:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3521070) -- Founders Legacy Demo
+-- MAIN APP DEPOTS
+addappid(3521071, 1, "b9e34c7314255b40bcb797923aec8b29f5a752294fb714708bc0a3a799e7a7ef") -- Depot 3521071
+setManifestid(3521071, "5304825468429475113", 539029072)
