e9c2d8b585880d23745481c1ba9468024938421c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:50 -0500

    add 3211020/3211020.lua

---
+++ 3211020/3211020.lua
@@ -0,0 +1,12 @@
+-- 3211020's Lua and Manifest Created by Hubcap Manifest
+-- Paper Planes Demo
+-- Created: August 28, 2026 at 14:49:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3211020) -- Paper Planes Demo
+-- MAIN APP DEPOTS
+addappid(3211021, 1, "2b218cc865aa5aa475d412885d3c1918c5e1112236ed45799137fad7911a567e") -- Depot 3211021
+setManifestid(3211021, "2224533822192344706", 10585844621)
