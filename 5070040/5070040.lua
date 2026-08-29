4dd9d528e7b403109cd5458b8e7cead09bdf603b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-29 09:24:23 -0500

    add 5070040/5070040.lua

---
+++ 5070040/5070040.lua
@@ -0,0 +1,16 @@
+-- 5070040's Lua and Manifest Created by Hubcap Manifest
+-- The Last Cosmonaut
+-- Created: August 29, 2026 at 01:10:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5070040) -- The Last Cosmonaut
+-- MAIN APP DEPOTS
+addappid(5070041, 1, "7de0009856c242a391f1cc86c2586ccb03a5b3d63d7dbaeae30e21c8f3a7a518") -- Depot 5070041
+setManifestid(5070041, "4249257272709932841", 345572644)
+addappid(5070042, 1, "b5c5630ca7115a862b66726b89f1649dea0d16132e277570a1f9a09aa265df4a") -- Depot 5070042
+setManifestid(5070042, "5198225448933472451", 364745026)
+addappid(5070043, 1, "5312822fc2de524b962118182bd2c009a4821cca835a859c5c49cc2975acd497") -- Depot 5070043
+setManifestid(5070043, "7660649432351434939", 344907107)
