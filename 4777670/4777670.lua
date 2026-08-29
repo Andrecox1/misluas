a637b7b3de84725366452abc507469a46c06ab85
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-29 09:24:18 -0500

    add 4777670/4777670.lua

---
+++ 4777670/4777670.lua
@@ -0,0 +1,14 @@
+-- 4777670's Lua and Manifest Created by Hubcap Manifest
+-- Pliant
+-- Created: August 29, 2026 at 03:04:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4777670) -- Pliant
+-- MAIN APP DEPOTS
+addappid(4777671, 1, "819494195b887853a556677c7a08724c4d227a7ed8fafa9d3911466cae4b4143") -- Depot 4777671
+setManifestid(4777671, "4636178766232333824", 441292277)
+addappid(4777672, 1, "75b21138c226c4d95335d6aa889f83abe6bb6d0ccd9da00f2aa35d40387eb6b9") -- Depot 4777672
+setManifestid(4777672, "3661810060896246356", 465353191)
