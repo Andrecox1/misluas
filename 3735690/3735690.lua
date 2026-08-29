f651a8ccf92c8a9776146424240ba64caecd3c9a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:14 -0500

    add 3735690/3735690.lua

---
+++ 3735690/3735690.lua
@@ -0,0 +1,12 @@
+-- 3735690's Lua and Manifest Created by Hubcap Manifest
+-- Mags and Body Bags
+-- Created: August 28, 2026 at 21:37:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3735690) -- Mags and Body Bags
+-- MAIN APP DEPOTS
+addappid(3735691, 1, "c319d77d6ec50e516de315f2e60bf8d3fcf0fa72d11183302dcac282689f2e33") -- Depot 3735691
+setManifestid(3735691, "3395130813805083221", 1672354137)
