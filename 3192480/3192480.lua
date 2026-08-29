eb664c7e906413d1fc4ec6b39a1dc1831e8e514a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:59 -0500

    add 3192480/3192480.lua

---
+++ 3192480/3192480.lua
@@ -0,0 +1,12 @@
+-- 3192480's Lua and Manifest Created by Hubcap Manifest
+-- Lil' Airport Demo
+-- Created: August 28, 2026 at 14:49:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3192480) -- Lil' Airport Demo
+-- MAIN APP DEPOTS
+addappid(3192481, 1, "2f2b945b24102865e08447b8096b5f18ee6f11fd17103dad39c18753744d3590") -- Depot 3192481
+setManifestid(3192481, "5944356251368876231", 101596544)
