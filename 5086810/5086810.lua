2b561a7e8d4cb05849a1b8d3494a15818a96f24a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-29 09:24:14 -0500

    add 5086810/5086810.lua

---
+++ 5086810/5086810.lua
@@ -0,0 +1,12 @@
+-- 5086810's Lua and Manifest Created by Hubcap Manifest
+-- 鬼牢關
+-- Created: August 29, 2026 at 03:05:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5086810) -- 鬼牢關
+-- MAIN APP DEPOTS
+addappid(5086811, 1, "2e7f157c2995a851aab37614b2060a04e322a75fa2fb92190bdd6068aa9bf6b2") -- Depot 5086811
+setManifestid(5086811, "3489924678692665541", 469753166)
