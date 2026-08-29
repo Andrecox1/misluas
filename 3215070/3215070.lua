c01370681e507a3f53d0c1838961c349010f4a45
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:36:47 -0500

    add 3215070/3215070.lua

---
+++ 3215070/3215070.lua
@@ -0,0 +1,12 @@
+-- 3215070's Lua and Manifest Created by Hubcap Manifest
+-- Neon Express Demo
+-- Created: August 28, 2026 at 14:49:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3215070) -- Neon Express Demo
+-- MAIN APP DEPOTS
+addappid(3215071, 1, "899d60cffc3fc1a54ce27379ad544536dcf358ad3dd5bd38d96efb57c349370d") -- Depot 3215071
+setManifestid(3215071, "6713654140989487291", 261112528)
