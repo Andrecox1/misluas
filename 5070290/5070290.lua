0041a92408792b8f10bc537d691dc63319bb4a68
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:35 -0500

    add 5070290/5070290.lua

---
+++ 5070290/5070290.lua
@@ -0,0 +1,12 @@
+-- 5070290's Lua and Manifest Created by Hubcap Manifest
+-- Path of the Yaoguai Playtest
+-- Created: August 28, 2026 at 21:37:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5070290) -- Path of the Yaoguai Playtest
+-- MAIN APP DEPOTS
+addappid(5070291, 1, "5f86240cd8406d86f92e66d7cd404046c3bbf8f6f7bf2f028487ec25db83de4e") -- Depot 5070291
+setManifestid(5070291, "7711950283448493631", 2802781022)
