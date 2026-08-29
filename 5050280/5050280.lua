66ee42099db4eb951c07248442fa8109a36623f3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:17 -0500

    add 5050280/5050280.lua

---
+++ 5050280/5050280.lua
@@ -0,0 +1,16 @@
+-- 5050280's Lua and Manifest Created by Hubcap Manifest
+-- 20 Trials to Heaven
+-- Created: August 28, 2026 at 13:01:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(5050280) -- 20 Trials to Heaven
+-- MAIN APP DEPOTS
+addappid(5050281, 1, "14a8288e0a1a5f03049f39fb2304ce8c7ef40d4d0da63c3f1db057b7718b8eb6") -- Depot 5050281
+setManifestid(5050281, "6897737948783419362", 32343172)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- 20 Trials to Heaven Supporter Pack (AppID: 5120340) - missing depot keys
+-- addappid(5120340)
