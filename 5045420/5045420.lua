c81fb5e5f0c195e51f3ee2fd96ecb7c1ee337805
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:41 -0500

    add 5045420/5045420.lua

---
+++ 5045420/5045420.lua
@@ -0,0 +1,18 @@
+-- 5045420's Lua and Manifest Created by Hubcap Manifest
+-- Palehollow Hospital | 灰谷医院 Demo
+-- Created: August 27, 2026 at 23:58:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(5045420) -- Palehollow Hospital | 灰谷医院 Demo
+-- MAIN APP DEPOTS
+addappid(5045421, 1, "b48f7ae83911737390761e4a1ce2211633863fd2b05e0a238356775a12c9d35a") -- Depot 5045421
+setManifestid(5045421, "7536769337754559819", 3872524677)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
