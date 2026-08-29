d8d7c2296bfe4f28c53b678c097d006f4165e2be
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-28 20:37:49 -0500

    add 5038820/5038820.lua

---
+++ 5038820/5038820.lua
@@ -0,0 +1,12 @@
+-- 5038820's Lua and Manifest Created by Hubcap Manifest
+-- My Yandere Streamer Demo
+-- Created: August 27, 2026 at 23:57:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5038820) -- My Yandere Streamer Demo
+-- MAIN APP DEPOTS
+addappid(5038821, 1, "65c402b29df3184dd6c942d902f2d4d82ccba0253f8846f76b841eb8f4a4b5c9") -- Depot 5038821
+setManifestid(5038821, "2747963715638267513", 373115224)
