aa061b7e444d2410be9d963e0b3c5dfb6f4a9288
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-31 10:19:08 -0500

    connection-fix: 12120/12120.lua

---
+++ 12120/12120.lua
@@ -0,0 +1,22 @@
+-- 12120's Lua and Manifest Created by Hubcap Manifest
+-- Grand Theft Auto: San Andreas
+-- Created: September 29, 2025 at 15:18:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(12120) -- Grand Theft Auto: San Andreas
+-- MAIN APP DEPOTS
+addappid(12123, 1, "b6246a66a369124f35879ade188971257e9827d14f178abace4d759001e6191a") -- Grand Theft Auto San Andreas content 3
+setManifestid(12123, "3229936372288409207", 1707644556)
+addappid(12122, 1, "eaa6ab82216553e3e53f1de147e88096b378cbc8b0cd488fd2c4d047ff263389") -- Grand Theft Auto San Andreas content 2
+setManifestid(12122, "5085199700668523983", 1676856594)
+addappid(12121, 1, "d89825855fd6f4b802378b6110effadaea8db612d89de6cf00f75dc92f898bf0") -- Grand Theft Auto San Andreas content
+setManifestid(12121, "242870450180196986", 1538954181)
+addappid(12124, 1, "020b11d09c31b51ddf1f3de13d5a2e420501f31de6638914b7ecfee4b15e3fc8") -- gtasa_lv_content
+setManifestid(12124, "1305660839026218242", 1114564927)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
