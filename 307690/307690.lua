eb3b5e02e8a9b4a8ab01dfbb83e294b136ce2e31
Andrecox1 <andresfelipear3357963@gmail.com>
2026-08-31 23:52:19 -0500

    connection-fix: 307690/307690.lua

---
+++ 307690/307690.lua
@@ -0,0 +1,28 @@
+-- 307690's Lua and Manifest Created by Hubcap Manifest
+-- Sleeping Dogs: Definitive Edition
+-- Created: September 30, 2025 at 14:47:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(307690) -- Sleeping Dogs: Definitive Edition
+-- MAIN APP DEPOTS
+addappid(307691, 1, "16e218fadcf1fd663c320861c008944503ae0e2725445797fd5043efb3806809") -- Sleeping Dogs: Definitive Edition Content
+setManifestid(307691, "1259773366995347002", 17690184686)
+addappid(307692, 1, "ccb1da56b95d82ffb9c3d29e4c19b6a782988c14c610a8ca674ead32d0f6cc8d") -- Sleeping Dogs: Definitive Edition UI
+setManifestid(307692, "8969342104525223947", 213063896)
+addappid(307693, 1, "9deece342bce4e677c7c91c18a4e137aaa45e689533db5aa669ba83ea6cbc8cb") -- Sleeping Dogs: Definitive Edition Executable
+setManifestid(307693, "521771309705279162", 37490688)
+addappid(307694, 1, "dd8ea35584589906ffef100d5d805f816c71b9dc99efe76a4e4eab647baceda4") -- Sleeping Dogs: Definitive Edition Artbook
+setManifestid(307694, "5203513487252783908", 43868258)
+addappid(307695, 1, "1c80d5c2e7555bfabf795cfbbe8cb7449132c279f20bfbaa43351e72fe09b2d4") -- Sleeping Dogs: Definitive Edition Mac Data
+setManifestid(307695, "1186231055532829370", 18006032909)
+addappid(307696, 1, "a265aef4fa6f4034eff3b39b421a9405aa44454654e8c7778847df6bb3684c59") -- Sleeping Dogs: Definitive Edition Mac App
+setManifestid(307696, "7419791888443965893", 72126555)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
