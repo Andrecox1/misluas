4b16a874b32b13569810546f2746f41ca21e9f1c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-01 22:16:44 -0500

    1326470

--- 1326470/1326470.lua
+++ 1326470/1326470.lua
@@ -1,7 +1,7 @@
--- 1326470's Lua and Manifest Created by Morrenus
+-- 1326470's Lua and Manifest Created by Hubcap Manifest
 -- Sons Of The Forest
 -- Created: October 03, 2025 at 17:15:54 EDT
--- Website: https://manifest.morrenus.xyz/
+-- Website: https://hubcapmanifest.com/
 -- Total Depots: 1
 -- Total DLCs: 0
 
@@ -9,4 +9,4 @@
 addappid(1326470) -- Sons Of The Forest
 -- MAIN APP DEPOTS
 addappid(1326471, 1, "dcc8b76a7563c836c1aed54a46abf85e7fec5fccabc0dd8e176d2151173e508e") -- Depot 1326471
---setManifestid(1326471, "8162016777027728069", 15989503975)
+setManifestid(1326471, "8162016777027728069", 15989503975)
