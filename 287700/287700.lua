beb1b9b01e9a9f1c2d5d746a11ae0a0c84dd1930
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-05 21:18:09 -0500

    Add files via upload

--- 287700/287700.lua
+++ 287700/287700.lua
@@ -1,23 +1,23 @@
--- 287700's Lua and Manifest Created by Morrenus
+-- 287700's Lua and Manifest Created by Hubcap Manifest
 -- METAL GEAR SOLID V: THE PHANTOM PAIN
--- Created: September 30, 2025 at 00:29:09 EDT
--- Website: https://manifest.morrenus.xyz/
+-- Created: August 04, 2026 at 02:21:48 EDT
+-- Website: https://hubcapmanifest.com/
 -- Total Depots: 4
 -- Total DLCs: 14
 -- Shared Depots: 2
 
 -- MAIN APPLICATION
-addappid(287700) -- METAL GEAR SOLID V: THE PHANTOM PAIN
+addappid(287700, 1, "96a637d7740f26b2a43cc7d0c250d595d86d3bc03c81481df01cadd8680bd0c5") -- METAL GEAR SOLID V: THE PHANTOM PAIN
 -- MAIN APP DEPOTS
 addappid(287701, 1, "f1c3bbf22c23c0777bf23c71491e2398e216ade65fbd5597762b7fb64165dd4f") -- MGS V: THE PHANTOM PAIN ENG
---setManifestid(287701, "2518831260221836800", 31227338145)
+setManifestid(287701, "1506669297791974057", 31106322849)
 addappid(287702, 1, "5fff13d6b2e316f81e7fbce5e82cc9e77fdcc6c1daa0f723a3f347c1f2017287") -- MGS V: THE PHANTOM PAIN JPN
---setManifestid(287702, "5656962073680694207", 31200504159)
+setManifestid(287702, "2786532116982545317", 31073951583)
 -- SHARED DEPOTS (from other apps)
 addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
---setManifestid(228984, "2547553897526095397", 13742505)
+setManifestid(228984, "2547553897526095397", 13742505)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
---setManifestid(228990, "1829726630299308803", 102931551)
+setManifestid(228990, "1829726630299308803", 102931551)
 -- DLCS WITHOUT DEDICATED DEPOTS
 addappid(406540) -- METAL GEAR SOLID V THE PHANTOM PAIN - Fatigues (Naked Snake)
 addappid(406560) -- METAL GEAR SOLID V THE PHANTOM PAIN - Sneaking Suit (Naked Snake)
@@ -34,3 +34,16 @@ addappid(437231) -- METAL GEAR SOLID V THE PHANTOM PAIN - MGO Appeal Action Pack
 addappid(437232) -- METAL GEAR SOLID V THE PHANTOM PAIN - Support Data
 addtoken(437232, "17207916683457290835")
 addappid(437233) -- METAL GEAR SOLID V THE PHANTOM PAIN - MB Coin 2000
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(406540) -- Depot 406540 (empty depot)
+-- addappid(406560) -- Depot 406560 (empty depot)
+-- addappid(406580) -- Depot 406580 (empty depot)
+-- addappid(406590) -- Depot 406590 (empty depot)
+-- addappid(406600) -- Depot 406600 (empty depot)
+-- addappid(406610) -- Depot 406610 (empty depot)
+-- addappid(406620) -- Depot 406620 (empty depot)
+-- addappid(436030) -- Depot 436030 (empty depot)
+-- addappid(436040) -- Depot 436040 (empty depot)
+-- addappid(436050) -- Depot 436050 (empty depot)
+-- addappid(437230) -- Depot 437230 (empty depot)
+-- addappid(437231) -- Depot 437231 (empty depot)
