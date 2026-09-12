97a01fd61bd985232b8eebbbdd4172a49dcce324
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-12 15:00:25 -0500

    connection-fix: 1448440/1448440.lua

--- 1448440/1448440.lua
+++ 1448440/1448440.lua
@@ -1,26 +1,26 @@
 -- 1448440's Lua and Manifest Created by Hubcap Manifest
 -- Wo Long: Fallen Dynasty
--- Created: October 01, 2025 at 07:52:47 EDT
+-- Created: June 07, 2026 at 23:00:33 EDT
 -- Website: https://hubcapmanifest.com/
 -- Total Depots: 4
 -- Total DLCs: 11
 -- Shared Depots: 1
 
 -- MAIN APPLICATION
-addappid(1448440) -- Wo Long: Fallen Dynasty
+addappid(1448440, 1, "eb976df4cb1dde3afdefb3d24ff6485e769d904cbfdc5fa5a4b58de7435bb817") -- Wo Long: Fallen Dynasty
 -- MAIN APP DEPOTS
 addappid(1448441, 1, "ef3b4eaabfb5d6bd17666007dda635b99375509180b267fbba485e2df18b4ed0") -- Depot 1448441
---setManifestid(1448441, "5746039873445988019", 57763889120)
+setManifestid(1448441, "537498482973662688", 57763889120)
 addappid(1448442, 1, "4e508b0bc29665862ed41a6e37c28fe63fc758dca12297e0237f28b2df4b2fd7") -- Depot 1448442
---setManifestid(1448442, "1386628775558457295", 185806576)
+setManifestid(1448442, "4349854675463499286", 185899624)
 -- SHARED DEPOTS (from other apps)
 addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
---setManifestid(228988, "6645201662696499616", 29212173)
+setManifestid(228988, "6645201662696499616", 29212173)
 -- DLCS WITH DEDICATED DEPOTS
 -- Wo Long Fallen Dynasty Digital Art Book  Digital Mini Soundtrack (AppID: 2232890)
 addappid(2232890)
 addappid(2232890, 1, "c9d303abb2711b7678880b74da95738ae983c74990967e0607993544a12c5087") -- Wo Long Fallen Dynasty Digital Art Book  Digital Mini Soundtrack - Depot 2232890
---setManifestid(2232890, "4304578856547169949", 6088155350)
+setManifestid(2232890, "4304578856547169949", 6088155350)
 -- DLCS WITHOUT DEDICATED DEPOTS
 addappid(2205890) -- Wo Long Fallen Dynasty Baihu Armor
 addappid(2205891) -- Wo Long Fallen Dynasty Zhuque Armor
@@ -32,3 +32,7 @@ addappid(2205896) -- Wo Long Fallen Dynasty Upheaval in Jingxiang
 addappid(2612470) -- Wo Long Fallen Dynasty Indigo Dual Swords
 addappid(2612490) -- Wo Long Fallen Dynasty Indigo Garment Set
 addappid(2654720) -- Wo Long Fallen Dynasty Records of the Grand Historian
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1448443) -- Depot 1448443 (empty depot)
+-- addappid(1448444) -- Depot 1448444 (empty depot)
+-- addappid(1448445) -- Depot 1448445 (empty depot)
