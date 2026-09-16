045d600be5a7b3767d438af7b94de3c39350aed1
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:13:44 -0500

    add 2334150/2334150.lua

---
+++ 2334150/2334150.lua
@@ -0,0 +1,22 @@
+-- 2334150's Lua and Manifest Created by Hubcap Manifest
+-- Mechanical Fury
+-- Created: September 15, 2026 at 00:35:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2334150, 1, "ae27579cac69fc5f4a42c6789c958f44434eff75648b713e344d722c6bc3e5eb") -- Mechanical Fury
+-- MAIN APP DEPOTS
+addappid(2334151, 1, "4b922b5693f14b53b73e1f608c7149e8694aaa1be03b5e0d0856b67f2f0ba10d") -- Depot 2334151
+setManifestid(2334151, "6487208124893693159", 1456986640)
+addappid(2334152, 1, "ce530906495ebd062fdbe78457d19378ad413b2e1b20bb5d3234f24ab92aedf2") -- Depot 2334152
+setManifestid(2334152, "2570693827688542015", 1319191303)
+addappid(2334153, 1, "431b0df7e0b8e483a716707899caa189f2f792d18c9eb10a3d816a8500752b31") -- Depot 2334153
+setManifestid(2334153, "948524962286009299", 2439374712)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
