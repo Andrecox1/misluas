45520effee9d6da51dbb76c1f1deec69d17827a5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:07 -0500

    add 4121330/4121330.lua

---
+++ 4121330/4121330.lua
@@ -0,0 +1,14 @@
+-- 4121330's Lua and Manifest Created by Hubcap Manifest
+-- Flagfall
+-- Created: September 15, 2026 at 16:03:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4121330) -- Flagfall
+-- MAIN APP DEPOTS
+addappid(4121331, 1, "e31d84b43561778110216579b1c687dc9c2010b161b226dfe88cdcc0a1ca9d1d") -- Depot 4121331
+setManifestid(4121331, "6010948123769655501", 213822290)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(5218640) -- Flagfall - Supporter Pack
