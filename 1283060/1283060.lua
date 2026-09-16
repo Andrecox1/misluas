908a3fb7fda0c5f6f2bd728ebd51ba110039304a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:30 -0500

    add 1283060/1283060.lua

---
+++ 1283060/1283060.lua
@@ -0,0 +1,18 @@
+-- 1283060's Lua and Manifest Created by Hubcap Manifest
+-- Furry Shakespeare: Emperor Penguin Lear
+-- Created: September 14, 2026 at 13:41:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1283060) -- Furry Shakespeare: Emperor Penguin Lear
+-- MAIN APP DEPOTS
+addappid(1283061, 1, "4f838e26ce357d83992962648c13b2e57f2859454150ff58237b0d4422235dfe") -- LEAR WIN
+setManifestid(1283061, "2823707277639142226", 592627074)
+addappid(1283062, 1, "26b1ff787bc0a04f0b009f1f27d28552599e9576c9338790bfbf87b82ceececc") -- LEAR UX
+setManifestid(1283062, "2649988920439200249", 582998804)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
