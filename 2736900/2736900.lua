ccf1be7878ead2aecd962c0a5bb13c0400d05229
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:53 -0500

    add 2736900/2736900.lua

---
+++ 2736900/2736900.lua
@@ -0,0 +1,12 @@
+-- 2736900's Lua and Manifest Created by Hubcap Manifest
+-- Quest Industries
+-- Created: September 15, 2026 at 04:18:20 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2736900) -- Quest Industries
+-- MAIN APP DEPOTS
+addappid(2736902, 1, "4ecb1260960ed45817cf31168abe1c05b5b2b3b5c4b8ca38aab6fb8ea048fd1c") -- Depot 2736902
+setManifestid(2736902, "2188034510089007175", 258820864)
