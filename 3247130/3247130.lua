96d60997f1f65eb1fcc4e932cf5ab980ea1d06e3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:24 -0500

    add 3247130/3247130.lua

---
+++ 3247130/3247130.lua
@@ -0,0 +1,12 @@
+-- 3247130's Lua and Manifest Created by Hubcap Manifest
+-- 술도, 사랑도 어려운 아가씨
+-- Created: September 14, 2026 at 12:07:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3247130) -- 술도, 사랑도 어려운 아가씨
+-- MAIN APP DEPOTS
+addappid(3247131, 1, "9d04d6f1652d2d4be9734f619e14ac7368fd62e676a61dc69eb63ef0fcb99982") -- Depot 3247131
+setManifestid(3247131, "5886860002726310783", 1221665472)
