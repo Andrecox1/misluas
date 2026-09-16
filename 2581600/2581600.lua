5751fbdbbe5107c03b30a602765dd51d699c4f78
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:14 -0500

    add 2581600/2581600.lua

---
+++ 2581600/2581600.lua
@@ -0,0 +1,18 @@
+-- 2581600's Lua and Manifest Created by Hubcap Manifest
+-- PRIMA: First Rogues
+-- Created: September 15, 2026 at 15:54:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2581600) -- PRIMA: First Rogues
+-- MAIN APP DEPOTS
+addappid(2581601, 1, "f63a3ef672bfc6ee5fdc321903e0ea3e72cee81fd7957c48c1aef629e766afa9") -- Depot 2581601
+setManifestid(2581601, "4750216624632989900", 5256203300)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
