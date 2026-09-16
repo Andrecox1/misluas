84b74f18016ca2a724c8c47fbc13ea40495e4764
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:29 -0500

    add 3003230/3003230.lua

---
+++ 3003230/3003230.lua
@@ -0,0 +1,13 @@
+-- 3003230's Lua and Manifest Created by Hubcap Manifest
+-- The Falling Car
+-- Created: September 15, 2026 at 15:44:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3003230) -- The Falling Car
+addtoken(3003230, "5060927230796781498")
+-- MAIN APP DEPOTS
+addappid(3003231, 1, "f9b533e16dba3de846d471b89cdcf3a46308f0bca2e9a2f9cd1df2e57961e8af") -- Depot 3003231
+setManifestid(3003231, "4446379028653979585", 249518179)
