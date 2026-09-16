1d2b0ad3d8d8239819a1c1cc20fc00029f3fade8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:25:47 -0500

    add 4721090/4721090.lua

---
+++ 4721090/4721090.lua
@@ -0,0 +1,12 @@
+-- 4721090's Lua and Manifest Created by Hubcap Manifest
+-- ScagJPT
+-- Created: September 15, 2026 at 23:15:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4721090) -- ScagJPT
+-- MAIN APP DEPOTS
+addappid(4721095, 1, "56e5eb777df588088ac0f3292047fbc7f3658d3cb7caca5a7fefc7ee6f1f2408") -- Depot 4721095
+setManifestid(4721095, "2700589716633441792", 373177048)
