725eb42540259448dee3082f051dbe3bc70d5b0d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:57 -0500

    add 2011730/2011730.lua

---
+++ 2011730/2011730.lua
@@ -0,0 +1,12 @@
+-- 2011730's Lua and Manifest Created by Hubcap Manifest
+-- Long Drop
+-- Created: September 15, 2026 at 04:18:13 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2011730, 1, "6d67dd432198ac89a36832469a9b20b12ad3289c8096735056634e81a001723a") -- Long Drop
+-- MAIN APP DEPOTS
+addappid(2011731, 1, "41fd5f2b2679ebf075044f57f5dda6b07d456e03cc3cf15e9ebb11ef0976fd62") -- Depot 2011731
+setManifestid(2011731, "3535853861924664937", 475358566)
