72ef7f6a1dcc399a9699350e2010110a28c678fb
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:26 -0500

    add 5112800/5112800.lua

---
+++ 5112800/5112800.lua
@@ -0,0 +1,12 @@
+-- 5112800's Lua and Manifest Created by Hubcap Manifest
+-- Don't Stop Cutting The Trees!
+-- Created: September 14, 2026 at 14:18:42 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5112800) -- Don't Stop Cutting The Trees!
+-- MAIN APP DEPOTS
+addappid(5112801, 1, "a6ce1ffd14b4de57b2b6182f37bee4bed51e4710fccd1ab3047ac98ff211db61") -- Depot 5112801
+setManifestid(5112801, "4084783189740404802", 111804144)
