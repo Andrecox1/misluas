9a7cf805f5d25b9fdf5ab4628ac8bcca8cfb5f6d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:38 -0500

    add 2658770/2658770.lua

---
+++ 2658770/2658770.lua
@@ -0,0 +1,14 @@
+-- 2658770's Lua and Manifest Created by Hubcap Manifest
+-- Die drei ??? - Ruf der Trolle
+-- Created: September 15, 2026 at 11:02:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2658770, 1, "9176e689533e5a35c4494ea5ee2d587bc199520390416711b6a265e3818c23a1") -- Die drei ??? - Ruf der Trolle
+-- MAIN APP DEPOTS
+addappid(2658771, 1, "d1a34c583c7fe1dc50b531dc8aecfc7ab1e06e3471672233639ecbbfafde8f00") -- Depot 2658771
+setManifestid(2658771, "2415017444384185721", 616317549)
+addappid(2658772, 1, "2d497b70a31d13379e7b2803a34fffe4b465f1b036a7dab8cfe02611c414f14f") -- Depot 2658772
+setManifestid(2658772, "2037803733238468254", 616381797)
