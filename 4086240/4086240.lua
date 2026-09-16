0f90936406461351292471f4906650ab44871f5e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:09 -0500

    add 4086240/4086240.lua

---
+++ 4086240/4086240.lua
@@ -0,0 +1,14 @@
+-- 4086240's Lua and Manifest Created by Hubcap Manifest
+-- WRITHING
+-- Created: September 15, 2026 at 16:47:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4086240, 1, "500608317cb5bf434edf05ffd2facfd2c2484a45e6b4e98be93fba1b84b6a6f6") -- WRITHING
+-- MAIN APP DEPOTS
+addappid(4086241, 1, "61be48a953a0f5a282492ea9deb37b5e30fa389daffb4104314c68ad25bf1a64") -- Depot 4086241
+setManifestid(4086241, "3531145124274181381", 2341447040)
+addappid(4086242, 1, "23cd5fd597277cfbd903b1e2ef6b7d6d15c3b5ee21d856a83cff11c1481c2ec4") -- Depot 4086242
+setManifestid(4086242, "1191263290590065570", 2305817448)
