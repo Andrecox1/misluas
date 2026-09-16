61d659878e6179c7edfa9ae004b3b0a87e4c9697
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:11:19 -0500

    add 5130150/5130150.lua

---
+++ 5130150/5130150.lua
@@ -0,0 +1,12 @@
+-- 5130150's Lua and Manifest Created by Hubcap Manifest
+-- Put Back Stuff
+-- Created: September 15, 2026 at 23:11:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5130150, 1, "0e0c21579918fd0a6954f429fbf43687c0fe0b2b704558ab21e4cb46d0b0d4aa") -- Put Back Stuff
+-- MAIN APP DEPOTS
+addappid(5130151, 1, "2044e2444dca8cff5d0d07d9575c6acbe862886fb0e73ebe9bd695758ffc1e2b") -- Depot 5130151
+setManifestid(5130151, "2049815193004314070", 322798835)
