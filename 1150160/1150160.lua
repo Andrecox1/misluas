9fa056e257e7b8a56c2ec9544ba9c988c95bcd57
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:34 -0500

    add 1150160/1150160.lua

---
+++ 1150160/1150160.lua
@@ -0,0 +1,12 @@
+-- 1150160's Lua and Manifest Created by Hubcap Manifest
+-- KobberParty - Castle Explorer
+-- Created: September 15, 2026 at 11:06:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1150160) -- KobberParty - Castle Explorer
+-- MAIN APP DEPOTS
+addappid(1150161, 1, "20bb97aede85b383b8fe73fa6e783986e75eaf0ce5d5806f2c03d12bdc314a04") -- KobberParty - Castle Explorer Content
+setManifestid(1150161, "8117141472760123304", 648755614)
