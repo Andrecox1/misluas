75df13f403d3039a1f527912034b8cbaa056d46e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:18 -0500

    add 5237280/5237280.lua

---
+++ 5237280/5237280.lua
@@ -0,0 +1,12 @@
+-- 5237280's Lua and Manifest Created by Hubcap Manifest
+-- Road to Jukai Playtest
+-- Created: September 16, 2026 at 00:14:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5237280) -- Road to Jukai Playtest
+-- MAIN APP DEPOTS
+addappid(5237281, 1, "117318e1cf562776d5bdb90f521f97e062bea8e7b2f3462d30fc680909ff03bc") -- Depot 5237281
+setManifestid(5237281, "8086820923503199043", 5263575010)
