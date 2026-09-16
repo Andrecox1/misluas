2eae8c09c03eb447c22c7127ff82d640e4d4f726
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:22 -0500

    add 4825160/4825160.lua

---
+++ 4825160/4825160.lua
@@ -0,0 +1,12 @@
+-- 4825160's Lua and Manifest Created by Hubcap Manifest
+-- Drift Invaders
+-- Created: September 14, 2026 at 14:22:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4825160) -- Drift Invaders
+-- MAIN APP DEPOTS
+addappid(4825161, 1, "98d17408a31404354aaa0290e4d4f90b251a6c1faf47ff73a72c77af2a29fb0a") -- Depot 4825161
+setManifestid(4825161, "5152019116216971702", 1552219846)
