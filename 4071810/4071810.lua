174e0d61016de5b9cb41406e7de378c39d7a6624
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:15 -0500

    add 4071810/4071810.lua

---
+++ 4071810/4071810.lua
@@ -0,0 +1,12 @@
+-- 4071810's Lua and Manifest Created by Hubcap Manifest
+-- F1 Simulator
+-- Created: September 15, 2026 at 16:47:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4071810) -- F1 Simulator
+-- MAIN APP DEPOTS
+addappid(4071811, 1, "0e6ea7af3b58f6e32d185ca8d70137bb617c985071989f7e676f61bfb76a9b4e") -- Depot 4071811
+setManifestid(4071811, "2004747231401981332", 483363328)
