8e28cafa7ba0d9bd442f7730d1b3329ac84d02ea
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:39 -0500

    add 5122600/5122600.lua

---
+++ 5122600/5122600.lua
@@ -0,0 +1,12 @@
+-- 5122600's Lua and Manifest Created by Hubcap Manifest
+-- Powdertron
+-- Created: September 15, 2026 at 12:47:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5122600) -- Powdertron
+-- MAIN APP DEPOTS
+addappid(5122601, 1, "d8aa2b914c51ce7b96d0864ce0a63aab45bf0b882ca04d470b74d9fb424ea879") -- Depot 5122601
+setManifestid(5122601, "5045772121577551480", 15791046)
