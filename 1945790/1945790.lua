08b6adbba125dccc2ee784f45267f7da627ee851
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:52 -0500

    add 1945790/1945790.lua

---
+++ 1945790/1945790.lua
@@ -0,0 +1,14 @@
+-- 1945790's Lua and Manifest Created by Hubcap Manifest
+-- Die drei ??? - Plan der Chamäleonbande
+-- Created: September 15, 2026 at 08:58:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1945790, 1, "7cb60c5c636c131bf6f1dbfd8e9aca14057ef359380b7b59987961a5d7c32226") -- Die drei ??? - Plan der Chamäleonbande
+-- MAIN APP DEPOTS
+addappid(1945791, 1, "1a8f91c9d21220b1ff0e27604b13832300b96de666ef205f076800f5d696993d") -- Depot 1945791
+setManifestid(1945791, "5655773088147401108", 639939703)
+addappid(1945792, 1, "47c5306b8f09030304b8271d2ab8ef8c3ccd2a395f252a599b94ffee042dc326") -- Depot 1945792
+setManifestid(1945792, "1458872593983007499", 639356919)
