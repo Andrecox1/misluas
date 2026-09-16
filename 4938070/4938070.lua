3c080966e8557f53b914149803e4087bfbbc20c4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:05 -0500

    add 4938070/4938070.lua

---
+++ 4938070/4938070.lua
@@ -0,0 +1,12 @@
+-- 4938070's Lua and Manifest Created by Hubcap Manifest
+-- 我的钓鱼世界2
+-- Created: September 16, 2026 at 00:14:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4938070) -- 我的钓鱼世界2
+-- MAIN APP DEPOTS
+addappid(4938071, 1, "6d102a33fd11bdaa8a62cb685f15a995fc1c86701675efaf1ac197a8b6b32f43") -- Depot 4938071
+setManifestid(4938071, "4279136015207729195", 2485226177)
