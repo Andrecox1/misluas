9fd49ed0ddd0fbcfe80e0776a39aefa426fcabd8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:18 -0500

    add 3003240/3003240.lua

---
+++ 3003240/3003240.lua
@@ -0,0 +1,13 @@
+-- 3003240's Lua and Manifest Created by Hubcap Manifest
+-- Catch Thieves
+-- Created: September 15, 2026 at 15:46:25 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3003240) -- Catch Thieves
+addtoken(3003240, "16943408131690038047")
+-- MAIN APP DEPOTS
+addappid(3003241, 1, "a75f1cbe82b358f44125fcb3e4b29cbd2b0b468c61ce2e4c1aa13bbd3a0976b5") -- Depot 3003241
+setManifestid(3003241, "3017189814581649041", 182100783)
