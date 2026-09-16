769964a58b5a9a252a782a7046f73c12a2d3f5ca
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:47 -0500

    add 2780990/2780990.lua

---
+++ 2780990/2780990.lua
@@ -0,0 +1,16 @@
+-- 2780990's Lua and Manifest Created by Hubcap Manifest
+-- Paradigm Island
+-- Created: September 15, 2026 at 04:18:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2780990, 1, "03097035d065ac66ff1befb15ef902013f94f02a4778f5f7f572e7f9017765f3") -- Paradigm Island
+-- MAIN APP DEPOTS
+addappid(2780991, 1, "3bca1689b57cf6f7856845c4da841dc67b5f385d1abea11616ab868b4b664f49") -- Depot 2780991
+setManifestid(2780991, "582659791242427164", 3318439148)
+addappid(2780992, 1, "c2b4808548bd68b5df7ecb3232a338079fc90a70a76fb48068344568dc026f7a") -- Depot 2780992
+setManifestid(2780992, "3290678965114119009", 3160373249)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2780993) -- Depot 2780993 (empty depot)
