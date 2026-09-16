a7689df681646bf0f124e7a7194a529db02c52e0
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:18 -0500

    add 1627430/1627430.lua

---
+++ 1627430/1627430.lua
@@ -0,0 +1,12 @@
+-- 1627430's Lua and Manifest Created by Hubcap Manifest
+-- Manikineko Online Playtest
+-- Created: September 15, 2026 at 05:08:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1627430) -- Manikineko Online Playtest
+-- MAIN APP DEPOTS
+addappid(1627431, 1, "b7187e6673142cbd24175a1c048d4508e79a239feb14ad51fa1010b4a68d458d") -- Depot 1627431
+setManifestid(1627431, "868548681828965241", 1859491528)
