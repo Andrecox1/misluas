4d52cac0dfb40840970c26f90ec9bcaa4d27b366
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:11:00 -0500

    add 2062380/2062380.lua

---
+++ 2062380/2062380.lua
@@ -0,0 +1,18 @@
+-- 2062380's Lua and Manifest Created by Hubcap Manifest
+-- Roots in the Sky - The Hand of Glory 2
+-- Created: September 15, 2026 at 16:46:12 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2062380, 1, "a8f547fb05141a6d99dec40691e4ba2ce73f95c55279b2bd615960cdbf6c57cf") -- Roots in the Sky - The Hand of Glory 2
+-- MAIN APP DEPOTS
+addappid(2062381, 1, "c8d5168fea5f42c6deaa093e96f53c60e87f2a12d88aaadb045bac02e38746ab") -- Depot 2062381
+setManifestid(2062381, "8687472200426316455", 0)
+addappid(2062382, 1, "eaa4da1cfad7a2f81f22dad031a9aed99ea0468fe5b5796b0702cf3000b608a2") -- Depot 2062382
+setManifestid(2062382, "7823093170326939959", 3011806104)
+addappid(2062383, 1, "0e0e2a04bce520770f544eff97a03ff746b80d7758af2456caca9315a1236b4e") -- Depot 2062383
+setManifestid(2062383, "8865289695045871718", 2971819344)
+addappid(2062385, 1, "10642bd79ff302886823183a3c34cab285956e830114e5d46fa97e718654df97") -- Depot 2062385
+setManifestid(2062385, "4897110878014359945", 2974772788)
