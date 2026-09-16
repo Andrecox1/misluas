3ede720aeed3ea9d5847389e06f5d1f21575d56b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:09:13 -0500

    add 4818230/4818230.lua

---
+++ 4818230/4818230.lua
@@ -0,0 +1,16 @@
+-- 4818230's Lua and Manifest Created by Hubcap Manifest
+-- Wo Long 2: Wings of Ember Alpha Demo
+-- Created: September 15, 2026 at 21:57:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4818230) -- Wo Long 2: Wings of Ember Alpha Demo
+-- MAIN APP DEPOTS
+addappid(4818231, 1, "242da6e8ea76e4433e87b82f567f6010bc4c49dbf358e8643814138b30978c14") -- Depot 4818231
+setManifestid(4818231, "8458126332439716561", 96983528698)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
