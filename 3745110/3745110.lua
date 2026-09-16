355a6c28930de386fc0834d6d4655fedf5eca35a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:40 -0500

    add 3745110/3745110.lua

---
+++ 3745110/3745110.lua
@@ -0,0 +1,16 @@
+-- 3745110's Lua and Manifest Created by Hubcap Manifest
+-- Jason Maxx
+-- Created: September 15, 2026 at 04:18:37 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3745110, 1, "a1d99958a874d218802a5afb010413d7d751da71b43c38ebd9d30ab0978eed8b") -- Jason Maxx
+-- MAIN APP DEPOTS
+addappid(3745111, 1, "5b41c1abfa3732f76b351fb259158c9bd91aa28e2fbfd302490ac01843a763f9") -- Depot 3745111
+setManifestid(3745111, "4217436519656565528", 839372738)
+addappid(3745112, 1, "4b2db90a42141956523ded45ac9503aa7a498288429f33707884cefd9b4209bd") -- Depot 3745112
+setManifestid(3745112, "237225834374521443", 867191963)
+addappid(3745113, 1, "de37f98185d36114f80cc33a1d67314d8f142d38f972b2b2f1461ab2892a8c05") -- Depot 3745113
+setManifestid(3745113, "890970146617683725", 853900001)
