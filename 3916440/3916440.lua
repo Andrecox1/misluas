517357335e48377b1236676822547a0654c354d4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:18 -0500

    add 3916440/3916440.lua

---
+++ 3916440/3916440.lua
@@ -0,0 +1,12 @@
+-- 3916440's Lua and Manifest Created by Hubcap Manifest
+-- 771
+-- Created: September 15, 2026 at 16:47:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3916440) -- 771
+-- MAIN APP DEPOTS
+addappid(3916441, 1, "544179350d0c8a48ef0c21ed45e3f0bef28f4dec070709fb5098cc0988525b87") -- Depot 3916441
+setManifestid(3916441, "2287206464484111729", 4060592772)
