4c3ac5a6955490f0e5190be5028de42e827e4d98
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:37 -0500

    add 5166890/5166890.lua

---
+++ 5166890/5166890.lua
@@ -0,0 +1,14 @@
+-- 5166890's Lua and Manifest Created by Hubcap Manifest
+-- Post Office: Tidy Up!
+-- Created: September 15, 2026 at 13:37:00 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5166890, 1, "2605e7864985207704ba20ae8ad1558cb945ee995f9b4d8bc400a30a1057fbf6") -- Post Office: Tidy Up!
+-- MAIN APP DEPOTS
+addappid(5166892, 1, "24ed62bf973d83323e1569cd8e753d9e968d5a4261e469dd04f55e67028156e5") -- Depot 5166892
+setManifestid(5166892, "6343322397623904936", 393135127)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(5166891) -- Depot 5166891 (empty depot)
