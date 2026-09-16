ca065b4972fd5a85899820ed2694e82ab691758f
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:41 -0500

    add 4461820/4461820.lua

---
+++ 4461820/4461820.lua
@@ -0,0 +1,14 @@
+-- 4461820's Lua and Manifest Created by Hubcap Manifest
+-- Oathbreak Dungeons
+-- Created: September 14, 2026 at 10:58:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4461820) -- Oathbreak Dungeons
+-- MAIN APP DEPOTS
+addappid(4461821, 1, "e70d982c4a76f466399a85a8152de974a90f76e9127acec46e7e1748de9277da") -- Depot 4461821
+setManifestid(4461821, "7838776578304604998", 399904653)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4461822) -- Depot 4461822 (empty depot)
