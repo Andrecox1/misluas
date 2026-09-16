f1717c6fc8f97c3de97f90e3faf3160ffb16c8a7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:50 -0500

    add 3169250/3169250.lua

---
+++ 3169250/3169250.lua
@@ -0,0 +1,14 @@
+-- 3169250's Lua and Manifest Created by Hubcap Manifest
+-- Escape From Vampire Castle: Miss Luna's Diary
+-- Created: September 15, 2026 at 16:46:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3169250) -- Escape From Vampire Castle: Miss Luna's Diary
+-- MAIN APP DEPOTS
+addappid(3169251, 1, "596fee57a786561ce7794f2b261fe1205148158b03d8750b51db06ae8e2ea380") -- Depot 3169251
+setManifestid(3169251, "1922898650761702253", 1534899252)
+addappid(3169252, 1, "d17185a9cf2691ae4bbd7b5227185f8bb94a4494a8c0065d6838893fdfa5711d") -- Depot 3169252
+setManifestid(3169252, "3345358897676882622", 2633263365)
