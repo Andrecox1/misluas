190a77eca118b432f3909a13f8da4e432f35621c
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:23 -0500

    add 3829310/3829310.lua

---
+++ 3829310/3829310.lua
@@ -0,0 +1,14 @@
+-- 3829310's Lua and Manifest Created by Hubcap Manifest
+-- Sublight
+-- Created: September 15, 2026 at 16:47:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3829310, 1, "62255bce763f1b384717faa1164ce1ea365050987fe55bcde0c78e5c282d7661") -- Sublight
+-- MAIN APP DEPOTS
+addappid(3829312, 1, "820ba2de8671a80b9138b12f48a783566f24f1c75f03c61914c7382dd93066c1") -- Depot 3829312
+setManifestid(3829312, "8405956331177024811", 4836791348)
+addappid(3829314, 1, "cd0196ff985b436941d113e20c9b83be06499f83f2814d62ab78dd0024fe3594") -- Depot 3829314
+setManifestid(3829314, "762878151087953595", 4876351821)
