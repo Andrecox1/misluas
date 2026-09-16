77502b7a616652528cd33afba2762239232174d4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:01 -0500

    add 3667960/3667960.lua

---
+++ 3667960/3667960.lua
@@ -0,0 +1,12 @@
+-- 3667960's Lua and Manifest Created by Hubcap Manifest
+-- Critical Shift Playtest
+-- Created: September 14, 2026 at 12:54:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3667960) -- Critical Shift Playtest
+-- MAIN APP DEPOTS
+addappid(3667961, 1, "cdd2cd3f02497c7c211d6dc7edaa85ba5f11b0f686516076aabfb2699797060c") -- Depot 3667961
+setManifestid(3667961, "8930097866357324527", 18379901406)
