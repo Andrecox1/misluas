a914cb416cb87c7d6688660811a323a79bd49940
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:09:59 -0500

    add 4311370/4311370.lua

---
+++ 4311370/4311370.lua
@@ -0,0 +1,12 @@
+-- 4311370's Lua and Manifest Created by Hubcap Manifest
+-- Supermarket Life Simulator
+-- Created: September 15, 2026 at 16:47:59 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4311370) -- Supermarket Life Simulator
+-- MAIN APP DEPOTS
+addappid(4311371, 1, "708b5a825b1a91319d792c147baa1d6baae2d1bd239aa35463b7d4fcb1f323e5") -- Depot 4311371
+setManifestid(4311371, "6028490691139751755", 139468124)
