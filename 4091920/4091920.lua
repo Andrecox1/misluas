aec765cbe6983eac9c6ccd30514828fa2d52a049
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:06:09 -0500

    add 4091920/4091920.lua

---
+++ 4091920/4091920.lua
@@ -0,0 +1,12 @@
+-- 4091920's Lua and Manifest Created by Hubcap Manifest
+-- Schedule Store Simulator
+-- Created: September 15, 2026 at 12:33:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4091920) -- Schedule Store Simulator
+-- MAIN APP DEPOTS
+addappid(4091921, 1, "b95cfa0795c43b1991e796750c89520d4c3997a923235a04bb5246ab30dc3cd8") -- Depot 4091921
+setManifestid(4091921, "2143110920248212937", 7258605960)
