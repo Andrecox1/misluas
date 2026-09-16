cfe63aa5f33e1de9cc6fb2c212a5dd6ffeea4abc
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:26 -0500

    add 4719560/4719560.lua

---
+++ 4719560/4719560.lua
@@ -0,0 +1,12 @@
+-- 4719560's Lua and Manifest Created by Hubcap Manifest
+-- Escape! House of Bonds
+-- Created: September 15, 2026 at 04:19:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4719560) -- Escape! House of Bonds
+-- MAIN APP DEPOTS
+addappid(4719561, 1, "ab8d143333c8bbfea837fadf3b7a31d5d9b429e54dd858eea49156c225c1b581") -- Depot 4719561
+setManifestid(4719561, "6792441639386921470", 185693737)
