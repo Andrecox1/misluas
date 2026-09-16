4f3a8a0e70b3138ce35ea236c7410cdbd64c72ed
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:41 -0500

    add 3220370/3220370.lua

---
+++ 3220370/3220370.lua
@@ -0,0 +1,22 @@
+-- 3220370's Lua and Manifest Created by Hubcap Manifest
+-- Soul Injector Commando
+-- Created: September 15, 2026 at 16:46:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3220370) -- Soul Injector Commando
+-- MAIN APP DEPOTS
+addappid(3220371, 1, "8d89209887c9b8fa6a6b4117ac48ba12a0bffc3fbe649eda15c237026bf8432a") -- Depot 3220371
+setManifestid(3220371, "9007315122355975594", 0)
+addappid(3220372, 1, "3dd8da0f9a165508eaf68efdc614a39701a99f9b51ebeeffbf033751750470da") -- Depot 3220372
+setManifestid(3220372, "1788416339248667034", 4130913740)
+addappid(3220373, 1, "c27caac1bc0524bba5129f53603d4c6a1ff060058cea69304b980e8ddbfeb5b4") -- Depot 3220373
+setManifestid(3220373, "4307798996489219003", 5226772990)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
