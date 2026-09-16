b90cc0a8d2bac346b0c756ca4a64c0c2b800055d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:30 -0500

    add 4604680/4604680.lua

---
+++ 4604680/4604680.lua
@@ -0,0 +1,12 @@
+-- 4604680's Lua and Manifest Created by Hubcap Manifest
+-- Tiny Tape Store
+-- Created: September 15, 2026 at 04:18:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4604680) -- Tiny Tape Store
+-- MAIN APP DEPOTS
+addappid(4604681, 1, "a32f3ee3b90b0fba15a7631f85f324b71a252cafd2fc62313be6c535267aa77d") -- Depot 4604681
+setManifestid(4604681, "5559474133451872186", 135555020)
