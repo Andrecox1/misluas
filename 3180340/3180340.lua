6115f654910ec8138dbaa33c9374a571e007b8b3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:52 -0500

    add 3180340/3180340.lua

---
+++ 3180340/3180340.lua
@@ -0,0 +1,18 @@
+-- 3180340's Lua and Manifest Created by Hubcap Manifest
+-- RIG Riot
+-- Created: September 15, 2026 at 16:55:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3180340) -- RIG Riot
+-- MAIN APP DEPOTS
+addappid(3180341, 1, "d600d2e463a8edf77a983f26d5755427debae6eab9b4444b3ca0d571308f457f") -- Depot 3180341
+setManifestid(3180341, "2023671248905790286", 3751403939)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
