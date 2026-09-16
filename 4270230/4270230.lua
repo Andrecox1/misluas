3f6387f463b9103f920271623b35ab542a9f7fc6
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:43 -0500

    add 4270230/4270230.lua

---
+++ 4270230/4270230.lua
@@ -0,0 +1,14 @@
+-- 4270230's Lua and Manifest Created by Hubcap Manifest
+-- Idle Bookstore
+-- Created: September 15, 2026 at 11:00:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4270230) -- Idle Bookstore
+-- MAIN APP DEPOTS
+addappid(4270231, 1, "d610315196d40157d39e3aca4fc8363ef7924c3ec651ab253b133d3855ba142e") -- Depot 4270231
+setManifestid(4270231, "6246848536547333789", 502288448)
+addappid(4270232, 1, "2e0dee80a47ac998f7fa49c89e0290c6936a5d431417d9e228e653392df1ea58") -- Depot 4270232
+setManifestid(4270232, "5216941728658569462", 496944811)
