91316afaa81ee2007e6c83bdb175da5b4995676e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:19 -0500

    add 2681980/2681980.lua

---
+++ 2681980/2681980.lua
@@ -0,0 +1,14 @@
+-- 2681980's Lua and Manifest Created by Hubcap Manifest
+-- Finite State Automaton Challenges 2
+-- Created: September 14, 2026 at 12:40:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2681980) -- Finite State Automaton Challenges 2
+-- MAIN APP DEPOTS
+addappid(2681981, 1, "2c9a4e50809aee84c5f3acd86d407e00120c5d5261d03fdb7a2088b9f65bf35d") -- Depot 2681981
+setManifestid(2681981, "84010632904671739", 232365179)
+addappid(2681982, 1, "da606197a3fa656857f9b20ce57968bf16609940e1fc0fe9267befc3c6d25b76") -- Depot 2681982
+setManifestid(2681982, "205642416367567628", 247525890)
