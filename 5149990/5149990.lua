a8782391df9c432d9498e5dcc2132d223a37c7c1
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:36 -0500

    add 5149990/5149990.lua

---
+++ 5149990/5149990.lua
@@ -0,0 +1,12 @@
+-- 5149990's Lua and Manifest Created by Hubcap Manifest
+-- Rushcremental Soundtrack
+-- Created: September 15, 2026 at 12:47:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(5149990) -- Rushcremental Soundtrack
+-- MAIN APP DEPOTS
+addappid(5149991, 1, "042c361cc46ad7fb36d944da3bd95452fa096d48b5afc584e9d399e6d7b8657a") -- Depot 5149991
+setManifestid(5149991, "8701827571776234353", 80302474)
