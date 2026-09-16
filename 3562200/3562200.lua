874ed3b7dc0e15c189d948eab963331f8cd6ea79
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:06:12 -0500

    add 3562200/3562200.lua

---
+++ 3562200/3562200.lua
@@ -0,0 +1,12 @@
+-- 3562200's Lua and Manifest Created by Hubcap Manifest
+-- Cloudscrapers
+-- Created: September 15, 2026 at 00:42:56 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3562200, 1, "00060ffb55e994814955855e901c7050f052dff64ad79ecbf1d381253400c26d") -- Cloudscrapers
+-- MAIN APP DEPOTS
+addappid(3562201, 1, "4d48089412ad6765f12748aa9746e75566fd7ab1bbb16edc3335aff75c4a6aee") -- Depot 3562201
+setManifestid(3562201, "367559560354326212", 1374703596)
