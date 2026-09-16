aa539e359d29c173407bae55aa5236c63b368664
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:09:05 -0500

    add 4126040/4126040.lua

---
+++ 4126040/4126040.lua
@@ -0,0 +1,16 @@
+-- 4126040's Lua and Manifest Created by Hubcap Manifest
+-- Aniimo
+-- Created: September 15, 2026 at 22:08:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4126040) -- Aniimo
+-- MAIN APP DEPOTS
+addappid(4126041, 1, "99f4818c2a9aea8378cc4c297b35877704b48a364b08393952515c7bc34a0627") -- Depot 4126041
+setManifestid(4126041, "4037846482976278183", 31135028020)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
