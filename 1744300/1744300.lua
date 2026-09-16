903fa54d7c314c81d10bb1c817158f4ce5d4b31b
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:09:35 -0500

    add 1744300/1744300.lua

---
+++ 1744300/1744300.lua
@@ -0,0 +1,12 @@
+-- 1744300's Lua and Manifest Created by Hubcap Manifest
+-- Dogs Organized Neatly Demo
+-- Created: September 15, 2026 at 20:31:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1744300) -- Dogs Organized Neatly Demo
+-- MAIN APP DEPOTS
+addappid(1744301, 1, "c6495aafae163770598721b9e7ac8443c8c05d886104ccaf2b0fec086ff18189") -- Depot 1744301
+setManifestid(1744301, "6184918087257504408", 82114458)
