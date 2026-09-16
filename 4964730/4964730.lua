472b1ced23f53472f5087c7b0e766c5df79ba3e9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:51 -0500

    add 4964730/4964730.lua

---
+++ 4964730/4964730.lua
@@ -0,0 +1,28 @@
+-- 4964730's Lua and Manifest Created by Hubcap Manifest
+-- MapCity
+-- Created: September 15, 2026 at 13:12:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4964730) -- MapCity
+-- MAIN APP DEPOTS
+addappid(4964731, 1, "c66abbfab714638be915079771b088feeec6b75b79e6de23c1b1a45a1aaf12ba") -- Depot 4964731
+setManifestid(4964731, "579252795796348584", 63194584)
+addappid(5057901, 1, "494662085ff1d135340bfdd4ae5ca6c94ba00be1c7a3e6947f57fda8201f78ec") -- Depot 5057901
+setManifestid(5057901, "3588329394258424787", 67861918)
+-- DLCS WITH DEDICATED DEPOTS
+-- MapCity North America (AppID: 5057960)
+addappid(5057960)
+addappid(5057960, 1, "473c357e4c2938dcf837b44209bbcbfc516b9b0a2c2fbd66df87afa1ea3d090f") -- MapCity North America - Depot 5057960
+setManifestid(5057960, "3995535094018692590", 185475320404)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4964732) -- Depot 4964732 (empty depot)
+-- addappid(4964733) -- Depot 4964733 (empty depot)
+-- addappid(4964734) -- Depot 4964734 (empty depot)
+-- addappid(4964735) -- Depot 4964735 (empty depot)
+-- addappid(4964736) -- Depot 4964736 (empty depot)
+-- addappid(4964737) -- Depot 4964737 (empty depot)
+-- addappid(4964738) -- Depot 4964738 (empty depot)
+-- addappid(4964739) -- Depot 4964739 (empty depot)
