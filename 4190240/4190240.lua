a6898680b0c42a27b193118ec239798b9ba7718a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:25:51 -0500

    add 4190240/4190240.lua

---
+++ 4190240/4190240.lua
@@ -0,0 +1,12 @@
+-- 4190240's Lua and Manifest Created by Hubcap Manifest
+-- Nuclear Design Bureau
+-- Created: September 15, 2026 at 17:50:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4190240, 1, "123c681feccc14a3c0562963b01d18896ca9d18613983ac5509feab88f1c0699") -- Nuclear Design Bureau
+-- MAIN APP DEPOTS
+addappid(4190241, 1, "65de2fb1c6dd98e87a4ae3b02178e224170de5135d7a24455a8b809bba8185d6") -- Depot 4190241
+setManifestid(4190241, "3770619305950991420", 1663324265)
