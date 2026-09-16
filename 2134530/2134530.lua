be6a955e12ca1686aa23964a7a1b8b00848ca1f5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:32 -0500

    add 2134530/2134530.lua

---
+++ 2134530/2134530.lua
@@ -0,0 +1,16 @@
+-- 2134530's Lua and Manifest Created by Hubcap Manifest
+-- Odyssey of Dremid'ir
+-- Created: September 15, 2026 at 15:05:26 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2134530) -- Odyssey of Dremid'ir
+-- MAIN APP DEPOTS
+addappid(2134531, 1, "db3b4dc1f4e503c503dbdb238f0b3635d024731f83cc69f11c5d347d9cea6e28") -- Depot 2134531
+setManifestid(2134531, "1565299161054731568", 3898207912)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Odyssey of Dremidir - Developer Commentary (AppID: 4829100) - missing depot keys
+-- addappid(4829100)
