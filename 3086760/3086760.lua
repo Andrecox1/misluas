e35fc7dd7acb76aa3f0c1a3aaac78c305f421e8a
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:06:24 -0500

    add 3086760/3086760.lua

---
+++ 3086760/3086760.lua
@@ -0,0 +1,12 @@
+-- 3086760's Lua and Manifest Created by Hubcap Manifest
+-- Melty Amethyst
+-- Created: September 15, 2026 at 00:35:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3086760, 1, "f7ae3d93345d1eee65f8683e5ffa2ad576a22865b53b74e74cdbd2f2c0c5f1ff") -- Melty Amethyst
+-- MAIN APP DEPOTS
+addappid(3086761, 1, "abaf81973084733299bd6139ca19ace5dfa83c69909ddd7825398524919d677d") -- Depot 3086761
+setManifestid(3086761, "802076297135246397", 298967375)
