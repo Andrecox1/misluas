83738e1a719145f0f8f230e1285e91d8de1c34b1
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:22 -0500

    add 3411270/3411270.lua

---
+++ 3411270/3411270.lua
@@ -0,0 +1,15 @@
+-- 3411270's Lua and Manifest Created by Hubcap Manifest
+-- EA SPORTS FC™ 26 Closed Beta
+-- Created: September 15, 2026 at 15:45:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3411270) -- EA SPORTS FC™ 26 Closed Beta
+addtoken(3411270, "4801779341281297926")
+-- MAIN APP DEPOTS
+addappid(3411271, 1, "8747d96535893ec375b85f9f57d155a8b4a21ea64c3d240190c47e95bd4af9f3") -- Depot 3411271
+setManifestid(3411271, "8179761851065322106", 54369890643)
+addappid(3411272, 1, "db86ecf1466659826a3221d0bb66817243b6a45320c07018bacdc0abc7d53033") -- Depot 3411272
+setManifestid(3411272, "8038845821977572929", 4523678870)
