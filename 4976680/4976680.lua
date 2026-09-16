39b7cf866bab6b9dd2d06755f2fc240255200b88
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:06 -0500

    add 4976680/4976680.lua

---
+++ 4976680/4976680.lua
@@ -0,0 +1,14 @@
+-- 4976680's Lua and Manifest Created by Hubcap Manifest
+-- Black Lotus
+-- Created: September 14, 2026 at 12:50:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4976680) -- Black Lotus
+-- MAIN APP DEPOTS
+addappid(4976681, 1, "82d3952547bb1a2aaedd54a7ed84b7e628f893543b2befb5c37d3b76e01c9313") -- Depot 4976681
+setManifestid(4976681, "1215588555495736628", 7522153570)
+addappid(4976682, 1, "43beefa41193f4f1a939321cc52e2b62e1ab632a843ea74e19467ac5e32b9b93") -- Depot 4976682
+setManifestid(4976682, "1213647588891227771", 7505455932)
