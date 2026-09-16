b001c12352c12d22deda40e0cde085aee5afa145
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:02 -0500

    add 2887580/2887580.lua

---
+++ 2887580/2887580.lua
@@ -0,0 +1,17 @@
+-- 2887580's Lua and Manifest Created by Hubcap Manifest
+-- Active Matter
+-- Created: September 15, 2026 at 12:04:04 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(2887580) -- Active Matter
+-- MAIN APP DEPOTS
+addappid(2887581, 1, "279f63502b38413b7f07b57ac3e32cf4b9c303e3977107d2891cf053b159f5c9") -- Depot 2887581
+setManifestid(2887581, "8718670431717722752", 38052950915)
+addappid(2887582, 1, "fe6a84f76a66a813069f11ee823b3e5946abe5a2503bc72302a56c1db2cde372") -- Depot 2887582
+setManifestid(2887582, "5826875760740471667", 752190179)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4107830) -- Active Matter - Upgrade to Advanced Edition
+addappid(4107840) -- Active Matter - Upgrade to Deluxe Edition
