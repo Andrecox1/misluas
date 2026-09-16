ae119495c6500c08d779bad5f33d95861f069620
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:34 -0500

    add 4301110/4301110.lua

---
+++ 4301110/4301110.lua
@@ -0,0 +1,14 @@
+-- 4301110's Lua and Manifest Created by Hubcap Manifest
+-- The Headlands
+-- Created: September 15, 2026 at 18:16:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4301110, 1, "59d545888d225dc76fab03d337efe10015cd42a57bb0be9388f5924e96c9d34b") -- The Headlands
+-- MAIN APP DEPOTS
+addappid(4301111, 1, "af541c65e31df9cd59f5971a9482d0e531bc5be6dbcca26b0cc77c3bf1940b3e") -- Depot 4301111
+setManifestid(4301111, "4729540924810717080", 1092004093)
+addappid(4301112, 1, "3ec173e8bafc14fd3e5d51ec05983ba510e84e12f10e27a858295238f8111571") -- Depot 4301112
+setManifestid(4301112, "1176466102321921373", 1112221307)
