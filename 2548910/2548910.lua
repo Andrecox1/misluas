508470ee5fec4047b8cc718c9bb23b29237ea0e2
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:29 -0500

    add 2548910/2548910.lua

---
+++ 2548910/2548910.lua
@@ -0,0 +1,21 @@
+-- 2548910's Lua and Manifest Created by Hubcap Manifest
+-- Peacemaker: Bloody Emperor
+-- Created: September 14, 2026 at 11:05:29 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2548910) -- Peacemaker: Bloody Emperor
+-- MAIN APP DEPOTS
+addappid(2548911, 1, "2c55d7da911aec258e3d8f7649ce866a6edca43cca6a52cd26927954ce7e6bce") -- Depot 2548911
+setManifestid(2548911, "7701382411664275900", 808590948)
+-- DLCS WITH DEDICATED DEPOTS
+-- Peacemaker Bloody Emperor - Official Artbook (AppID: 5013360)
+addappid(5013360)
+addappid(5013361, 1, "38ecfd898e20c53cb1966b1d29871411f5b9eea65eee2f14eb3c33963fc85aec") -- Peacemaker Bloody Emperor - Official Artbook - Depot 5013361
+setManifestid(5013361, "6063081034080784693", 170449938)
+addappid(5013362, 1, "12e225dfe26963c3222ca3c62839ce3c15ecc0646389f30d1885d94aa9aba314") -- Peacemaker Bloody Emperor - Official Artbook - Depot 5013362
+setManifestid(5013362, "1754724459544187359", 170871045)
+addappid(5013363, 1, "0e5ee983be670c8b021f40e0b6f6057bea15dcfa7eb2e972f132a596964fbf15") -- Peacemaker Bloody Emperor - Official Artbook - Depot 5013363
+setManifestid(5013363, "5914843704372049765", 170781138)
