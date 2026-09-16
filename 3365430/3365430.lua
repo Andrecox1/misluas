3accd075273dcec53d3dacffa0abf66864ba7b82
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:32 -0500

    add 3365430/3365430.lua

---
+++ 3365430/3365430.lua
@@ -0,0 +1,14 @@
+-- 3365430's Lua and Manifest Created by Hubcap Manifest
+-- ファンタジーワールド ～魔王の想いと真実と～
+-- Created: September 15, 2026 at 16:46:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3365430) -- ファンタジーワールド ～魔王の想いと真実と～
+-- MAIN APP DEPOTS
+addappid(3365432, 1, "276126609d5d24a71c5df180284db70b47510fc556c4f318bf59c665ab50cc70") -- Depot 3365432
+setManifestid(3365432, "6781032662620701823", 1053889241)
+addappid(3365431, 1, "5e107c0802e12249c4514d90e04f10071c44fb3fe6a22b89a135caaf2e29a13a") -- Depot 3365431
+setManifestid(3365431, "4216508201809205872", 1053639217)
