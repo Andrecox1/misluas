ca5d4f66502002517031bfdf18fae6b5f4ba98b7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:04 -0500

    add 3199470/3199470.lua

---
+++ 3199470/3199470.lua
@@ -0,0 +1,16 @@
+-- 3199470's Lua and Manifest Created by Hubcap Manifest
+-- Knuckle Jet
+-- Created: September 15, 2026 at 21:43:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3199470) -- Knuckle Jet
+-- MAIN APP DEPOTS
+addappid(3199471, 1, "5331c9b56dc82e7d5e7a31dc6e3d1732d140f3b762e9d2ab0994b7423d4df14a") -- Depot 3199471
+setManifestid(3199471, "1062195749972967007", 10981005772)
+addappid(3199472, 1, "4437301afc26ee8abcf1004599593a992461b5c8cd04c09ec70b1ba0555a4dab") -- Depot 3199472
+setManifestid(3199472, "4039905352090159079", 0)
+addappid(3199473, 1, "8fcf27162bd4660c6ea04837e9c2671add83ef64f3bf512e998066cfb2b10729") -- Depot 3199473
+setManifestid(3199473, "3747710148731423620", 11122103608)
