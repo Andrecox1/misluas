fe794c255e39e71667c7e7e012d9f8fdf04c6b24
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:28 -0500

    add 3820860/3820860.lua

---
+++ 3820860/3820860.lua
@@ -0,0 +1,12 @@
+-- 3820860's Lua and Manifest Created by Hubcap Manifest
+-- Touch of Madness
+-- Created: September 15, 2026 at 16:47:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3820860) -- Touch of Madness
+-- MAIN APP DEPOTS
+addappid(3820861, 1, "49e21eed15466ed6078a7193cbe2a536881812af489d4c72918b25741b259caa") -- Depot 3820861
+setManifestid(3820861, "8849625235701149055", 227104530)
