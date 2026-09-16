8f4b31ca193f0ad1b4ca823e118df3e9bd2ef931
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:16 -0500

    add 4659250/4659250.lua

---
+++ 4659250/4659250.lua
@@ -0,0 +1,14 @@
+-- 4659250's Lua and Manifest Created by Hubcap Manifest
+-- A Game About Flipping Cards
+-- Created: September 15, 2026 at 23:27:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4659250) -- A Game About Flipping Cards
+-- MAIN APP DEPOTS
+addappid(4659252, 1, "fffd9a3692f3dd0e6fd5f7745fa32041c35101c451b9d08bc6ef7dfe186a1234") -- Depot 4659252
+setManifestid(4659252, "817933357449836915", 213681627)
+addappid(4659253, 1, "99119da99a8d76018bf007ae530590337d61a826f943d7a1c22620cb44e398c0") -- Depot 4659253
+setManifestid(4659253, "8018946600744190327", 276556397)
