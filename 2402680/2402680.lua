bb9139289016d4830627afeff1b6ec9d71f58153
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:03 -0500

    add 2402680/2402680.lua

---
+++ 2402680/2402680.lua
@@ -0,0 +1,12 @@
+-- 2402680's Lua and Manifest Created by Hubcap Manifest
+-- Dimraeth
+-- Created: September 15, 2026 at 22:57:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2402680, 1, "bb30bdf9f0811948e2480d9fb2fa16b82ecafac8921aeb0f4a1b8a7e64eff311") -- Dimraeth
+-- MAIN APP DEPOTS
+addappid(2402681, 1, "4f10d1efbb0cab1a8340d1e37c165519ab3ab8d50cea09ecbeff7ce6d3d5b71e") -- Depot 2402681
+setManifestid(2402681, "4744128189606120587", 4597932588)
