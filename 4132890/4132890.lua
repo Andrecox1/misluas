ba61e635703abb91533e105fc018f681a30ad3a9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:05 -0500

    add 4132890/4132890.lua

---
+++ 4132890/4132890.lua
@@ -0,0 +1,18 @@
+-- 4132890's Lua and Manifest Created by Hubcap Manifest
+-- Fright Train
+-- Created: September 15, 2026 at 16:47:51 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4132890, 1, "b3d3b6d6af88e7a35245ce5bcbfee47d51895b3e2e822a5db247f0e6df5b0338") -- Fright Train
+-- MAIN APP DEPOTS
+addappid(4132891, 1, "32ca09c22fff81e09d23c36da5e01ac03da8164540b34ea864c5748b105cc868") -- Depot 4132891
+setManifestid(4132891, "8038127132106501648", 774625750)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
