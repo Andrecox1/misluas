85fbcf2c1eabfbe07001043972269cffa8deb451
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:13:56 -0500

    add 4884730/4884730.lua

---
+++ 4884730/4884730.lua
@@ -0,0 +1,12 @@
+-- 4884730's Lua and Manifest Created by Hubcap Manifest
+-- AVALONCLASH
+-- Created: September 15, 2026 at 00:22:41 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4884730) -- AVALONCLASH
+-- MAIN APP DEPOTS
+addappid(4884732, 1, "e3ec1eb40507eea7f13b1cd6a577c1a4eb4216e30efbac057943352f0cb6bc1f") -- Depot 4884732
+setManifestid(4884732, "2876120390880170104", 713161676)
