84da7980d7454462642b97b276e215f0c7e831e5
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:57 -0500

    add 3179160/3179160.lua

---
+++ 3179160/3179160.lua
@@ -0,0 +1,12 @@
+-- 3179160's Lua and Manifest Created by Hubcap Manifest
+-- Chef Hands : Kitchen Mayhem
+-- Created: September 15, 2026 at 07:15:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3179160) -- Chef Hands : Kitchen Mayhem
+-- MAIN APP DEPOTS
+addappid(3179161, 1, "4f375dba4218185b3a5561a4d3c93087d85c8c4f0ede75df06f9ad9e9c83e372") -- Depot 3179161
+setManifestid(3179161, "1461737847035834885", 754815103)
