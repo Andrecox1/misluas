a182f7b419f7c1798285f17c53dbe8b736d5dbd8
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:21 -0500

    add 4210440/4210440.lua

---
+++ 4210440/4210440.lua
@@ -0,0 +1,18 @@
+-- 4210440's Lua and Manifest Created by Hubcap Manifest
+-- Solvizz
+-- Created: September 15, 2026 at 12:03:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 3
+
+-- MAIN APPLICATION
+addappid(4210440) -- Solvizz
+-- MAIN APP DEPOTS
+addappid(4210441, 1, "54ac6f33c7a919fa8aa8ef2fb9f6fb0cb50b26174516a85fbfe436e2d46def8f") -- Depot 4210441
+setManifestid(4210441, "2169723880796677475", 329457805)
+addappid(4210442, 1, "532d0d1620782a65798c620eec8596e1487ec138b49bd40716b7e8f6c7be26f1") -- Depot 4210442
+setManifestid(4210442, "5466660058006529819", 318049873)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(5094680) -- Solvizz Cold Steel Theme
+addappid(5094690) -- Solvizz Stained Glass Theme
+addappid(5094700) -- Solvizz Petal  Lace Theme
