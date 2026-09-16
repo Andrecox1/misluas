84adbb4962f869bd2d8336eae0403cbe71808efb
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:42 -0500

    add 4353350/4353350.lua

---
+++ 4353350/4353350.lua
@@ -0,0 +1,20 @@
+-- 4353350's Lua and Manifest Created by Hubcap Manifest
+-- Meeple Incremental
+-- Created: September 14, 2026 at 13:41:15 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(4353350) -- Meeple Incremental
+-- MAIN APP DEPOTS
+addappid(4353351, 1, "5f12f8b1befcea00bd4fbe00110803c9674aa8ccd55a1c630c59e952639c611e") -- Depot 4353351
+setManifestid(4353351, "2582471629809915505", 151994309)
+addappid(4353352, 1, "e472442f583e3027ee51baa291db1d0674d5f95c52b270ba786533eaf5dd310c") -- Depot 4353352
+setManifestid(4353352, "5765472904700419134", 144538805)
+addappid(4353353, 1, "92951df1a82e143d760e29baa6e58089f7f49089d761cc6735a26f32bc69ac9e") -- Depot 4353353
+setManifestid(4353353, "115142659414651007", 190428227)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4360720) -- Meeple Incremental - Supporter Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4360720) -- Depot 4360720 (empty depot)
