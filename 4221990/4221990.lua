e1a4f3a7d22f3111efbd545ec6eac936e4b325dd
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:15 -0500

    add 4221990/4221990.lua

---
+++ 4221990/4221990.lua
@@ -0,0 +1,16 @@
+-- 4221990's Lua and Manifest Created by Hubcap Manifest
+-- Fish Lab
+-- Created: September 15, 2026 at 09:07:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(4221990) -- Fish Lab
+-- MAIN APP DEPOTS
+addappid(4221991, 1, "bfd0777fd4fed0e834227630f10e9b28a167250c2298bc6df4f7673ec256b963") -- Depot 4221991
+setManifestid(4221991, "6432074660723705728", 407024704)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
