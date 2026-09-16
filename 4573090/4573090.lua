4df95664fc476551e33ceeeeba9f474989b386b3
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:38 -0500

    add 4573090/4573090.lua

---
+++ 4573090/4573090.lua
@@ -0,0 +1,12 @@
+-- 4573090's Lua and Manifest Created by Hubcap Manifest
+-- Pibcremental
+-- Created: September 15, 2026 at 12:06:22 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4573090) -- Pibcremental
+-- MAIN APP DEPOTS
+addappid(4573091, 1, "f64e53ee14737d39d0eaa9c0032b5bbfa13dd86edffb1b57a8fcdd7ec8a20359") -- Depot 4573091
+setManifestid(4573091, "681595022750925240", 263919537)
