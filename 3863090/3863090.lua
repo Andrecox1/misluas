5dc39af9f6698762d2944a860bd8d52689bd8f4d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:48 -0500

    add 3863090/3863090.lua

---
+++ 3863090/3863090.lua
@@ -0,0 +1,12 @@
+-- 3863090's Lua and Manifest Created by Hubcap Manifest
+-- Blood Lily: Killers Can't Sink a Yuri Ship
+-- Created: September 15, 2026 at 10:59:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3863090) -- Blood Lily: Killers Can't Sink a Yuri Ship
+-- MAIN APP DEPOTS
+addappid(3863091, 1, "00a579baa3b3df4c24e8135d65c2d195c576010ec8d382746302273ad6c3e439") -- Depot 3863091
+setManifestid(3863091, "5032015815737356014", 494940336)
