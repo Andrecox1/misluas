06223735d6d27fbbe36666010c64422c8adf9208
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:04:40 -0500

    add 4991340/4991340.lua

---
+++ 4991340/4991340.lua
@@ -0,0 +1,12 @@
+-- 4991340's Lua and Manifest Created by Hubcap Manifest
+-- Might & Poison Mushroom
+-- Created: September 16, 2026 at 00:01:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4991340) -- Might & Poison Mushroom
+-- MAIN APP DEPOTS
+addappid(4991341, 1, "dbdf305555d11d45ec05d104a3e7fe0f588a1b7deffb62f6e81ba7c08dfe27b0") -- Depot 4991341
+setManifestid(4991341, "4808879392802887984", 218693513)
