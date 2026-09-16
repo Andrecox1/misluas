4a0aecad8b44af4857e6591a2b6c7cd2eba3fde7
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:09:22 -0500

    add 4944090/4944090.lua

---
+++ 4944090/4944090.lua
@@ -0,0 +1,19 @@
+-- 4944090's Lua and Manifest Created by Hubcap Manifest
+-- Perfect Place
+-- Created: September 15, 2026 at 21:27:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4944090) -- Perfect Place
+addtoken(4944090, "8912752596922361476")
+-- MAIN APP DEPOTS
+addappid(4944091, 1, "6492b7c375241774230089db8ca4bcc05f15217c46c96d2608026f08794dc64b") -- Depot 4944091
+setManifestid(4944091, "2480958594520241232", 5818289864)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
