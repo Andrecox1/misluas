30ed55afcecaf3faad5803d42c0ac4c1fe12149d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:23 -0500

    add 4804070/4804070.lua

---
+++ 4804070/4804070.lua
@@ -0,0 +1,14 @@
+-- 4804070's Lua and Manifest Created by Hubcap Manifest
+-- 嘘！超能少女代购中！
+-- Created: September 15, 2026 at 04:19:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4804070) -- 嘘！超能少女代购中！
+-- MAIN APP DEPOTS
+addappid(4804071, 1, "5ed5361dc54ef6c84f29d84204ce7f8b81e7f8fad4938f6d4721f448ca39b423") -- Depot 4804071
+setManifestid(4804071, "6613497966532199645", 675301186)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4804072) -- Depot 4804072 (empty depot)
