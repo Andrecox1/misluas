2c4bb6b80bfa043d9179e6174b9af644a071660e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:06:04 -0500

    add 4883590/4883590.lua

---
+++ 4883590/4883590.lua
@@ -0,0 +1,21 @@
+-- 4883590's Lua and Manifest Created by Hubcap Manifest
+-- iRacing® Studios NASCAR 26
+-- Created: September 15, 2026 at 23:06:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(4883590, 1, "3a2266924431e4d529497c79fe4dc08bc861f4eeddae12915d366dacf413612c") -- iRacing® Studios NASCAR 26
+-- MAIN APP DEPOTS
+addappid(4883591, 1, "3158af4ba833f96b19d14b98177e42aeb9690b57976e24b6f2cdcf2b9fbbbc6d") -- Depot 4883591
+setManifestid(4883591, "4683279188952810071", 62021559435)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4899500) -- NASCAR 26 - September Pack
+addappid(4899550) -- NASCAR 26 - Season Pass
