967d709be47436ef36c51dcd5057ae79fb7500c9
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:11:22 -0500

    add 4983480/4983480.lua

---
+++ 4983480/4983480.lua
@@ -0,0 +1,12 @@
+-- 4983480's Lua and Manifest Created by Hubcap Manifest
+-- Treasure Cove Playtest
+-- Created: September 15, 2026 at 16:23:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4983480) -- Treasure Cove Playtest
+-- MAIN APP DEPOTS
+addappid(4983481, 1, "f725ade6b2fa840bdb2928902c9cb10eba4fe24d8bd09a1016100c4b8e9ca807") -- Depot 4983481
+setManifestid(4983481, "8642570198493192120", 2972185582)
