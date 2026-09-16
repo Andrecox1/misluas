c1c77f6b93e573d1640b6b178074a687bb601f9d
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:25:56 -0500

    add 3891400/3891400.lua

---
+++ 3891400/3891400.lua
@@ -0,0 +1,16 @@
+-- 3891400's Lua and Manifest Created by Hubcap Manifest
+-- Mixed Spirits: Bartender
+-- Created: September 15, 2026 at 16:22:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3891400) -- Mixed Spirits: Bartender
+-- MAIN APP DEPOTS
+addappid(3891401, 1, "078ede55794a7752650b08bd3f404fb5f18028b0a0e50b585617c692b04c3331") -- Depot 3891401
+setManifestid(3891401, "3266107397778334396", 3228525546)
+addappid(3891402, 1, "3f82c9162042223bf36bba89fbd97c8aa832ad8c81eea48e37df5990627487cf") -- Depot 3891402
+setManifestid(3891402, "5622359647144612565", 3245374232)
+addappid(3891403, 1, "e842d67d7ab1210af461c69b24e977e0f457f4385fb2713fb4c99630ed56924b") -- Depot 3891403
+setManifestid(3891403, "623917331743559214", 3224559930)
