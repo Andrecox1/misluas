d28ed8eee1a9a42b54036832a8a39e216577e929
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:26:43 -0500

    add 2695500/2695500.lua

---
+++ 2695500/2695500.lua
@@ -0,0 +1,18 @@
+-- 2695500's Lua and Manifest Created by Hubcap Manifest
+-- Rushcremental
+-- Created: September 15, 2026 at 12:47:14 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2695500) -- Rushcremental
+-- MAIN APP DEPOTS
+addappid(2695502, 1, "5b65e564e2fb61ec8b650724d09522597d0939f38bad4b0ae1531a50b3ff9530") -- Depot 2695502
+setManifestid(2695502, "7694375361416107941", 117509807)
+addappid(2695503, 1, "9ea58d81f726b824f111e5b0e428efa9488892327592b4c4aafebb02d8101120") -- Depot 2695503
+setManifestid(2695503, "3058509114891065051", 272710086)
+addappid(2695504, 1, "e76b48e2221dcfd04ae80db7f0c98d3ebb4d199220bdcdcacdfb7799006efc4f") -- Depot 2695504
+setManifestid(2695504, "1296958308516740696", 264034506)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2695501) -- Depot 2695501 (empty depot)
