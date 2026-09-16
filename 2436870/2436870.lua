212b39dc74819487d0c929363a4b82dbe2cc4021
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:05:14 -0500

    add 2436870/2436870.lua

---
+++ 2436870/2436870.lua
@@ -0,0 +1,20 @@
+-- 2436870's Lua and Manifest Created by Hubcap Manifest
+-- Sigilfarer: A Dice-Rolling Roguelite
+-- Created: September 15, 2026 at 10:49:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2436870, 1, "65c022583724b7fcd7ea28bb72ec655b49d56d0f28c3b1ee44bbb29fd2ecbee1") -- Sigilfarer: A Dice-Rolling Roguelite
+-- MAIN APP DEPOTS
+addappid(2436875, 1, "46346f5804b3abea2ecd3045b1eab6fd06a411fd0e926c305a567b2464c6e331") -- Depot 2436875
+setManifestid(2436875, "7804899158155444547", 1380378193)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2436871) -- Depot 2436871 (empty depot)
