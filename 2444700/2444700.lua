a4af5ff852073c800141876937fbb1ef797a1dea
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:10:56 -0500

    add 2444700/2444700.lua

---
+++ 2444700/2444700.lua
@@ -0,0 +1,12 @@
+-- 2444700's Lua and Manifest Created by Hubcap Manifest
+-- The Event
+-- Created: September 15, 2026 at 16:46:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2444700) -- The Event
+-- MAIN APP DEPOTS
+addappid(2444701, 1, "81ce9e41da54af9f883af8102f7bbf9a13a816b6dc44cf2102d3c530e0645534") -- Depot 2444701
+setManifestid(2444701, "5993241101529193579", 14388648356)
