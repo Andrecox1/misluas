8266e25e4748cc6a5c4e634c03b437158ae72bca
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:11:36 -0500

    add 5034120/5034120.lua

---
+++ 5034120/5034120.lua
@@ -0,0 +1,20 @@
+-- 5034120's Lua and Manifest Created by Hubcap Manifest
+-- 6F: NIGHT SCHOOL LOOP∞
+-- Created: September 15, 2026 at 16:22:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(5034120) -- 6F: NIGHT SCHOOL LOOP∞
+-- MAIN APP DEPOTS
+addappid(5034121, 1, "3dd8074e6ba9241167cce724e63dfa6bcacae1cdd2fdef1b601556766dccbdf6") -- Depot 5034121
+setManifestid(5034121, "5844760996118356845", 1101486787)
+addappid(5034122, 1, "58970da32cb923a1385661d642dc77305f156cf23dce7027db18e3a9a5c5674b") -- Depot 5034122
+setManifestid(5034122, "2313689307159423394", 603696416)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
