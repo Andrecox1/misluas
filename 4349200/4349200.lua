6bc1cd3ecd122d2b75cca93b8d16d5ebe6c7d073
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:09:26 -0500

    add 4349200/4349200.lua

---
+++ 4349200/4349200.lua
@@ -0,0 +1,12 @@
+-- 4349200's Lua and Manifest Created by Hubcap Manifest
+-- Probably Stolen Demo
+-- Created: September 15, 2026 at 20:49:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4349200) -- Probably Stolen Demo
+-- MAIN APP DEPOTS
+addappid(4349201, 1, "e89eab55ebd90d262576d10ce59a9584f72c6f858808cdd5284be84d0ad6221d") -- Depot 4349201
+setManifestid(4349201, "8181183626709590689", 938874484)
