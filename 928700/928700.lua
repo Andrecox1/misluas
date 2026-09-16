0e38b7ae2b5ea200c168ea30a94f00fb0ebab287
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:13:52 -0500

    add 928700/928700.lua

---
+++ 928700/928700.lua
@@ -0,0 +1,13 @@
+-- 928700's Lua and Manifest Created by Hubcap Manifest
+-- !BurnToDie!
+-- Created: September 15, 2026 at 00:30:43 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(928700) -- !BurnToDie!
+addtoken(928700, "11286492955629269694")
+-- MAIN APP DEPOTS
+addappid(928701, 1, "47f6b04e2b626f14a5dfbdc6982741b99b3fe4a4b1d4a689136201ef5b809e67") -- Depot 928701
+setManifestid(928701, "1188509487060520626", 17481340)
