050e4856b6d3118062a31805f19581a652e93f40
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:14:56 -0500

    add 4103250/4103250.lua

---
+++ 4103250/4103250.lua
@@ -0,0 +1,12 @@
+-- 4103250's Lua and Manifest Created by Hubcap Manifest
+-- Arcturus Vision Camera
+-- Created: September 14, 2026 at 13:20:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4103250) -- Arcturus Vision Camera
+-- MAIN APP DEPOTS
+addappid(4103251, 1, "2176103f1aa9ede083d418237a650c0453a398f28bf94d3e44193ab000504fd0") -- Depot 4103251
+setManifestid(4103251, "6404913942723855035", 1095997225)
