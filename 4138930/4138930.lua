90d04cffb8f272fecc1904fda5181fc2a26f0d1e
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:04:56 -0500

    add 4138930/4138930.lua

---
+++ 4138930/4138930.lua
@@ -0,0 +1,18 @@
+-- 4138930's Lua and Manifest Created by Hubcap Manifest
+-- GameZone - Game Store Simulator
+-- Created: September 15, 2026 at 05:14:02 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4138930) -- GameZone - Game Store Simulator
+-- MAIN APP DEPOTS
+addappid(4138932, 1, "aaf123748c070181b5049a53597d01e9ff009648c30028d76bca20a3af138cab") -- Depot 4138932
+setManifestid(4138932, "1950723828226159077", 3188165724)
+addappid(4138933, 1, "22d3b1f7f2c89955d0ff80ac5ce1d4a9a04f28cb0c30d8f6e11d22c13f4e3cfd") -- Depot 4138933
+setManifestid(4138933, "2348322872986477474", 1478328148)
+addappid(4138934, 1, "97cb90288d7a0a2973e9e0ccf2253352896ff97cd867c0ad415eb99cae5fe821") -- Depot 4138934
+setManifestid(4138934, "4285249890017416379", 3278008725)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4138931) -- Depot 4138931 (empty depot)
