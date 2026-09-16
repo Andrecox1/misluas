efddcdd6993ed8a17e82ec1a3e8f5fa744809db4
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 22:27:26 -0500

    add 3371710/3371710.lua

---
+++ 3371710/3371710.lua
@@ -0,0 +1,12 @@
+-- 3371710's Lua and Manifest Created by Hubcap Manifest
+-- Rabbit Trail
+-- Created: September 15, 2026 at 12:03:35 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3371710) -- Rabbit Trail
+-- MAIN APP DEPOTS
+addappid(3371711, 1, "fe6c3b714cb766406715292aa97052d5fdfbda9d5ec62709fe0ca3ce1fba9478") -- Depot 3371711
+setManifestid(3371711, "2351540650063292511", 336699026)
