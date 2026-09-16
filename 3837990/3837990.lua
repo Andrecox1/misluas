ff2fccfeebca7ea9ae7b2701375daeb8085a1180
Andrecox1 <andresfelipear3357963@gmail.com>
2026-09-15 23:15:52 -0500

    add 3837990/3837990.lua

---
+++ 3837990/3837990.lua
@@ -0,0 +1,15 @@
+-- 3837990's Lua and Manifest Created by Hubcap Manifest
+-- シノビザムライ／Shinobi Samurai
+-- Created: September 14, 2026 at 10:58:19 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3837990) -- シノビザムライ／Shinobi Samurai
+-- MAIN APP DEPOTS
+addappid(3837991, 1, "bd7771f84721ab8c01be926aac7d8058a0ed7db9d0c3572a03f6b97b0f56b77a") -- Depot 3837991
+setManifestid(3837991, "3543863724986370942", 281550546)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3837992) -- Depot 3837992 (empty depot)
+-- addappid(3837993) -- Depot 3837993 (empty depot)
